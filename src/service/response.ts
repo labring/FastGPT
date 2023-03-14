eb95da415d66011cfc6611b979b465f31a338559
Archer <stakeswky@gmail.com>
2023-03-14 14:58:35 +0800

    perf: 流响应

--- src/service/response.ts
+++ src/service/response.ts
@@ -1,5 +1,5 @@
 import { NextApiResponse } from 'next';
-import { openaiError } from './errorCode';
+import { openaiError, proxyError } from './errorCode';
 
 export interface ResponseType<T = any> {
   code: number;
@@ -23,12 +23,13 @@ export const jsonRes = (
     msg = error?.message || '请求错误';
     if (typeof error === 'string') {
       msg = error;
-    } else if (error?.response?.data?.message in openaiError) {
-      msg = openaiError[error?.response?.data?.message];
+    } else if (proxyError[error?.code]) {
+      msg = '服务器代理出错';
+    } else if (openaiError[error?.response?.statusText]) {
+      msg = openaiError[error.response.statusText];
     }
 
-    console.log('error->', error);
-    console.log('error->', msg);
+    console.log('error->', error.code, error?.response?.statusText, msg);
   }
 
   res.json({
