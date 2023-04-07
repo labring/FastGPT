84d91f3f76e18895b192bc85d695bfc7f7860940
archer <545436317@qq.com>
2023-04-07 15:46:30 +0800

    perf: 接口大小

--- src/pages/api/model/data/splitData.ts
+++ src/pages/api/model/data/splitData.ts
@@ -76,3 +76,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb'
+    }
+  }
+};
