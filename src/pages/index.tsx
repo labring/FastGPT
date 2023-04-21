901527b02484ec8f7169c4bf40569e88d0944035
archer <stakeswky@gmail.com>
2023-04-21 23:47:52 +0800

    fix: 邀请id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from '@chakra-ui/react';
 import Markdown from '@/components/Markdown';
 import { useMarkdown } from '@/hooks/useMarkdown';
+import { useRouter } from 'next/router';
 
 const Home = () => {
+  const { inviterId } = useRouter().query as { inviterId: string };
   const { data } = useMarkdown({ url: '/intro.md' });
+
+  useEffect(() => {
+    if (inviterId) {
+      localStorage.setItem('inviterId', inviterId);
+    }
+  }, [inviterId]);
 
   return (
     <Card p={5} lineHeight={2}>
