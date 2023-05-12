7f1828d9d46773a122c96c3fcab8e696322eb96f
PaperDragon-SH <2678885646@qq.com>
2023-05-12 14:38:23 +0800

    change email template

--- src/service/utils/sendNote.ts
+++ src/service/utils/sendNote.ts
@@ -20,7 +20,106 @@ const mailTransport = nodemailer.createTransport({
 const emailMap: { [key: string]: any } = {
   [UserAuthTypeEnum.register]: {
     subject: '注册 FastGPT 账号',
-    html: (code: string) => `<div>您正在注册 FastGPT 账号，验证码为：${code}</div>`
+    html: (code: string) => `
+    <div>
+    <includetail>
+        <table style="font-family: Segoe UI, SegoeUIWF, Arial, sans-serif; font-size: 12px; color: #333333; border-spacing: 0px; border-collapse: collapse; padding: 0px; width: 580px; direction: ltr">
+            <tbody>
+            <tr>
+                <td style="font-size: 10px; padding: 0px 0px 7px 0px; text-align: right">
+                    您正在注册 FastGPT 账号，验证码为：${code} FastGPT &nbsp;GPT
+                </td>
+            </tr>
+            <tr style="background-color: #0078D4">
+                <td style="padding: 0px">
+                    <table style="font-family: Segoe UI, SegoeUIWF, Arial, sans-serif; border-spacing: 0px; border-collapse: collapse; width: 100%">
+                        <tbody>
+                        <tr>
+                            <td style="padding: 0px; width: 175px; max-width: 175px">
+                                <img src="https://omex.cdn.office.net/images/retailer.images/invite/4aa98a27d0d4978a.png">
+                            </td>
+                            <td style="padding: 0px; width: 100%">
+                            </td>
+                            <td style="padding: 0px; width: 107px; max-width: 107px">
+                                <img src="https://omex.cdn.office.net/images/retailer.images/invite/fd67eae9865f5f10.png">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size: 38px; color: #FFFFFF; padding: 12px 22px 4px 22px" colspan="3">
+                               <div>您正在注册 FastGPT 账号，验证码为：</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size: 20px; color: #FFFFFF; padding: 0px 22px 18px 22px" colspan="3">
+                              ${code}
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 30px 20px; border-bottom-style: solid; border-bottom-color: #0078D4; border-bottom-width: 4px">
+                    <table style="font-family: Segoe UI, SegoeUIWF, Arial, sans-serif; font-size: 12px; color: #333333; border-spacing: 0px; border-collapse: collapse; width: 100%">
+                        <tbody>
+                        <tr>
+                            <td style="font-size: 12px; padding: 0px 0px 5px 0px">
+                                使用此账号的人将拥有:
+                                <ul style="font-size: 14px">
+                                    <li style="padding-top: 10px">
+                                        世界上目前上最顶尖的人工智能助理。
+                                    </li>
+                                    <li>
+                                        管理安帐户的页面。
+                                    </li>
+                                    <li>
+                                        他们专属的 FastGPT 帐户，包含额外 xxx 的每日额度。
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size: 12px; padding: 0px 0px 15px 0px">
+                                希望邀请更多人? 或删除一些人?
+                                <a href="" style="color: #0044CC; text-decoration: none">转到您的帐户页面。</a>
+                                您需要首先登录到您的 xxxxx 帐户。
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size: 12px; padding: 0px;">
+                                想要了解有关  FastGPTxxx 更多信息?
+                                <a href="" style="color: #0044CC; text-decoration: none">查看 Office 常见问题。</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 35px 0px; color: #B2B2B2; font-size: 12px">
+                    Microsoft Office
+                    <br>
+                    One Microsoft Way
+                    <br>
+                    Redmond, WA
+                    <br>
+                    98052 USA
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 0px 0px 10px 0px; color: #B2B2B2; font-size: 12px">
+                    版权所有 Microsoft Corporation
+                    <br>
+                    <a href="" style="color: #0044CC">隐私声明</a>
+                    <br>
+                    <a href="" style="color: #0044CC">需要帮助? 请与支持部门联系</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+      </includetail>
+      </div>
+    `
   },
   [UserAuthTypeEnum.findPassword]: {
     subject: '修改 FastGPT 密码',
