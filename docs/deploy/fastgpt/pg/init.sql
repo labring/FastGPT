116e9c8d85dddb1d67cd5ad13d60d15dd386744e
HDS <hedongshu@foxmail.com>
2023-05-26 08:47:48 +0800

    fix: 修改了init.sql重复的bug (#62)

--- docs/deploy/fastgpt/pg/init.sql
+++ docs/deploy/fastgpt/pg/init.sql
@@ -1,9 +1,9 @@
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 
-CREATE EXTENSION vector;
+CREATE EXTENSION IF NOT EXISTS vector;
 -- init table
-CREATE TABLE modelData (
+CREATE TABLE IF NOT EXISTS modelData (
     id BIGSERIAL PRIMARY KEY,
     vector VECTOR(1536),
     status VARCHAR(50) NOT NULL,
@@ -14,8 +14,8 @@ CREATE TABLE modelData (
     a TEXT NOT NULL
 );
 -- create index
-CREATE INDEX modelData_status_index ON modelData USING HASH (status);
-CREATE INDEX modelData_userId_index ON modelData USING HASH (user_id);
-CREATE INDEX modelData_userId_index ON modelData USING HASH (model_id);
-CREATE INDEX modelData_kbId_index ON modelData USING HASH (kb_id);
+CREATE INDEX IF NOT EXISTS modelData_status_index ON modelData USING HASH (status);
+CREATE INDEX IF NOT EXISTS modelData_userId_index ON modelData USING HASH (user_id);
+CREATE INDEX IF NOT EXISTS modelData_modelId_index ON modelData USING HASH (model_id);
+CREATE INDEX IF NOT EXISTS modelData_kbId_index ON modelData USING HASH (kb_id);
 EOSQL
