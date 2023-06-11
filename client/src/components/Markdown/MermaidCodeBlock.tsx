d0c3d60751dab238f0f69e37a16fb0128f42e8ab
archer <545436317@qq.com>
2023-06-11 16:32:06 +0800

    support mermaid

--- client/src/components/Markdown/MermaidCodeBlock.tsx
+++ client/src/components/Markdown/MermaidCodeBlock.tsx
@@ -1,63 +1,99 @@
-import React, { FC, useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef, memo, useCallback, useState } from 'react';
+import { Box } from '@chakra-ui/react';
+// @ts-ignore
 import mermaid from 'mermaid';
-import { Spinner } from '@chakra-ui/react';
+import MyIcon from '../Icon';
 
-interface MermaidCodeBlockProps {
-  code: string;
-}
+import styles from './index.module.scss';
 
-const MermaidCodeBlock: FC<MermaidCodeBlockProps> = ({ code }) => {
-  const [svg, setSvg] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const codeTimeoutIdRef = useRef<number | null>(null);
+const mermaidAPI = mermaid.mermaidAPI;
+mermaidAPI.initialize({
+  startOnLoad: false,
+  theme: 'base',
+  themeVariables: {
+    fontSize: '14px',
+    primaryColor: '#d6e8ff',
+    primaryTextColor: '#485058',
+    primaryBorderColor: '#fff',
+    lineColor: '#5A646E',
+    secondaryColor: '#B5E9E5',
+    tertiaryColor: '#485058'
+  }
+});
+
+const MermaidBlock = ({ code }: { code: string }) => {
+  const dom = useRef<HTMLDivElement>(null);
+  const [svg, setSvg] = useState('');
 
   useEffect(() => {
-    if (codeTimeoutIdRef.current) {
-      clearTimeout(codeTimeoutIdRef.current);
+    try {
+      mermaidAPI.render('mermaid-svg', code, (svgCode: string) => {
+        setSvg(svgCode);
+      });
+    } catch (error) {
+      console.log(error);
     }
-
-    codeTimeoutIdRef.current = window.setTimeout(() => {
-      setLoading(true);
-
-      const mermaidAPI = (mermaid as any).mermaidAPI as any;
-      mermaidAPI.initialize({ startOnLoad: false, theme: 'forest' });
-
-      try {
-        mermaidAPI.parse(code);
-        mermaidAPI.render('mermaid-svg', code, (svgCode: string) => {
-          setSvg(svgCode);
-          setLoading(false);
-        });
-      } catch (error) {
-        console.error('Error parsing Mermaid code:', '\n', error, '\n', 'Code:', code);
-        setLoading(false);
-        return;
-      }
-    }, 1000);
   }, [code]);
 
-  useEffect(() => {
-    return () => {
-      if (codeTimeoutIdRef.current) {
-        clearTimeout(codeTimeoutIdRef.current);
-      }
+  const onclickExport = useCallback(() => {
+    const svg = dom.current?.children[0];
+    if (!svg) return;
+
+    const w = svg.clientWidth * 4;
+    const h = svg.clientHeight * 4;
+
+    const canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+    // 绘制白色背景
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, w, h);
+
+    const img = new Image();
+    img.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(dom.current.innerHTML)}`;
+
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, w, h);
+
+      const jpgDataUrl = canvas.toDataURL('image/jpeg', 1);
+      const a = document.createElement('a');
+      a.href = jpgDataUrl;
+      a.download = 'mermaid.jpg';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    };
+    img.onerror = (e) => {
+      console.log(e);
     };
   }, []);
 
   return (
-    <>
-      {loading ? (
-        <div className="loading">
-          <img src="/imgs/loading.gif" alt="Loading..." />
-        </div>
-      ) : (
-        <div
-          className="mermaid-wrapper"
-          dangerouslySetInnerHTML={svg ? { __html: svg } : undefined}
-        />
-      )}
-    </>
+    <Box position={'relative'}>
+      <Box
+        ref={dom}
+        className={styles.mermaid}
+        minH={'50px'}
+        py={4}
+        dangerouslySetInnerHTML={{ __html: svg }}
+      />
+      <MyIcon
+        name={'export'}
+        w={'20px'}
+        position={'absolute'}
+        color={'myGray.600'}
+        _hover={{
+          color: 'myBlue.700'
+        }}
+        right={0}
+        top={0}
+        cursor={'pointer'}
+        onClick={onclickExport}
+      />
+    </Box>
   );
 };
 
-export default MermaidCodeBlock;
+export default memo(MermaidBlock);
