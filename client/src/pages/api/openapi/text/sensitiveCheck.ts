36a0ea7e43be53e26cc3ce26087e08d722e1ca97
archer <545436317@qq.com>
2023-06-12 18:29:22 +0800

    fix: sensitive check

--- client/src/pages/api/openapi/text/sensitiveCheck.ts
+++ client/src/pages/api/openapi/text/sensitiveCheck.ts
@@ -2,47 +2,54 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { jsonRes } from '@/service/response';
 import { authUser, getSystemOpenAiKey } from '@/service/utils/auth';
-import type { TextPluginRequestParams } from '@/types/plugin';
 import axios from 'axios';
 import { axiosConfig } from '@/service/utils/tools';
 
+export type Props = {
+  input: string;
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    if (!global.systemEnv.sensitiveCheck) {
-      return jsonRes(res);
-    }
-
     await authUser({ req });
 
-    const { input } = req.body as TextPluginRequestParams;
+    const result = await sensitiveCheck(req.body);
 
-    const response = await axios({
-      ...axiosConfig(getSystemOpenAiKey('chat')),
-      method: 'POST',
-      url: `/moderations`,
-      data: {
-        input
-      }
+    jsonRes(res, {
+      data: result,
+      message: result
     });
-
-    const data = (response.data.results?.[0]?.category_scores as Record<string, number>) || {};
-
-    const values = Object.values(data);
-
-    for (const val of values) {
-      if (val > 0.2) {
-        return jsonRes(res, {
-          code: 500,
-          message: '您的内容不合规'
-        });
-      }
-    }
-
-    jsonRes(res);
   } catch (err) {
     jsonRes(res, {
       code: 500,
       error: err
     });
   }
+}
+
+export async function sensitiveCheck({ input }: Props) {
+  if (!global.systemEnv.sensitiveCheck) {
+    return Promise.resolve('');
+  }
+
+  const response = await axios({
+    ...axiosConfig(getSystemOpenAiKey('chat')),
+    method: 'POST',
+    url: `/moderations`,
+    data: {
+      input
+    }
+  });
+
+  const data = (response.data.results?.[0]?.category_scores as Record<string, number>) || {};
+
+  const values = Object.values(data);
+
+  for (const val of values) {
+    if (val > 0.2) {
+      return Promise.reject('您的内容不合规');
+    }
+  }
+
+  return '';
 }
