dec6f1e4e387cbd45ba4b1a92484442da9e6d9a4
archer <stakeswky@gmail.com>
2023-06-15 20:06:56 +0800

    perf: image

--- client/src/components/Markdown/Loading.tsx
+++ client/src/components/Markdown/Loading.tsx
@@ -1,17 +1,24 @@
 import React, { memo } from 'react';
 import { Box } from '@chakra-ui/react';
 
-const Loading = () => {
+const Loading = ({ text }: { text?: string }) => {
   return (
-    <Box
-      minW={'100px'}
-      w={'100%'}
-      h={'80px'}
-      backgroundImage={'url("/imgs/loading.gif")'}
-      backgroundSize={'contain'}
-      backgroundRepeat={'no-repeat'}
-      backgroundPosition={'center'}
-    />
+    <Box>
+      <Box
+        minW={'100px'}
+        w={'100%'}
+        h={'80px'}
+        backgroundImage={'url("/imgs/loading.gif")'}
+        backgroundSize={'contain'}
+        backgroundRepeat={'no-repeat'}
+        backgroundPosition={'center'}
+      />
+      {text && (
+        <Box mt={1} textAlign={'center'} fontSize={'sm'} color={'myGray.600'}>
+          {text}
+        </Box>
+      )}
+    </Box>
   );
 };
 
