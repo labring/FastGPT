f2d37c30a5ed56242da27b4a207ca7bb482f0745
archer <545436317@qq.com>
2023-06-19 17:28:25 +0800

    feat: baidu statistic

---
+++ client/public/js/baidutongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+
+(function () {
+  const hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?a5357e9dab086658bac0b6faf148882e';
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
