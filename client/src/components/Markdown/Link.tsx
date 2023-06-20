6fb312ccfd9cc9044a8d9837c4b89d1d7d2801a4
archer <545436317@qq.com>
2023-06-20 10:29:15 +0800

    link text

--- client/src/components/Markdown/Link.tsx
+++ client/src/components/Markdown/Link.tsx
@@ -3,12 +3,13 @@ import { Box } from '@chakra-ui/react';
 
 const regex = /((http|https|ftp):\/\/[^\s\u4e00-\u9fa5\u3000-\u303f\uff00-\uffef]+)/gi;
 
-const Link = ({ href }: { href?: string }) => {
-  const decText = decodeURIComponent(href || '');
+const Link = (props: { href?: string; children?: React.ReactNode[] }) => {
+  const decText = decodeURIComponent(props.href || '');
   const replaceText = decText.replace(regex, (match, p1) => {
+    const text = decText === props.children?.[0] ? p1 : props.children?.[0];
     const isInternal = /^\/#/i.test(p1);
     const target = isInternal ? '_self' : '_blank';
-    return `<a href="${p1}" target=${target}>${p1}</a>`;
+    return `<a href="${p1}" target=${target}>${text}</a>`;
   });
 
   return <Box as={'span'} dangerouslySetInnerHTML={{ __html: replaceText }} />;
