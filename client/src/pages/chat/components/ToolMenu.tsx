7f407f155837c170135d8a93a0e73af98e5c4b31
archer <stakeswky@gmail.com>
2023-07-28 15:56:50 +0800

    fix: save chat and hook

--- client/src/pages/chat/components/ToolMenu.tsx
+++ client/src/pages/chat/components/ToolMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useMemo } from 'react';
 import { useChatBox } from '@/components/ChatBox';
 import { ChatItemType } from '@/types/chat';
 import { Menu, MenuButton, MenuList, MenuItem, Box } from '@chakra-ui/react';
@@ -8,30 +8,36 @@ import { useRouter } from 'next/router';
 const ToolMenu = ({ history }: { history: ChatItemType[] }) => {
   const { onExportChat } = useChatBox();
   const router = useRouter();
-  const menuList = useRef([
-    {
-      icon: 'chat',
-      label: '新对话',
-      onClick: () => {
-        router.push({
-          query: {
-            appId: router.query?.appId
-          }
-        });
-      }
-    },
-    {
-      icon: 'apiLight',
-      label: 'HTML导出',
-      onClick: () => onExportChat({ type: 'html', history })
-    },
-    {
-      icon: 'markdown',
-      label: 'Markdown导出',
-      onClick: () => onExportChat({ type: 'md', history })
-    },
-    { icon: 'pdf', label: 'PDF导出', onClick: () => onExportChat({ type: 'pdf', history }) }
-  ]);
+  const { appId } = router.query;
+
+  const menuList = useMemo(
+    () => [
+      {
+        icon: 'chat',
+        label: '新对话',
+        onClick: () => {
+          router.push({
+            query: {
+              appId
+            }
+          });
+        }
+      },
+      {
+        icon: 'apiLight',
+        label: 'HTML导出',
+        onClick: () => onExportChat({ type: 'html', history })
+      },
+      {
+        icon: 'markdown',
+        label: 'Markdown导出',
+        onClick: () => onExportChat({ type: 'md', history })
+      },
+      { icon: 'pdf', label: 'PDF导出', onClick: () => onExportChat({ type: 'pdf', history }) }
+    ],
+    [appId, history, onExportChat, router]
+  );
+
   return history.length > 0 ? (
     <Menu autoSelect={false} isLazy>
       <MenuButton
@@ -45,7 +51,7 @@ const ToolMenu = ({ history }: { history: ChatItemType[] }) => {
         <MyIcon name={'more'} w={'14px'} p={2} />
       </MenuButton>
       <MenuList color={'myGray.700'} minW={`120px !important`} zIndex={10}>
-        {menuList.current.map((item) => (
+        {menuList.map((item) => (
           <MenuItem key={item.label} onClick={item.onClick} py={[2, 3]}>
             <MyIcon name={item.icon as any} w={['14px', '16px']} />
             <Box ml={[1, 2]}>{item.label}</Box>
