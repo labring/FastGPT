debd1202a27832c8f0dd493d3037e99893c01d1e
archer <stakeswky@gmail.com>
2023-08-04 14:00:50 +0800

    default config

--- client/src/pages/api/user/sendAuthCode.ts
+++ client/src/pages/api/user/sendAuthCode.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { jsonRes } from '@/service/response';
 import { AuthCode } from '@/service/models/authCode';
