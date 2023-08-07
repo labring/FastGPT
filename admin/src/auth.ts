e190ee92e8f78f7086d176c7ac0fd26d964df5bc
archer <545436317@qq.com>
2023-08-07 11:23:39 +0800

    perf: navbar

--- admin/src/auth.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthProvider, type AuthProvider } from 'tushan';
-
-export const authProvider: AuthProvider = createAuthProvider({
-  loginUrl: `${import.meta.env.VITE_PUBLIC_SERVER_URL}/api/login`
-});
