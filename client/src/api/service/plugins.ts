b01d7ca75b5bc3b646d9e9ed425e20bf830cc270
archer <stakeswky@gmail.com>
2023-08-23 15:00:54 +0800

    perf: plugin response

--- client/src/api/service/plugins.ts
+++ client/src/api/service/plugins.ts
@@ -1,3 +1,8 @@
 import { GET, POST } from './request';
 
-export const textCensor = (data: { text: string }) => POST('/plugins/censor/text_baidu', data);
+export const textCensor = (data: { text: string }) =>
+  POST<{ code?: number; message: string }>('/plugins/censor/text_baidu', data).then((res) => {
+    if (res?.code === 5000) {
+      return Promise.reject(res.message);
+    }
+  });
