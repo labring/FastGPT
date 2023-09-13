7d5dc1e990bcd721f3d26a0b72d59178decefae6
Joe <rxyjoey@gmail.com>
2023-09-13 18:42:40 +0800

    fix: http status code handling issue of textCensor

--- client/src/api/service/plugins.ts
+++ client/src/api/service/plugins.ts
@@ -2,7 +2,7 @@ import { GET, POST } from './request';
 
 export const textCensor = (data: { text: string }) =>
   POST<{ code?: number; message: string }>('/plugins/censor/text_baidu', data).then((res) => {
-    if (res?.code === 5000) {
+    if (res?.code === 500) {
       return Promise.reject(res.message);
     }
   });
