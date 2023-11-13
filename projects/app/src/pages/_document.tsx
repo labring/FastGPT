280171e1c78104b7055183bf13b07230110776bb
archer <545436317@qq.com>
2023-11-13 20:38:20 +0800

    perf: soe

--- projects/app/src/pages/_document.tsx
+++ projects/app/src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
+      <title>{process.env.SYSTEM_NAME || 'FastGPT'}</title>
       <Head />
       <body>
         <Main />
