c6a12c67d82fd621276ca7d6876c05e2efbff753
archer <545436317@qq.com>
2023-12-28 18:02:59 +0800

    perf: emb iframe

--- packages/global/support/wallet/bill/constants.ts
+++ packages/global/support/wallet/bill/constants.ts
@@ -6,12 +6,14 @@ export enum BillSourceEnum {
   fastgpt = 'fastgpt',
   api = 'api',
   shareLink = 'shareLink',
-  training = 'training'
+  training = 'training',
+  datasetStore = 'datasetStore'
 }
 
 export const BillSourceMap: Record<`${BillSourceEnum}`, string> = {
   [BillSourceEnum.fastgpt]: '在线使用',
   [BillSourceEnum.api]: 'Api',
   [BillSourceEnum.shareLink]: '免登录链接',
-  [BillSourceEnum.training]: '数据训练'
+  [BillSourceEnum.training]: '数据训练',
+  [BillSourceEnum.datasetStore]: '知识库存储'
 };
