f35e5140e73fbdcc8879d0f9ccabb15a18688974
archer <545436317@qq.com>
2024-01-09 10:39:51 +0800

    save chat origin ip

--- projects/app/src/pages/api/v1/chat/completions.ts
+++ projects/app/src/pages/api/v1/chat/completions.ts
@@ -69,6 +69,8 @@ export default withNextCors(async function handler(req: NextApiRequest, res: Nex
   } = req.body as Props;
 
   try {
+    const originIp = requestIp.getClientIp(req);
+
     await connectToDatabase();
     // body data check
     if (!messages) {
@@ -99,7 +101,7 @@ export default withNextCors(async function handler(req: NextApiRequest, res: Nex
       if (shareId && outLinkUid) {
         const { user, appId, authType, responseDetail, uid } = await authOutLinkChatStart({
           shareId,
-          ip: requestIp.getClientIp(req),
+          ip: originIp,
           outLinkUid,
           question: question.value
         });
@@ -245,7 +247,10 @@ export default withNextCors(async function handler(req: NextApiRequest, res: Nex
             value: answerText,
             responseData
           }
-        ]
+        ],
+        metadata: {
+          originIp
+        }
       });
     }
 
