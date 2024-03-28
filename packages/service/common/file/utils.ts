9a4b6c84bd051754c45dad168becddcac22cdc3d
Archer <stakeswky@gmail.com>
2024-03-28 10:17:28 +0800

    fix doc images (#1083)
    * perf: clear tmp files
    
    * fix doc images
    
    * update docker-compose

--- packages/service/common/file/utils.ts
+++ packages/service/common/file/utils.ts
@@ -1,4 +1,6 @@
+import { isProduction } from '../system/constants';
 import fs from 'fs';
+import path from 'path';
 
 export const removeFilesByPaths = (paths: string[]) => {
   paths.forEach((path) => {
@@ -39,6 +41,33 @@ export const clearDirFiles = (dirPath: string) => {
       clearDirFiles(curPath);
     } else {
       fs.unlinkSync(curPath);
+    }
+  });
+};
+
+export const clearTmpUploadFiles = () => {
+  if (!isProduction) return;
+  const tmpPath = '/tmp';
+
+  fs.readdir(tmpPath, (err, files) => {
+    if (err) return;
+
+    for (const file of files) {
+      if (file === 'v8-compile-cache-0') continue;
+
+      const filePath = path.join(tmpPath, file);
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) return;
+
+        // 如果文件是在1小时前上传的，则认为是临时文件并删除它
+        if (Date.now() - stats.mtime.getTime() > 1 * 60 * 60 * 1000) {
+          fs.unlink(filePath, (err) => {
+            if (err) return;
+            console.log(`Deleted temp file: ${filePath}`);
+          });
+        }
+      });
     }
   });
 };
--- packages/service/common/system/constants.ts
+++ packages/service/common/system/constants.ts
@@ -1,1 +1,3 @@
 export const FastGPTProUrl = process.env.PRO_URL ? `${process.env.PRO_URL}/api` : '';
+
+export const isProduction = process.env.NODE_ENV === 'production';
