7937f22df2231d7b1cb110baa5993b7b9b389f29
archer <545436317@qq.com>
2024-05-10 23:44:46 +0800

    file i18n

--- projects/app/src/web/context/I18n.tsx
+++ projects/app/src/web/context/I18n.tsx
@@ -6,6 +6,7 @@ type I18nContextType = {
   commonT: TFunction<['common'], undefined>;
   appT: TFunction<['app'], undefined>;
   datasetT: TFunction<['dataset'], undefined>;
+  fileT: TFunction<['file'], undefined>;
 };
 
 export const I18nContext = createContext<I18nContextType>({
@@ -17,13 +18,15 @@ const I18nContextProvider = ({ children }: { children: React.ReactNode }) => {
   const { t: commonT } = useTranslation('common');
   const { t: appT } = useTranslation('app');
   const { t: datasetT } = useTranslation('dataset');
+  const { t: fileT } = useTranslation('file');
 
   return (
     <I18nContext.Provider
       value={{
         commonT,
         appT,
-        datasetT
+        datasetT,
+        fileT
       }}
     >
       {children}
