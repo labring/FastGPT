76948e5e9eb1650481b19be5d1a6f56c2aa12669
archer <545436317@qq.com>
2024-05-28 16:43:47 +0800

    perf: chat model

--- projects/app/src/service/core/app/workflow.ts
+++ projects/app/src/service/core/app/workflow.ts
@@ -1,11 +1,10 @@
-import { FlowNodeTypeEnum } from '@fastgpt/global/core/workflow/node/constant';
+import { NodeInputKeyEnum } from '@fastgpt/global/core/workflow/constants';
 import type { StoreNodeItemType } from '@fastgpt/global/core/workflow/type/index.d';
 
-export const getChatModelNameListByModules = (modules: StoreNodeItemType[]): string[] => {
-  const chatModules = modules.filter((item) => item.flowNodeType === FlowNodeTypeEnum.chatNode);
-  return chatModules
+export const getChatModelNameListByModules = (nodes: StoreNodeItemType[]): string[] => {
+  return nodes
     .map((item) => {
-      const model = item.inputs.find((input) => input.key === 'model')?.value;
+      const model = item.inputs.find((input) => input.key === NodeInputKeyEnum.aiModel)?.value;
       return global.llmModels.find((item) => item.model === model)?.name || '';
     })
     .filter(Boolean);
