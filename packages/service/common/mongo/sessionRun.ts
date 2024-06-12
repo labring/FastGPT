fe6fcfac00b7c5b57b29038561c7103d6e5f4e57
archer <545436317@qq.com>
2024-06-12 20:20:37 +0800

    feat: abort session tip

--- packages/service/common/mongo/sessionRun.ts
+++ packages/service/common/mongo/sessionRun.ts
@@ -1,17 +1,24 @@
+import { addLog } from '../system/log';
 import { connectionMongo, ClientSession } from './index';
 
 export const mongoSessionRun = async <T = unknown>(fn: (session: ClientSession) => Promise<T>) => {
   const session = await connectionMongo.startSession();
+  let committed = false;
 
   try {
     session.startTransaction();
     const result = await fn(session);
 
     await session.commitTransaction();
+    committed = true;
 
     return result as T;
   } catch (error) {
-    await session.abortTransaction();
+    if (!committed) {
+      await session.abortTransaction();
+    } else {
+      addLog.warn('Un catch mongo session error', { error });
+    }
     return Promise.reject(error);
   } finally {
     await session.endSession();
