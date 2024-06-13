e8275a54df04b2d1bf1a6ed0b76d216c5624d547
FinleyGe <m13203533462@163.com>
2024-06-13 16:39:51 +0800

    feat: new auth collection and file method
    Signed-off-by: FinleyGe <m13203533462@163.com>

--- packages/service/support/permission/dataset/auth.ts
+++ packages/service/support/permission/dataset/auth.ts
@@ -1,20 +1,27 @@
 import { PermissionValueType } from '@fastgpt/global/support/permission/type';
 import { getResourcePermission, parseHeaderCert } from '../controller';
 import { AuthPropsType, AuthResponseType } from '../type/auth';
-import { DatasetSchemaType } from '@fastgpt/global/core/dataset/type';
+import {
+  CollectionWithDatasetType,
+  DatasetFileSchema,
+  DatasetSchemaType
+} from '@fastgpt/global/core/dataset/type';
 import { getTmbInfoByTmbId } from 'support/user/team/controller';
 import { MongoDataset } from 'core/dataset/schema';
 import { PerResourceTypeEnum } from '@fastgpt/global/support/permission/constant';
 import { DatasetErrEnum } from '@fastgpt/global/common/error/code/dataset';
 import { DatasetPermission } from '@fastgpt/global/support/permission/dataset/controller';
+import { getCollectionWithDataset } from 'core/dataset/controller';
+import { MongoDatasetCollection } from 'core/dataset/collection/schema';
+import { getFileById } from 'common/file/gridfs/controller';
+import { BucketNameEnum } from '@fastgpt/global/common/file/constants';
+import { CommonErrEnum } from '@fastgpt/global/common/error/code/common';
 
 export async function authDatasetByTmbId({
-  // teamId,
   tmbId,
   datasetId,
   per
 }: {
-  // teamId: string;
   tmbId: string;
   datasetId: string;
   per: PermissionValueType;
@@ -64,7 +71,7 @@ export async function authDataset({
 }: AuthPropsType & {
   datasetId: string;
 }): Promise<
-  AuthResponseType & {
+  AuthResponseType<DatasetPermission> & {
     dataset: DatasetSchemaType;
   }
 > {
@@ -83,4 +90,84 @@ export async function authDataset({
     dataset,
     permission: dataset.permission
   };
+}
+
+// the temporary solution for authDatasetCollection is getting the
+export async function authDatasetCollection({
+  collectionId,
+  per,
+  ...props
+}: AuthPropsType & {
+  collectionId: string;
+}): Promise<
+  AuthResponseType<DatasetPermission> & {
+    collection: CollectionWithDatasetType;
+  }
+> {
+  const { teamId, tmbId } = await parseHeaderCert(props);
+  const collection = await getCollectionWithDataset(collectionId);
+
+  if (!collection) {
+    return Promise.reject(DatasetErrEnum.unExist);
+  }
+
+  await authDatasetByTmbId({
+    tmbId,
+    datasetId: collection.datasetId._id,
+    per
+  });
+
+  return {
+    teamId,
+    tmbId,
+    collection,
+    permission: collection.datasetId.permission
+  };
+}
+
+export async function authDatasetFile({
+  fileId,
+  per,
+  ...props
+}: AuthPropsType & {
+  fileId: string;
+}): Promise<
+  AuthResponseType<DatasetPermission> & {
+    file: DatasetFileSchema;
+  }
+> {
+  const { teamId, tmbId } = await parseHeaderCert(props);
+
+  const [file, collection] = await Promise.all([
+    getFileById({ bucketName: BucketNameEnum.dataset, fileId }),
+    MongoDatasetCollection.findOne({
+      teamId,
+      fileId
+    })
+  ]);
+
+  if (!file) {
+    return Promise.reject(CommonErrEnum.fileNotFound);
+  }
+
+  if (!collection) {
+    return Promise.reject(DatasetErrEnum.unAuthDatasetFile);
+  }
+
+  try {
+    const { permission } = await authDatasetCollection({
+      ...props,
+      collectionId: collection._id,
+      per
+    });
+
+    return {
+      teamId,
+      tmbId,
+      file,
+      permission
+    };
+  } catch (error) {
+    return Promise.reject(DatasetErrEnum.unAuthDatasetFile);
+  }
 }
