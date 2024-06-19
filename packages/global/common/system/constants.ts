35fa10bae2bd7e4b5fc34a5f20686b7d7d932d13
姚瑞 <352532060@qq.com>
2024-06-19 16:39:29 +0800

    修改配置，优化打包报错问题

--- packages/global/common/system/constants.ts
+++ packages/global/common/system/constants.ts
@@ -1,3 +1,6 @@
 export const HUMAN_ICON = `/icon/human.svg`;
 export const LOGO_ICON = `/icon/logo.svg`;
 export const HUGGING_FACE_ICON = `/imgs/model/huggingface.svg`;
+
+//C:\Users\Administrator\Desktop\zgzp\FastGPT\projects\app\src\pages\login\components\LoginForm\components\FormLayout.tsx 报错，加了下面一行
+export const LOGO_ICON_PNG = `/icon/logo.svg`;
