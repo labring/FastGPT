ba81184c1836de220bedfba8021a8f3bf7c251fa
Ben Gao <bengao168@msn.com>
2024-06-26 13:07:00 +0800

    updateUserFeedback api could be authenticated by api key through bearer token as well with more flexibility

--- projects/app/src/pages/api/core/chat/feedback/updateUserFeedback.ts
+++ projects/app/src/pages/api/core/chat/feedback/updateUserFeedback.ts
@@ -25,6 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await autChatCrud({
       req,
       authToken: true,
+      authApiKey: true,
       appId,
       teamId,
       teamToken,
