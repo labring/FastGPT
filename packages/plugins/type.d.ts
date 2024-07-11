7a3ec52632b662e1a86c235beaef6c42781837f8
archer <545436317@qq.com>
2024-07-11 17:53:13 +0800

    feat: system plugin

--- packages/plugins/type.d.ts
+++ packages/plugins/type.d.ts
@@ -1,7 +1,9 @@
+import { PluginTemplateType } from '@fastgpt/global/core/plugin/type.d';
 import { SystemPluginTemplateItemType } from '@fastgpt/global/core/workflow/type';
+
 export type SystemPluginResponseType = Promise<Record<string, any>>;
 
 declare global {
-  var communitySystemPlugins: SystemPluginTemplateItemType[];
-  var communitySystemPluginCb: Record<string, (e: any) => SystemPluginResponseType>;
+  var systemPlugins: SystemPluginTemplateItemType[];
+  var systemPluginCb: Record<string, (e: any) => SystemPluginResponseType>;
 }
---
+++ packages/service/core/app/plugin/utils.ts
@@ -0,0 +1,21 @@
+import { PluginRuntimeType } from '@fastgpt/global/core/workflow/runtime/type';
+import { ChatNodeUsageType } from '@fastgpt/global/support/wallet/bill/type';
+import { splitCombinePluginId } from './controller';
+import { PluginSourceEnum } from '@fastgpt/global/core/plugin/constants';
+
+/* 
+  1. Commercial plugin: n points per times
+  2. Other plugin: sum of children points
+*/
+export const computedPluginUsage = async (
+  plugin: PluginRuntimeType,
+  childrenUsage: ChatNodeUsageType[]
+) => {
+  const { source } = await splitCombinePluginId(plugin.id);
+
+  if (source === PluginSourceEnum.commercial) {
+    return plugin.currentCost ?? 0;
+  }
+
+  return childrenUsage.reduce((sum, item) => sum + (item.totalPoints || 0), 0);
+};
