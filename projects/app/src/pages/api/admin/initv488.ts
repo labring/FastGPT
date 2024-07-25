a196e61bb130d970e3929867781bd6692a4c7f6d
archer <545436317@qq.com>
2024-07-26 00:41:48 +0800

    perf: update init

--- projects/app/src/pages/api/admin/initv488.ts
+++ projects/app/src/pages/api/admin/initv488.ts
@@ -3,6 +3,7 @@ import { jsonRes } from '@fastgpt/service/common/response';
 import { connectToDatabase } from '@/service/mongo';
 import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { MongoDataset } from '@fastgpt/service/core/dataset/schema';
+import { DatasetDefaultPermissionVal } from '@fastgpt/global/support/permission/dataset/constant';
 
 /* pg 中的数据搬到 mongo dataset.datas 中，并做映射 */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -11,10 +12,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     await authCert({ req, authRoot: true });
 
     await MongoDataset.updateMany(
-      {},
+      {
+        inheritPermission: { $exists: false }
+      },
       {
         $set: {
           inheritPermission: true
+        }
+      }
+    );
+    await MongoDataset.updateMany(
+      {
+        defaultPermission: { $exists: false }
+      },
+      {
+        $set: {
+          defaultPermission: DatasetDefaultPermissionVal
         }
       }
     );
