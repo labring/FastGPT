a32e6bd8673fdc47419ddc25d44fc7abab854271
heheer <1239331448@qq.com>
2024-08-03 10:35:48 +0800

    fix: get plugin input variables from history

--- packages/global/core/app/plugin/utils.ts
+++ packages/global/core/app/plugin/utils.ts
@@ -5,6 +5,20 @@ import { FlowNodeTypeEnum } from '../../workflow/node/constant';
 export const getPluginInputsFromStoreNodes = (nodes: StoreNodeItemType[]) => {
   return nodes.find((node) => node.flowNodeType === FlowNodeTypeEnum.pluginInput)?.inputs || [];
 };
-export const getPluginRunContent = (e: { pluginInputs: FlowNodeInputItemType[] }) => {
-  return JSON.stringify(e);
+export const getPluginRunContent = ({
+  pluginInputs,
+  variables
+}: {
+  pluginInputs: FlowNodeInputItemType[];
+  variables: Record<string, any>;
+}) => {
+  const pluginInputsWithValue = pluginInputs.map((input) => {
+    const { key } = input;
+    const value = variables?.hasOwnProperty(key) ? variables[key] : input.defaultValue;
+    return {
+      ...input,
+      value
+    };
+  });
+  return JSON.stringify(pluginInputsWithValue);
 };
