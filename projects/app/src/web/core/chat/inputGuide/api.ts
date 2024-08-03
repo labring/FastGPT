918ee39c657a1725a4701ea55809d431414e8ca9
heheer <stakeswky@gmail.com>
2024-08-03 12:29:15 +0800

    fix: add delete all input guide lexicon button (#2241)
    * fix: add delete all input guide lexicon button
    
    * fix
    
    * fix permisstion

--- projects/app/src/web/core/chat/inputGuide/api.ts
+++ projects/app/src/web/core/chat/inputGuide/api.ts
@@ -12,11 +12,12 @@ import type {
   createInputGuideResponse
 } from '@/pages/api/core/chat/inputGuide/create';
 import type { updateInputGuideBody } from '@/pages/api/core/chat/inputGuide/update';
-import type { deleteChatInputGuideQuery } from '@/pages/api/core/chat/inputGuide/delete';
+import type { deleteInputGuideBody } from '@/pages/api/core/chat/inputGuide/delete';
 import type {
   QueryChatInputGuideBody,
   QueryChatInputGuideResponse
 } from '@/pages/api/core/chat/inputGuide/query';
+import { deleteAllInputGuideBody } from '@/pages/api/core/chat/inputGuide/deleteAll';
 
 export const getCountChatInputGuideTotal = (data: countChatInputGuideTotalQuery) =>
   GET<countChatInputGuideTotalResponse>(`/core/chat/inputGuide/countTotal`, data);
@@ -39,5 +40,7 @@ export const postChatInputGuides = (data: createInputGuideBody) =>
   POST<createInputGuideResponse>(`/core/chat/inputGuide/create`, data);
 export const putChatInputGuide = (data: updateInputGuideBody) =>
   PUT(`/core/chat/inputGuide/update`, data);
-export const delChatInputGuide = (data: deleteChatInputGuideQuery) =>
+export const delChatInputGuide = (data: deleteInputGuideBody) =>
   POST(`/core/chat/inputGuide/delete`, data);
+export const delAllChatInputGuide = (data: deleteAllInputGuideBody) =>
+  POST(`/core/chat/inputGuide/deleteAll`, data);
