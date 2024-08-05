a42164fa41a1b27e056c97f982e57ee899127b1c
Patrickill <1322301665@qq.com>
2024-08-05 15:42:24 +0800

    add error info i18n

--- packages/global/common/error/code/chat.ts
+++ packages/global/common/error/code/chat.ts
@@ -1,5 +1,5 @@
 import { ErrType } from '../errorCode';
-
+import { i18nT } from '../../../../web/i18n/utils';
 /* dataset: 504000 */
 export enum ChatErrEnum {
   unAuthChat = 'unAuthChat'
@@ -7,7 +7,7 @@ export enum ChatErrEnum {
 const errList = [
   {
     statusText: ChatErrEnum.unAuthChat,
-    message: '无权操作该对话记录'
+    message: i18nT('error:chat_error.un_auth')
   }
 ];
 export default errList.reduce((acc, cur, index) => {
