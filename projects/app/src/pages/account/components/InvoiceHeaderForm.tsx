214a9464b4e751382eadf19fd2d2cb09a79b043d
Patrickill <1322301665@qq.com>
2024-08-07 20:05:33 +0800

    feat: default voice header

---
+++ projects/app/src/pages/account/components/InvoiceHeaderForm.tsx
@@ -0,0 +1,173 @@
+import Divider from '@/pages/app/detail/components/WorkflowComponents/Flow/components/Divider';
+import { getTeamInvoiceHeader, updateTeamInvoiceHeader } from '@/web/support/user/team/api';
+import { Box, Button, Flex, Input, Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import { TeamInvoiceHeaderInfoType } from '@fastgpt/global/support/user/team/type';
+import MyBox from '@fastgpt/web/components/common/MyBox';
+import { useRequest2 } from '@fastgpt/web/hooks/useRequest';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useToast } from '@fastgpt/web/hooks/useToast';
+const InputItem = ({
+  label,
+  value,
+  onChange,
+  name
+}: {
+  label: string;
+  value: string;
+  onChange: (e: any) => void;
+  name: string;
+}) => {
+  return (
+    <>
+      <Flex justify={'space-between'}>
+        <Box fontSize={'14px'} lineHeight={'2rem'}>
+          {label}
+        </Box>
+        <Input w={'21.25rem'} placeholder={label} value={value} onChange={onChange} name={name} />
+      </Flex>
+    </>
+  );
+};
+const InvoiceHeaderForm = () => {
+  const [formData, setFormData] = useState<TeamInvoiceHeaderInfoType>({
+    teamName: '',
+    unifiedCreditCode: '',
+    companyAddress: '',
+    companyPhone: '',
+    bankName: '',
+    bankAccount: '',
+    needInvoice: false,
+    emailAddress: ''
+  });
+  const { loading: isLoading } = useRequest2(() => getTeamInvoiceHeader(), {
+    manual: false,
+    onSuccess: (data) => {
+      setFormData(data);
+    }
+  });
+  const { t } = useTranslation();
+  const { toast } = useToast();
+  const handleChange = useCallback((e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+  const handleRatiosChange = useCallback((v: string) => {
+    setFormData((prev) => ({ ...prev, needInvoice: v === 'true' }));
+  }, []);
+  const isValid = useCallback((v: TeamInvoiceHeaderInfoType) => {
+    const emailRegex = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    for (const [key, value] of Object.entries(v)) {
+      if (typeof value === 'string' && value.trim() === '') {
+        return false;
+      }
+    }
+    return emailRegex.test(v.emailAddress);
+  }, []);
+  const handleSubmit = useCallback(async () => {
+    if (!isValid(formData)) {
+      toast({
+        title: t('common:support.wallet.invoice_data.in_valid'),
+        status: 'info'
+      });
+      return;
+    }
+    try {
+      await updateTeamInvoiceHeader(formData);
+      toast({
+        title: t('common:common.submit_success'),
+        status: 'success'
+      });
+    } catch (error: any) {
+      toast({
+        title: t('common:common.Submit failed'),
+        description: error?.message,
+        status: 'warning'
+      });
+    }
+  }, [formData, isValid, t, toast]);
+  return (
+    <>
+      <MyBox isLoading={isLoading} pt="3.5rem">
+        <Flex w={'100%'} justify={'center'} flexDir={'column'} align={'center'}>
+          <Flex w={'36rem'} flexDir={'column'} gap={'1rem'}>
+            <InputItem
+              label={t('common:support.wallet.invoice_data.organization_name')}
+              value={formData.teamName}
+              onChange={handleChange}
+              name="teamName"
+            />
+            <InputItem
+              label={t('common:support.wallet.invoice_data.unit_code')}
+              value={formData.unifiedCreditCode}
+              onChange={handleChange}
+              name="unifiedCreditCode"
+            />
+            <InputItem
+              label={t('common:support.wallet.invoice_data.company_address')}
+              value={formData.companyAddress}
+              onChange={handleChange}
+              name="companyAddress"
+            />
+            <InputItem
+              label={t('common:support.wallet.invoice_data.company_phone')}
+              value={formData.companyPhone}
+              onChange={handleChange}
+              name="companyPhone"
+            />
+            <InputItem
+              label={t('common:support.wallet.invoice_data.bank')}
+              value={formData.bankName}
+              onChange={handleChange}
+              name="bankName"
+            />
+            <InputItem
+              label={t('common:support.wallet.invoice_data.bank_account')}
+              value={formData.bankAccount}
+              onChange={handleChange}
+              name="bankAccount"
+            />
+            <Flex justify={'space-between'}>
+              <Box fontSize={'14px'} lineHeight={'2rem'}>
+                {t('common:support.wallet.invoice_data.need_special_invoice')}
+              </Box>
+              <RadioGroup
+                value={formData.needInvoice ? 'true' : 'false'}
+                onChange={handleRatiosChange}
+                w={'21.25rem'}
+              >
+                <Stack direction="row" h={'2rem'}>
+                  <Radio value="true" pr={'1rem'}>
+                    <Box fontSize={'14px'}>{t('common:yes')}</Box>
+                  </Radio>
+                  <Radio value="false">
+                    <Box fontSize={'14px'}>{t('common:no')}</Box>
+                  </Radio>
+                </Stack>
+              </RadioGroup>
+            </Flex>
+            <Box w={'100%'}>
+              <Divider />
+            </Box>
+            <InputItem
+              label={t('common:support.wallet.invoice_data.email')}
+              value={formData.emailAddress}
+              onChange={handleChange}
+              name="emailAddress"
+            />
+          </Flex>
+          <Flex w={'100%'} justify={'center'} mt={'3rem'}>
+            <Button variant={'primary'} px="0" onClick={handleSubmit}>
+              <Flex alignItems={'center'} px={'20px'}>
+                <Box px={'1.25rem'} py={'0.5rem'}>
+                  {t('common:common.Save')}
+                </Box>
+              </Flex>
+            </Button>
+          </Flex>
+        </Flex>
+      </MyBox>
+    </>
+  );
+};
+export default InvoiceHeaderForm;
