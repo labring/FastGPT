6a6b2164f881281fb90c887855aee6374d885cf8
Finley Ge <m13203533462@163.com>
2024-08-20 14:31:43 +0800

    feat: sso redirect page

--- projects/app/src/pages/login/sso.tsx
+++ projects/app/src/pages/login/sso.tsx
@@ -1,83 +1,45 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { useRouter } from 'next/router';
-import { useSystemStore } from '@/web/common/system/useSystemStore';
 import type { ResLogin } from '@/global/support/api/userRes.d';
 import { useChatStore } from '@/web/core/chat/context/storeChat';
 import { useUserStore } from '@/web/support/user/useUserStore';
 import { clearToken, setToken } from '@/web/support/user/auth';
 import { ssoLogin } from '@/web/support/user/api';
-import { useToast } from '@fastgpt/web/hooks/useToast';
 import Loading from '@fastgpt/web/components/common/MyLoading';
-import { getErrText } from '@fastgpt/global/common/error/utils';
 import { useTranslation } from 'next-i18next';
+import { useRequest2 } from '@fastgpt/web/hooks/useRequest';
 
 const provider = () => {
   const { t } = useTranslation();
-  const { loginStore } = useSystemStore();
   const { setLastChatId, setLastChatAppId } = useChatStore();
   const { setUserInfo } = useUserStore();
   const router = useRouter();
-  // const { code, state, error } = router.query as { code: string; state: string; error?: string };
-  const query = router.query as any;
-  const { toast } = useToast();
+  const { query } = router;
 
   const loginSuccess = useCallback(
     (res: ResLogin) => {
+      console.log('loginSuccess', res);
       setToken(res.token);
       setUserInfo(res.user);
-
       // init store
       setLastChatId('');
       setLastChatAppId('');
-
-      setTimeout(() => {
-        router.push(
-          loginStore?.lastRoute ? decodeURIComponent(loginStore?.lastRoute) : '/app/list'
-        );
-      }, 100);
+      router.push('/app/list');
     },
-    [setLastChatId, setLastChatAppId, setUserInfo, router, loginStore?.lastRoute]
+    [setLastChatId, setLastChatAppId, setUserInfo, router]
   );
 
-  const handleSSO = useCallback(
-    async (query: any) => {
-      console.debug('handleSSO');
-      if (!loginStore) {
-        router.replace('/login');
-        return;
-      }
-      try {
-        const res = await ssoLogin(query);
-
-        if (!res) {
-          toast({
-            status: 'warning',
-            title: t('common:support.user.login.error')
-          });
-          return setTimeout(() => {
-            router.replace('/login');
-          }, 1000);
-        }
-        loginSuccess(res);
-      } catch (error) {
-        toast({
-          status: 'warning',
-          title: getErrText(error, t('common:support.user.login.error'))
-        });
-        setTimeout(() => {
-          router.replace('/login');
-        }, 1000);
-      }
-    },
-    [loginStore, loginSuccess, router, t, toast]
-  );
+  const { run: handleSSO } = useRequest2(() => ssoLogin(query), {
+    onSuccess: loginSuccess,
+    errorToast: t('common:support.user.login.error')
+  });
 
   useEffect(() => {
-    if (!query) return;
-    clearToken();
-    router.prefetch('/app/list');
-    handleSSO(query);
-  }, [handleSSO, query, router]);
+    if (query && Object.keys(query).length > 0) {
+      clearToken();
+      handleSSO();
+    }
+  }, [handleSSO, query]);
 
   return <Loading />;
 };
