a4c19fbd0a9057a2b85c5e092c31c8df99449222
Archer <545436317@qq.com>
2024-08-26 01:00:11 +0800

    4810 doc (#2504)
    * 4810 doc
    
    * doc
    
    * feishu doc
    
    * 4811 文档

--- projects/app/src/pages/api/admin/initv4810.ts
+++ projects/app/src/pages/api/admin/initv4810.ts
@@ -5,6 +5,8 @@ import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { MongoDataset } from '@fastgpt/service/core/dataset/schema';
 import { DatasetDefaultPermissionVal } from '@fastgpt/global/support/permission/dataset/constant';
 import { MongoAppVersion } from '@fastgpt/service/core/app/version/schema';
+import { FastGPTProUrl } from '@fastgpt/service/common/system/constants';
+import { POST } from '@fastgpt/service/common/api/plusRequest';
 
 /* pg 中的数据搬到 mongo dataset.datas 中，并做映射 */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -20,6 +22,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
       }
     );
+
+    if (FastGPTProUrl) {
+      await POST('/admin/init/4810');
+    }
 
     jsonRes(res, {
       message: 'success'
