fd3f32d08366c9465718255825dc7c851d20ce0e
heheer <heheer@sealos.io>
2024-08-26 16:03:15 +0800

    fix welcome text rerender & add copyright (#2525)

--- packages/web/components/common/Textarea/PromptEditor/utils.ts
+++ packages/web/components/common/Textarea/PromptEditor/utils.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import type { DecoratorNode, Klass, LexicalEditor, LexicalNode } from 'lexical';
 import type { EntityMatch } from '@lexical/text';
 import { $createTextNode, $getRoot, $isTextNode, TextNode } from 'lexical';
