4db67e0137dcb9e7505fac399da238cba4968bf5
archer <545436317@qq.com>
2024-08-30 20:05:12 +0800

    fix: ts

--- projects/app/src/pages/app/detail/components/WorkflowComponents/Flow/nodes/render/RenderInput/templates/SelectApp.tsx
+++ projects/app/src/pages/app/detail/components/WorkflowComponents/Flow/nodes/render/RenderInput/templates/SelectApp.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import type { RenderInputProps } from '../type';
 import { Box, Button, useDisclosure } from '@chakra-ui/react';
-import { SelectAppItemType } from '@fastgpt/global/core/workflow/template/system/runApp/type';
+import type { SelectAppItemType } from '@fastgpt/global/core/workflow/template/system/abandoned/runApp/type';
 import Avatar from '@fastgpt/web/components/common/Avatar';
 import SelectAppModal from '../../../../SelectAppModal';
 import { useTranslation } from 'next-i18next';
