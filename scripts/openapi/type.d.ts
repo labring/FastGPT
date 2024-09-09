78ad2791cd2e21ca88ccf102543074c1ba448b06
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2024-09-09 15:43:09 +0800

    chore: openapi doc generator (#2644)
    * chore: extract the type and comment from apis
    
    * chore: template code
    
    * feat: openapi
    
    * pref: openapi generator. send into public/openapi folder

---
+++ scripts/openapi/type.d.ts
@@ -0,0 +1,22 @@
+export type ApiMetaData = {
+  name?: string;
+  author?: string;
+  version?: string;
+};
+
+export type ApiType = {
+  description?: string;
+  path: string;
+  url: string;
+  query?: itemType | itemType[];
+  body?: itemType | itemType[];
+  response?: itemType | itemType[];
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+} & ApiMetaData;
+
+export type itemType = {
+  comment?: string;
+  key?: string;
+  type: string;
+  required?: boolean;
+};
