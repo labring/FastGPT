6e7f78a97222ff3c805b1c1290831e5b3aae1cf5
archer <545436317@qq.com>
2024-09-10 16:29:05 +0800

    perf: workflow connection

--- projects/app/src/pages/app/detail/components/WorkflowComponents/Flow/nodes/render/Handle/ConnectionHandle.tsx
+++ projects/app/src/pages/app/detail/components/WorkflowComponents/Flow/nodes/render/Handle/ConnectionHandle.tsx
@@ -11,88 +11,98 @@ export const ConnectionSourceHandle = ({ nodeId }: { nodeId: string }) => {
   const nodeList = useContextSelector(WorkflowContext, (v) => v.nodeList);
   const edges = useContextSelector(WorkflowContext, (v) => v.edges);
 
-  const node = useMemo(() => nodeList.find((node) => node.nodeId === nodeId), [nodeList, nodeId]);
+  const { showSourceHandle, RightHandle, LeftHandlee, TopHandlee, BottomHandlee } = useMemo(() => {
+    const node = nodeList.find((node) => node.nodeId === nodeId);
 
-  /* not node/not connecting node, hidden */
-  const showSourceHandle = useMemo(() => {
-    if (!node) return false;
-    if (connectingEdge && connectingEdge.nodeId !== nodeId) return false;
-    return true;
-  }, [connectingEdge, node, nodeId]);
+    /* not node/not connecting node, hidden */
+    const showSourceHandle = (() => {
+      if (!node) return false;
+      if (connectingEdge && connectingEdge.nodeId !== nodeId) return false;
+      return true;
+    })();
 
-  const RightHandle = useMemo(() => {
-    const handleId = getHandleId(nodeId, 'source', Position.Right);
-    const rightTargetConnected = edges.some(
-      (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Right)
-    );
+    const RightHandle = (() => {
+      const handleId = getHandleId(nodeId, 'source', Position.Right);
+      const rightTargetConnected = edges.some(
+        (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Right)
+      );
 
-    if (!node || !node?.sourceHandle?.right || rightTargetConnected) return null;
+      if (!node || !node?.sourceHandle?.right || rightTargetConnected) return null;
 
-    return (
-      <SourceHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Right}
-        translate={[2, 0]}
-      />
-    );
-  }, [edges, node, nodeId]);
-  const LeftHandlee = useMemo(() => {
-    const leftTargetConnected = edges.some(
-      (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Left)
-    );
-    if (!node || !node?.sourceHandle?.left || leftTargetConnected) return null;
+      return (
+        <SourceHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Right}
+          translate={[2, 0]}
+        />
+      );
+    })();
+    const LeftHandlee = (() => {
+      const leftTargetConnected = edges.some(
+        (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Left)
+      );
+      if (!node || !node?.sourceHandle?.left || leftTargetConnected) return null;
 
-    const handleId = getHandleId(nodeId, 'source', Position.Left);
+      const handleId = getHandleId(nodeId, 'source', Position.Left);
 
-    return (
-      <SourceHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Left}
-        translate={[-6, 0]}
-      />
-    );
-  }, [edges, node, nodeId]);
-  const TopHandlee = useMemo(() => {
-    if (
-      edges.some(
-        (edge) => edge.target === nodeId && edge.targetHandle === NodeOutputKeyEnum.selectedTools
+      return (
+        <SourceHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Left}
+          translate={[-6, 0]}
+        />
+      );
+    })();
+    const TopHandlee = (() => {
+      if (
+        edges.some(
+          (edge) => edge.target === nodeId && edge.targetHandle === NodeOutputKeyEnum.selectedTools
+        )
       )
-    )
-      return null;
+        return null;
 
-    const handleId = getHandleId(nodeId, 'source', Position.Top);
-    const topTargetConnected = edges.some(
-      (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Top)
-    );
-    if (!node || !node?.sourceHandle?.top || topTargetConnected) return null;
+      const handleId = getHandleId(nodeId, 'source', Position.Top);
+      const topTargetConnected = edges.some(
+        (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Top)
+      );
+      if (!node || !node?.sourceHandle?.top || topTargetConnected) return null;
 
-    return (
-      <SourceHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Top}
-        translate={[0, -2]}
-      />
-    );
-  }, [edges, node, nodeId]);
-  const BottomHandlee = useMemo(() => {
-    const handleId = getHandleId(nodeId, 'source', Position.Bottom);
-    const targetConnected = edges.some(
-      (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Bottom)
-    );
-    if (!node || !node?.sourceHandle?.bottom || targetConnected) return null;
+      return (
+        <SourceHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Top}
+          translate={[0, -2]}
+        />
+      );
+    })();
+    const BottomHandlee = (() => {
+      const handleId = getHandleId(nodeId, 'source', Position.Bottom);
+      const targetConnected = edges.some(
+        (edge) => edge.targetHandle === getHandleId(nodeId, 'target', Position.Bottom)
+      );
+      if (!node || !node?.sourceHandle?.bottom || targetConnected) return null;
 
-    return (
-      <SourceHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Bottom}
-        translate={[0, 2]}
-      />
-    );
-  }, [edges, node, nodeId]);
+      return (
+        <SourceHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Bottom}
+          translate={[0, 2]}
+        />
+      );
+    })();
+
+    return {
+      showSourceHandle,
+      RightHandle,
+      LeftHandlee,
+      TopHandlee,
+      BottomHandlee
+    };
+  }, [connectingEdge, edges, nodeId, nodeList]);
 
   return showSourceHandle ? (
     <>
@@ -104,74 +114,96 @@ export const ConnectionSourceHandle = ({ nodeId }: { nodeId: string }) => {
   ) : null;
 };
 
-export const ConnectionTargetHandle = ({ nodeId }: { nodeId: string }) => {
+export const ConnectionTargetHandle = React.memo(function ConnectionTargetHandle({
+  nodeId
+}: {
+  nodeId: string;
+}) {
   const connectingEdge = useContextSelector(WorkflowContext, (ctx) => ctx.connectingEdge);
   const nodeList = useContextSelector(WorkflowContext, (v) => v.nodeList);
+  const edges = useContextSelector(WorkflowContext, (v) => v.edges);
 
-  const node = useMemo(() => nodeList.find((node) => node.nodeId === nodeId), [nodeList, nodeId]);
+  const { showHandle, LeftHandle, rightHandle, topHandle, bottomHandle } = useMemo(() => {
+    const node = nodeList.find((node) => node.nodeId === nodeId);
+    const connectingNode = nodeList.find((node) => node.nodeId === connectingEdge?.nodeId);
 
-  const showHandle = useMemo(() => {
-    if (!node) return false;
-    if (connectingEdge && connectingEdge.nodeId === nodeId) return false;
-    return true;
-  }, [connectingEdge, node, nodeId]);
+    const sourceEdges = edges.filter((edge) => edge.target === connectingNode?.nodeId);
+    const connectingNodeSourceNodeIds = sourceEdges.map((edge) => edge.source);
 
-  const LeftHandle = useMemo(() => {
-    if (!node || !node?.targetHandle?.left) return null;
+    const showHandle = (() => {
+      if (!node) return false;
+      // Unable to connect oneself
+      if (connectingEdge && connectingEdge.nodeId === nodeId) return false;
+      // Unable to connect to the source node
+      if (connectingNodeSourceNodeIds.includes(nodeId)) return false;
+      return true;
+    })();
 
-    const handleId = getHandleId(nodeId, 'target', Position.Left);
+    const LeftHandle = (() => {
+      if (!node || !node?.targetHandle?.left) return null;
 
-    return (
-      <TargetHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Left}
-        translate={[-2, 0]}
-      />
-    );
-  }, [node, nodeId]);
-  const rightHandle = useMemo(() => {
-    if (!node || !node?.targetHandle?.right) return null;
+      const handleId = getHandleId(nodeId, 'target', Position.Left);
 
-    const handleId = getHandleId(nodeId, 'target', Position.Right);
+      return (
+        <TargetHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Left}
+          translate={[-2, 0]}
+        />
+      );
+    })();
+    const rightHandle = (() => {
+      if (!node || !node?.targetHandle?.right) return null;
 
-    return (
-      <TargetHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Right}
-        translate={[2, 0]}
-      />
-    );
-  }, [node, nodeId]);
-  const topHandle = useMemo(() => {
-    if (!node || !node?.targetHandle?.top) return null;
+      const handleId = getHandleId(nodeId, 'target', Position.Right);
 
-    const handleId = getHandleId(nodeId, 'target', Position.Top);
+      return (
+        <TargetHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Right}
+          translate={[2, 0]}
+        />
+      );
+    })();
+    const topHandle = (() => {
+      if (!node || !node?.targetHandle?.top) return null;
 
-    return (
-      <TargetHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Top}
-        translate={[0, -2]}
-      />
-    );
-  }, [node, nodeId]);
-  const bottomHandle = useMemo(() => {
-    if (!node || !node?.targetHandle?.bottom) return null;
+      const handleId = getHandleId(nodeId, 'target', Position.Top);
 
-    const handleId = getHandleId(nodeId, 'target', Position.Bottom);
+      return (
+        <TargetHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Top}
+          translate={[0, -2]}
+        />
+      );
+    })();
+    const bottomHandle = (() => {
+      if (!node || !node?.targetHandle?.bottom) return null;
 
-    return (
-      <TargetHandle
-        nodeId={nodeId}
-        handleId={handleId}
-        position={Position.Bottom}
-        translate={[0, 2]}
-      />
-    );
-  }, [node, nodeId]);
+      const handleId = getHandleId(nodeId, 'target', Position.Bottom);
+
+      return (
+        <TargetHandle
+          nodeId={nodeId}
+          handleId={handleId}
+          position={Position.Bottom}
+          translate={[0, 2]}
+        />
+      );
+    })();
+
+    return {
+      showHandle,
+      LeftHandle,
+      rightHandle,
+      topHandle,
+      bottomHandle
+    };
+  }, [connectingEdge, edges, nodeId, nodeList]);
 
   return showHandle ? (
     <>
@@ -181,7 +213,7 @@ export const ConnectionTargetHandle = ({ nodeId }: { nodeId: string }) => {
       {bottomHandle}
     </>
   ) : null;
-};
+});
 
 export default function Dom() {
   return <></>;
