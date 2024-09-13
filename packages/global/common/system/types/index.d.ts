315350ef467cfd5c745ded514358effc359a0367
Patrickill <1322301665@qq.com>
2024-09-13 16:39:21 +0800

    优化代码

--- packages/global/common/system/types/index.d.ts
+++ packages/global/common/system/types/index.d.ts
@@ -34,7 +34,7 @@ export type FastGPTFeConfigsType = {
   show_openai_account?: boolean;
   show_promotion?: boolean;
   show_team_chat?: boolean;
-  show_compliance_documentation?: boolean;
+  show_compliance_copywriting?: boolean;
   concatMd?: string;
 
   docUrl?: string;
