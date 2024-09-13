8d7f2d2e7b1d92f4906165ed40d9b7a39a526708
archer <545436317@qq.com>
2024-09-13 15:58:22 +0800

    feat: sandbox strToBase64 function

--- projects/sandbox/src/sandbox/utils.ts
+++ projects/sandbox/src/sandbox/utils.ts
@@ -2,22 +2,24 @@ import { RunCodeDto, RunCodeResponse } from 'src/sandbox/dto/create-sandbox.dto'
 import IsolatedVM, { ExternalCopy, Isolate, Reference } from 'isolated-vm';
 import { countToken } from './jsFn/tiktoken';
 import { timeDelay } from './jsFn/delay';
+import { strToBase64 } from './jsFn/str2Base64';
 
 const CustomLogStr = 'CUSTOM_LOG';
 
 /* 
-    Rewrite code to add custom functions: Promise function; Log.
-  */
+  Rewrite code to add custom functions: Promise function; Log.
+*/
 function getFnCode(code: string) {
+  // rewrite log
+  code = code.replace(/console\.log/g, `${CustomLogStr}`);
+
+  // Promise function rewrite
   const rewriteSystemFn = `
     const thisDelay = (...args) => global_delay.applySyncPromise(undefined,args)
 `;
 
   // rewrite delay
   code = code.replace(/delay\((.*)\)/g, `thisDelay($1)`);
-
-  // rewrite log
-  code = code.replace(/console\.log/g, `${CustomLogStr}`);
 
   const runCode = `
     (async() => { 
@@ -35,11 +37,12 @@ function getFnCode(code: string) {
   return runCode;
 }
 
+// Register global function
 function registerSystemFn(jail: IsolatedVM.Reference<Record<string | number | symbol, any>>) {
   return Promise.all([
-    // delay
     jail.set('global_delay', new Reference(timeDelay)),
-    jail.set('countToken', countToken)
+    jail.set('countToken', countToken),
+    jail.set('strToBase64', strToBase64)
   ]);
 }
 
