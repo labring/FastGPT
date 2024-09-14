1da4f6a323a3fe53d812e8673ab2ad1f34a43d89
archer <545436317@qq.com>
2024-09-14 12:23:52 +0800

    perf: query extension prompt

--- packages/service/core/ai/functions/queryExtension.ts
+++ packages/service/core/ai/functions/queryExtension.ts
@@ -11,7 +11,10 @@ import { getLLMModel } from '../model';
     可以根据上下文，消除指代性问题以及扩展问题，利于检索。
 */
 
-const defaultPrompt = `作为一个向量检索助手，你的任务是结合历史记录，从不同角度，为“原问题”生成个不同版本的“检索词”，从而提高向量检索的语义丰富度，提高向量检索的精度。生成的问题要求指向对象清晰明确，并与“原问题语言相同”。
+const title = global.feConfigs?.systemTitle || 'FastAI';
+const defaultPrompt = `作为一个向量检索助手，你的任务是结合历史记录，从不同角度，为“原问题”生成个不同版本的“检索词”，从而提高向量检索的语义丰富度，提高向量检索的精度。
+生成的问题要求指向对象清晰明确，并与“原问题语言相同”。
+
 参考 <Example></Example> 标中的示例来完成任务。
 
 <Example>
@@ -50,49 +53,50 @@ A: 护产假的天数根据员工所在的城市而定。请提供您所在的�
 历史记录: 
 """
 Q: 作者是谁？
-A: FastGPT 的作者是 labring。
+A: ${title} 的作者是 labring。
 """
 原问题: Tell me about him
-检索词: ["Introduce labring, the author of FastGPT." ," Background information on author labring." "," Why does labring do FastGPT?"]
+检索词: ["Introduce labring, the author of ${title}." ," Background information on author labring." "," Why does labring do ${title}?"]
 ----------------
 历史记录:
 """
 Q: 对话背景。
-A: 关于 FatGPT 的介绍和使用等问题。
+A: 关于 ${title} 的介绍和使用等问题。
 """
 原问题: 你好。
 检索词: ["你好"]
 ----------------
 历史记录:
 """
-Q: FastGPT 如何收费？
-A: FastGPT 收费可以参考……
+Q: ${title} 如何收费？
+A: ${title} 收费可以参考……
 """
 原问题: 你知道 laf 么？
 检索词: ["laf 的官网地址是多少？","laf 的使用教程。","laf 有什么特点和优势。"]
 ----------------
 历史记录:
 """
-Q: FastGPT 的优势
+Q: ${title} 的优势
 A: 1. 开源
    2. 简便
    3. 扩展性强
 """
 原问题: 介绍下第2点。
-检索词: ["介绍下 FastGPT 简便的优势", "从哪些方面，可以体现出 FastGPT 的简便"]。
+检索词: ["介绍下 ${title} 简便的优势", "从哪些方面，可以体现出 ${title} 的简便"]。
 ----------------
 历史记录:
 """
-Q: 什么是 FastGPT？
-A: FastGPT 是一个 RAG 平台。
+Q: 什么是 ${title}？
+A: ${title} 是一个 RAG 平台。
 Q: 什么是 Laf？
 A: Laf 是一个云函数开发平台。
 """
 原问题: 它们有什么关系？
-检索词: ["FastGPT和Laf有什么关系？","介绍下FastGPT","介绍下Laf"]
+检索词: ["${title}和Laf有什么关系？","介绍下${title}","介绍下Laf"]
 </Example>
 
-----------------
+-----
+
 下面是正式的任务：
 
 历史记录:
