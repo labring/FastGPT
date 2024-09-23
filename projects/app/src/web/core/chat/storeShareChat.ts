f4d4d6516c4adc34f2f9cac7a91abef5c6d7d671
Archer <545436317@qq.com>
2024-09-23 15:43:57 +0800

    Support simpleApp select workflow (#2772)
    * fix: share page id error
    
    * feat: simple workflow support childApp tool
    
    * perf: aichat box animation

--- projects/app/src/web/core/chat/storeShareChat.ts
+++ projects/app/src/web/core/chat/storeShareChat.ts
@@ -1,25 +1,34 @@
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet(
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWSYZ1234567890_',
+  24
+);
 
 type State = {
   localUId: string;
-  setLocalUId: (id: string) => void;
+  loaded: boolean;
 };
 
 export const useShareChatStore = create<State>()(
   devtools(
     persist(
       immer((set, get) => ({
-        localUId: '',
-        setLocalUId(id) {
-          set((state) => {
-            state.localUId = id;
-          });
-        }
+        localUId: `shareChat-${Date.now()}-${nanoid()}`,
+        loaded: false
       })),
       {
-        name: 'shareChatStore'
+        name: 'shareChatStore',
+        onRehydrateStorage: () => (state) => {
+          if (state) {
+            state.loaded = true;
+          }
+        },
+        partialize: (state) => ({
+          localUId: state.localUId
+        })
       }
     )
   )
