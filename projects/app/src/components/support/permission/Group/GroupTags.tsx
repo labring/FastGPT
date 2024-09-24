e6c6a66ff7c22a49ee06740ae10002f096f950e0
Finley Ge <m13203533462@163.com>
2024-09-24 11:54:36 +0800

    feat(fe): group tag

---
+++ projects/app/src/components/support/permission/Group/GroupTags.tsx
@@ -0,0 +1,38 @@
+import { Flex } from '@chakra-ui/react';
+import Tag from '@fastgpt/web/components/common/Tag';
+import React from 'react';
+
+type Props = {
+  max: number;
+  names?: string[];
+};
+
+function GroupTags({ max, names }: Props) {
+  const [hover, setHover] = React.useState(false);
+  const length = names?.length || 0;
+
+  return (
+    <Flex onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} flexWrap="wrap">
+      {!hover &&
+        names?.slice(0, max).map((name, index) => (
+          <Tag key={index} colorSchema={'gray'} ml={2}>
+            {name}
+          </Tag>
+        ))}
+      {!hover && length > max && (
+        <Tag colorSchema={'gray'} ml={2}>
+          {'+' + (length - max)}
+        </Tag>
+      )}
+
+      {hover &&
+        names?.map((name, index) => (
+          <Tag key={index} colorSchema={'gray'} ml={2}>
+            {name}
+          </Tag>
+        ))}
+    </Flex>
+  );
+}
+
+export default GroupTags;
