3a18ed82ef479a1eafa5b1a191b53cf398ccbd7a
Finley Ge <m13203533462@163.com>
2024-10-15 15:36:08 +0800

    chore(test): pref mock

--- projects/app/src/pages/api/__mocks__/type.d.ts
+++ projects/app/src/pages/api/__mocks__/type.d.ts
@@ -1,4 +1,11 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
+import type { MongoMemoryReplSet, MongoMemoryServer } from 'mongodb-memory-server';
 declare global {
   var mongod: MongoMemoryServer | undefined;
+  var replSet: MongoMemoryReplSet | undefined;
 }
+
+export type RequestResponse<T = any> = {
+  code: number;
+  error?: string;
+  data?: T;
+};
