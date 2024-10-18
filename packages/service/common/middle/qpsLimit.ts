efec2329cb1050fad5ae7316de800ecdefeedecd
Finley Ge <m13203533462@163.com>
2024-10-18 17:14:38 +0800

    chore: use request-ip to get client ip

--- packages/service/common/middle/qpsLimit.ts
+++ packages/service/common/middle/qpsLimit.ts
@@ -2,13 +2,17 @@ import { CommonErrEnum } from '@fastgpt/global/common/error/code/common';
 import { TmpDataEnum } from '@fastgpt/global/support/tmpData/constant';
 import { getTmpData, setTmpData } from '../../support/tmpData/controller';
 import { ApiRequestProps } from 'type/next';
+import requestIp from 'request-ip';
 
 // unit: times/s
 // how to use?
 // export default NextAPI(useQPSLimit(10), handler); // limit 10 times per second for a ip
 export function useQPSLimit(limit: number) {
   return async (req: ApiRequestProps) => {
-    const ip = String(req.headers['x-forwarded-for']);
+    const ip = requestIp.getClientIp(req);
+    if (!ip) {
+      return;
+    }
     const data = await getTmpData({
       type: TmpDataEnum.QPSLimit,
       metadata: {
