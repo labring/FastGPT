3a44084cf08bd3e3821025ccb4d95f625644f714
archer <545436317@qq.com>
2024-10-22 12:01:06 +0800

    concat textinput to input

--- packages/service/core/workflow/dispatch/agent/runTool/index.ts
+++ packages/service/core/workflow/dispatch/agent/runTool/index.ts
@@ -152,8 +152,8 @@ export const dispatchRunTools = async (props: DispatchToolModuleProps): Promise<
   } = await (async () => {
     const adaptMessages = chats2GPTMessages({
       messages,
-      reserveId: false,
-      reserveTool: !!toolModel.toolChoice
+      reserveId: false
+      // reserveTool: !!toolModel.toolChoice
     });
 
     if (toolModel.toolChoice) {
