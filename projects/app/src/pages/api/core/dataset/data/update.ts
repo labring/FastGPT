ad776a96b9bd7c81280ddad85f0ee1030882ea05
Archer <stakeswky@gmail.com>
2024-10-29 15:44:46 +0800

    perf: dataset data auth (#3015)

--- projects/app/src/pages/api/core/dataset/data/update.ts
+++ projects/app/src/pages/api/core/dataset/data/update.ts
@@ -5,7 +5,6 @@ import { NextAPI } from '@/service/middleware/entry';
 import { WritePermissionVal } from '@fastgpt/global/support/permission/constant';
 import { authDatasetData } from '@fastgpt/service/support/permission/dataset/auth';
 import { ApiRequestProps } from '@fastgpt/service/type/next';
-import { MongoDatasetData } from '@fastgpt/service/core/dataset/data/schema';
 
 async function handler(req: ApiRequestProps<UpdateDatasetDataProps>) {
   const { dataId, q, a, indexes = [] } = req.body;
