48cd6fa2873f1271bbf6509fd4d6de8ffab45608
archer <545436317@qq.com>
2024-11-25 00:35:40 +0800

    perf: Add service entry

--- projects/app/src/service/middleware/entry.ts
+++ projects/app/src/service/middleware/entry.ts
@@ -1,6 +1,5 @@
-import { connectToDatabase } from '../mongo';
 import { NextEntry } from '@fastgpt/service/common/middle/entry';
 
 export const NextAPI = NextEntry({
-  beforeCallback: [connectToDatabase()]
+  beforeCallback: []
 });
