b73b78e389b66ae001c10f7954dcbc0c7281a75d
Archer <545436317@qq.com>
2024-11-25 10:30:51 +0800

    perf: Add service entry (#3226)

---
+++ projects/app/src/instrumentation.ts
@@ -0,0 +1,55 @@
+import { exit } from 'process';
+
+/* 
+  Init system
+*/
+export async function register() {
+  try {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+      // 基础系统初始化
+      const [
+        { connectMongo },
+        { systemStartCb },
+        { initGlobalVariables },
+        { getInitConfig },
+        { initVectorStore },
+        { initRootUser },
+        { getSystemPluginCb },
+        { startMongoWatch },
+        { startCron },
+        { startTrainingQueue }
+      ] = await Promise.all([
+        import('@fastgpt/service/common/mongo/init'),
+        import('@fastgpt/service/common/system/tools'),
+        import('@/service/common/system'),
+        import('@/service/common/system'),
+        import('@fastgpt/service/common/vectorStore/controller'),
+        import('@/service/mongo'),
+        import('@/service/core/app/plugin'),
+        import('@/service/common/system/volumnMongoWatch'),
+        import('@/service/common/system/cron'),
+        import('@/service/core/dataset/training/utils')
+      ]);
+
+      // 执行初始化流程
+      systemStartCb();
+      initGlobalVariables();
+
+      // Connect to MongoDB
+      await connectMongo();
+
+      //init system config；init vector database；init root user
+      await Promise.all([getInitConfig(), initVectorStore(), initRootUser()]);
+
+      getSystemPluginCb();
+      startMongoWatch();
+      startCron();
+      startTrainingQueue(true);
+
+      console.log('Init system success');
+    }
+  } catch (error) {
+    console.log('Init system error', error);
+    exit(1);
+  }
+}
