09e7a9d469f585d2f8af2aeec0015b35246af9bb
Patrickill <1322301665@qq.com>
2024-11-27 20:18:36 +0800

    script: 新增i18n json文件创建脚本

--- packages/web/types/i18next.d.ts
+++ packages/web/types/i18next.d.ts
@@ -1,4 +1,11 @@
 import 'i18next';
+import account_promotion from '../i18n/zh-CN/account_promotion.json';
+import account_inform from '../i18n/zh-CN/account_inform.json';
+import account_individuation from '../i18n/zh-CN/account_individuation.json';
+import account_apikey from '../i18n/zh-CN/account_apikey.json';
+import account_bill from '../i18n/zh-CN/account_bill.json';
+import account_usage from '../i18n/zh-CN/account_usage.json';
+import account_info from '../i18n/zh-CN/account_info.json';
 import common from '../i18n/zh-CN/common.json';
 import dataset from '../i18n/zh-CN/dataset.json';
 import app from '../i18n/zh-CN/app.json';
@@ -19,6 +26,13 @@ export interface I18nNamespaces {
   user: typeof user;
   chat: typeof chat;
   login: typeof login;
+  account_info: typeof account_info;
+  account_usage: typeof account_usage;
+  account_bill: typeof account_bill;
+  account_apikey: typeof account_apikey;
+  account_individuation: typeof account_individuation;
+  account_inform: typeof account_inform;
+  account_promotion: typeof account_promotion;
 }
 
 export type I18nNsType = (keyof I18nNamespaces)[];
@@ -34,7 +48,24 @@ export type I18nKeyFunction = {
 declare module 'i18next' {
   interface CustomTypeOptions {
     returnNull: false;
-    defaultNS: ['common', 'dataset', 'app', 'file', 'publish', 'workflow', 'user', 'chat', 'login'];
+    defaultNS: [
+      'common',
+      'dataset',
+      'app',
+      'file',
+      'publish',
+      'workflow',
+      'user',
+      'chat',
+      'login',
+      'account_info',
+      'account_usage',
+      'account_bill',
+      'account_apikey',
+      'account_individuation',
+      'account_inform',
+      'account_promotion'
+    ];
     resources: I18nNamespaces;
   }
 }
