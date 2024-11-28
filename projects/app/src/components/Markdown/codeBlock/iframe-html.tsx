56e4bb36979aa91fd4f0fdcaeec594eefded03f3
Archer <545436317@qq.com>
2024-11-28 17:09:18 +0800

    4.8.15 test (#3267)
    * tmp log
    
    * perf: login direct
    
    * perf: iframe html code
    
    * remove log

--- projects/app/src/components/Markdown/codeBlock/iframe-html.tsx
+++ projects/app/src/components/Markdown/codeBlock/iframe-html.tsx
@@ -3,23 +3,19 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import {
   Box,
   Flex,
-  Tooltip,
-  IconButton,
-  Button,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalBody,
   ModalHeader,
-  useDisclosure
+  useDisclosure,
+  ModalCloseButton
 } from '@chakra-ui/react';
-import CloseIcon from '@fastgpt/web/components/common/Icon/close';
 import Icon from '@fastgpt/web/components/common/Icon';
 import { useCopyData } from '@/web/common/hooks/useCopyData';
 import { useTranslation } from 'next-i18next';
 import { useMarkdownWidth } from '../hooks';
 import type { IconNameType } from '@fastgpt/web/components/common/Icon/type.d';
-import { SmallCloseIcon } from '@chakra-ui/icons';
 import { codeLight } from '../CodeLight';
 import MyTooltip from '@fastgpt/web/components/common/MyTooltip';
 
@@ -38,60 +34,50 @@ const StyledButton = ({
   viewMode: 'source' | 'iframe';
   isMobile?: boolean;
 }) => {
-  const buttonPadding = isMobile ? 1 : 4;
-  const color =
-    viewMode === 'iframe'
-      ? isActive
-        ? 'myGray.900'
-        : 'myGray.500'
-      : isActive
-        ? '#FFF'
-        : 'rgba(255, 255, 255, 0.8)';
+  const isPreview = viewMode === 'iframe';
+
+  const textColor = isPreview
+    ? isActive
+      ? 'myGray.900'
+      : 'myGray.500'
+    : isActive
+      ? '#FFF'
+      : 'rgba(255, 255, 255, 0.8)';
+  const bg = isPreview ? (isActive ? 'myGray.150' : '') : isActive ? '#333A47' : '';
+  const hoverBg = isPreview ? 'myGray.150' : '#333A47';
 
   return (
-    <Box
-      as="button"
-      bg={
-        viewMode === 'iframe'
-          ? isActive
-            ? 'myGray.100'
-            : 'rgba(251, 251, 252)'
-          : isActive
-            ? '#333A47'
-            : 'myGray.800'
-      }
-      color={color}
+    <Flex
+      bg={bg}
+      color={textColor}
       borderRadius="5px"
       boxShadow="none"
-      height={isMobile ? '22px' : '29px'}
+      fontWeight={isActive ? 500 : 400}
       _hover={{
-        bg:
-          viewMode === 'iframe'
-            ? isActive
-              ? 'myGray.100'
-              : 'myGray.50'
-            : isActive
-              ? 'myGray.600'
-              : '#424957'
+        bg: hoverBg
       }}
+      alignItems="center"
+      justifyContent="center"
       onClick={onClick}
-      padding="4px 8px"
+      cursor="pointer"
+      px={isMobile ? '6px' : '8px'}
+      h={isMobile ? '24px' : '28px'}
     >
       {isMobile ? (
         <MyTooltip label={label} placement="bottom" hasArrow>
           <Flex alignItems="center" justifyContent="center">
-            <Icon name={iconName} width="14px" height="14px" color={color} />
+            <Icon name={iconName} width="14px" height="14px" />
           </Flex>
         </MyTooltip>
       ) : (
         <Flex alignItems="center" justifyContent="flex-start">
-          <Icon name={iconName} width="14px" height="14px" color={color} />
+          <Icon name={iconName} width="14px" height="14px" />
           <Box ml={2} fontSize="sm">
             {label}
           </Box>
         </Flex>
       )}
-    </Box>
+    </Flex>
   );
 };
 
@@ -109,8 +95,12 @@ const IframeHtmlCodeBlock = ({
   const { t } = useTranslation();
   const { copyData } = useCopyData();
   const [viewMode, setViewMode] = useState<'source' | 'iframe'>('source');
+  const isPreview = viewMode === 'iframe';
+
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { width, Ref } = useMarkdownWidth();
+  const isMobile = width <= 420;
 
   const codeBoxName = useMemo(() => {
     const input = match?.['input'] || '';
@@ -119,36 +109,58 @@ const IframeHtmlCodeBlock = ({
     const splitInput = input.split('#');
     return splitInput[1] || match?.[1]?.toUpperCase();
   }, [match]);
-  const isMobile = width <= 410;
 
-  const fontSize = isMobile ? 'xs' : 'sm';
-  const gapping = isMobile ? '1px' : '5px';
+  const Iframe = useMemo(
+    () => (
+      <iframe
+        srcDoc={String(children)}
+        sandbox=""
+        referrerPolicy="no-referrer"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          background: 'white'
+        }}
+      />
+    ),
+    [children]
+  );
 
   if (codeBlock) {
     return (
       <Box
+        ref={Ref}
         my={3}
         borderRadius={'md'}
         overflow={'hidden'}
         boxShadow={
-          '0px 0px 1px 0px rgba(19, 51, 107, 0.08), 0px 1px 2px 0px rgba(19, 51, 107, 0.05)'
+          '0px 1px 2px 0px rgba(19, 51, 107, 0.05), 0px 0px 1px 0px rgba(19, 51, 107, 0.08)'
         }
       >
         <Flex
-          py={1}
+          py={2}
           px={4}
-          bg={viewMode === 'iframe' ? 'rgba(251, 251, 252)' : 'myGray.800'}
           color={'white'}
-          fontSize={fontSize}
           userSelect={'none'}
-          gap={gapping}
           alignItems="center"
+          fontSize={'sm'}
+          gap={1.5}
+          {...(isPreview
+            ? {
+                borderBottom: '1px solid',
+                borderColor: 'gray.150',
+                bg: 'myGray.25'
+              }
+            : {
+                bg: 'myGray.800'
+              })}
         >
           <Box
             flex={1}
             display="flex"
             alignItems="center"
-            color={viewMode === 'iframe' ? 'myGray.800' : 'rgba(255, 255, 255, 0.9)'}
+            color={isPreview ? 'myGray.800' : 'rgba(255, 255, 255, 0.9)'}
           >
             {codeBoxName}
             <Flex
@@ -157,7 +169,7 @@ const IframeHtmlCodeBlock = ({
               alignItems="center"
               ml={2}
             >
-              <Icon name="copy" width="14px" height="14px" />
+              <Icon name="copy" width="14px" />
             </Flex>
           </Box>
           <StyledButton
@@ -184,68 +196,42 @@ const IframeHtmlCodeBlock = ({
             isMobile={isMobile}
           />
         </Flex>
-        {viewMode === 'source' ? (
+        {isPreview ? (
+          <Box w={width} h="60vh">
+            {Iframe}
+          </Box>
+        ) : (
           <SyntaxHighlighter style={codeLight as any} language={match?.[1]} PreTag="pre">
             {String(children).replace(/&nbsp;/g, ' ')}
           </SyntaxHighlighter>
-        ) : (
-          <Box w={width} ref={Ref} h="60vh">
-            <iframe
-              srcDoc={String(children)}
-              sandbox=""
-              referrerPolicy="no-referrer"
-              style={{
-                width: '100%',
-                height: '100%',
-                border: 'none',
-                background: 'myWhite'
-              }}
-            />
-          </Box>
         )}
-        <Modal isOpen={isOpen} onClose={onClose} size="full">
-          <ModalOverlay />
-          <ModalContent h={'100vh'} display={'flex'} flexDirection={'column'}>
-            <ModalHeader
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              p={4}
-              bg="white"
-              borderBottom="1px solid"
-              borderColor="gray.300"
-              height="60px"
-            >
-              <Box fontSize="18px" color="myGray.900">
-                {t('common:common.FullScreenLight')}
-              </Box>
-              <MyTooltip label={t('common:common.Close')}>
-                <IconButton
-                  ml={4}
-                  icon={<SmallCloseIcon fontSize={'22px'} />}
-                  variant={'grayBase'}
-                  size={'smSquare'}
-                  aria-label={''}
-                  onClick={onClose}
-                  bg={'none'}
-                />
-              </MyTooltip>
-            </ModalHeader>
 
-            <ModalBody p={0} flex="1">
-              <iframe
-                srcDoc={String(children)}
-                sandbox="allow-scripts allow-forms allow-popups allow-same-origin"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  border: 'none',
-                  background: 'myWhite'
-                }}
-              />
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+        {isOpen && (
+          <Modal onClose={onClose} isOpen size={'full'}>
+            <ModalOverlay />
+            <ModalContent h={'100vh'} display={'flex'} flexDirection={'column'}>
+              <ModalHeader
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                p={4}
+                bg="white"
+                borderBottom="1px solid"
+                borderColor="gray.300"
+                height="60px"
+              >
+                <Box fontSize="lg" color="myGray.900">
+                  {t('common:common.FullScreenLight')}
+                </Box>
+                <ModalCloseButton zIndex={1} position={'relative'} top={0} right={0} />
+              </ModalHeader>
+
+              <ModalBody p={0} flex="1">
+                {Iframe}
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        )}
       </Box>
     );
   }
