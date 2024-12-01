1cef206c13013f90a0811a9c221189bdcae33eb9
Archer <545436317@qq.com>
2024-12-01 21:09:13 +0800

    Fix share page feedback auth (#3284)
    * fix: share page mark auth
    
    * perf: lang check
    
    * perf: load share

--- projects/app/src/pages/app/detail/components/Publish/Link/SelectUsingWayModal.tsx
+++ projects/app/src/pages/app/detail/components/Publish/Link/SelectUsingWayModal.tsx
@@ -50,8 +50,8 @@ const SelectUsingWayModal = ({ share, onClose }: { share: OutLinkSchema; onClose
     defaultValues: {
       usingWay: UsingWayEnum.link,
       showHistory: true,
-      scriptIconCanDrag: true,
-      scriptDefaultOpen: true,
+      scriptIconCanDrag: false,
+      scriptDefaultOpen: false,
       scriptOpenIcon:
         'data:image/svg+xml;base64,PHN2ZyB0PSIxNjkwNTMyNzg1NjY0IiBjbGFzcz0iaWNvbiIgdmlld0JveD0iMCAwIDEwMjQgMTAyNCIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHAtaWQ9IjQxMzIiIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48cGF0aCBkPSJNNTEyIDMyQzI0Ny4wNCAzMiAzMiAyMjQgMzIgNDY0QTQxMC4yNCA0MTAuMjQgMCAwIDAgMTcyLjQ4IDc2OEwxNjAgOTY1LjEyYTI1LjI4IDI1LjI4IDAgMCAwIDM5LjA0IDIyLjRsMTY4LTExMkE1MjguNjQgNTI4LjY0IDAgMCAwIDUxMiA4OTZjMjY0Ljk2IDAgNDgwLTE5MiA0ODAtNDMyUzc3Ni45NiAzMiA1MTIgMzJ6IG0yNDQuOCA0MTZsLTM2MS42IDMwMS43NmExMi40OCAxMi40OCAwIDAgMS0xOS44NC0xMi40OGw1OS4yLTIzMy45MmgtMTYwYTEyLjQ4IDEyLjQ4IDAgMCAxLTcuMzYtMjMuMzZsMzYxLjYtMzAxLjc2YTEyLjQ4IDEyLjQ4IDAgMCAxIDE5Ljg0IDEyLjQ4bC01OS4yIDIzMy45MmgxNjBhMTIuNDggMTIuNDggMCAwIDEgOCAyMi4wOHoiIGZpbGw9IiM0ZTgzZmQiIHAtaWQ9IjQxMzMiPjwvcGF0aD48L3N2Zz4=',
       scriptCloseIcon:
@@ -96,6 +96,7 @@ const SelectUsingWayModal = ({ share, onClose }: { share: OutLinkSchema; onClose
     [UsingWayEnum.script]: {
       blockTitle: t('common:core.app.outLink.Script block title'),
       code: `<script
+  type="text/javascript"
   src="${baseUrl}/js/iframe.js"
   id="chatbot-iframe" 
   data-bot-src="${linkUrl}" 
@@ -104,10 +105,7 @@ const SelectUsingWayModal = ({ share, onClose }: { share: OutLinkSchema; onClose
   data-open-icon="${getValues('scriptOpenIcon')}"
   data-close-icon="${getValues('scriptCloseIcon')}"
   defer
-></script>
-<script>
-console.log("Chat box loaded")
-</script>`
+></script>`
     }
   };
 
