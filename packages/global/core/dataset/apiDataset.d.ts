1009c141f1489d511cbf9d36d317dab3aec3a90a
heheer <1239331448@qq.com>
2024-12-13 10:17:48 +0800

    move type position

--- packages/global/core/dataset/apiDataset.d.ts
+++ packages/global/core/dataset/apiDataset.d.ts
@@ -29,46 +29,7 @@ export type feishuServer = {
   folderToken: string;
 };
 
-export type feishuFileListResponse = {
-  files: {
-    token: string;
-    parent_token: string;
-    name: string;
-    type: string;
-    modified_time: number;
-    created_time: number;
-    url: string;
-    owner_id: string;
-  }[];
-  has_more: boolean;
-  next_page_token: string;
-};
-
 export type yuqueServer = {
   userId: string;
   token: string;
 };
-
-export type yuqueRepoListResponse = {
-  id: string;
-  name: string;
-  title: string;
-  book_id: string | null;
-  type: string;
-  updated_at: Date;
-  created_at: Date;
-}[];
-
-export type yuqueTocListResponse = {
-  uuid: string;
-  type: string;
-  title: string;
-  url: string;
-  slug: string;
-  id: string;
-  doc_id: string;
-  prev_uuid: string;
-  sibling_uuid: string;
-  child_uuid: string;
-  parent_uuid: string;
-}[];
