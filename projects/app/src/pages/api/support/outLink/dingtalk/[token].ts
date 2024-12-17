f9331be8ba015fcb9734199ce9665b101809941d
a.e. <49438478+I-Info@users.noreply.github.com>
2024-12-17 19:57:02 +0800

    feat: dingtalk bot integration (#3415)
    * feat: dingtalk bot integration
    
    * docs: config dingtalk bot

---
+++ projects/app/src/pages/api/support/outLink/dingtalk/[token].ts
@@ -0,0 +1,20 @@
+import type { ApiRequestProps, ApiResponseType } from '@fastgpt/service/type/next';
+import { POST } from '@fastgpt/service/common/api/plusRequest';
+
+export type OutLinkDingtalkQuery = any;
+export type OutLinkDingtalkBody = any;
+export type OutLinkFeishuResponse = {};
+
+async function handler(
+  req: ApiRequestProps<OutLinkDingtalkBody, OutLinkDingtalkQuery>,
+  res: ApiResponseType<any>
+): Promise<void> {
+  // send to pro
+  const { token } = req.query;
+  const result = await POST<any>(`support/outLink/dingtalk/${token}`, req.body, {
+    headers: req.headers as any
+  });
+  res.json(result);
+}
+
+export default handler;
