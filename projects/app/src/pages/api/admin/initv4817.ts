7b2a4a4619aa5fd50934a7ed6be5eadc45257752
archer <545436317@qq.com>
2024-12-27 19:13:58 +0800

    perf: templates refresh

--- projects/app/src/pages/api/admin/initv4817.ts
+++ projects/app/src/pages/api/admin/initv4817.ts
@@ -1,4 +1,5 @@
 import { NextAPI } from '@/service/middleware/entry';
+import { mongoSessionRun } from '@fastgpt/service/common/mongo/sessionRun';
 import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { MongoUser } from '@fastgpt/service/support/user/schema';
 import { MongoTeam } from '@fastgpt/service/support/user/team/teamSchema';
@@ -7,17 +8,29 @@ import { NextApiRequest, NextApiResponse } from 'next';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   await authCert({ req, authRoot: true });
 
-  const users = await MongoUser.find({ openaiAccount: { $exists: true, $ne: null } });
-  for (const user of users) {
-    await MongoTeam.updateOne(
-      { ownerId: user._id },
-      {
-        $set: { openaiAccount: (user as any).openaiAccount }
-      },
-      { new: true }
-    );
+  const users = await MongoUser.find(
+    { openaiAccount: { $exists: true, $ne: null } },
+    '_id openaiAccount'
+  );
 
-    await MongoUser.updateOne({ _id: user._id }, { $unset: { openaiAccount: '' } });
+  console.log(`共 ${users.length} 个用户需要更新`);
+  let count = 0;
+  for (const user of users) {
+    await mongoSessionRun(async (session) => {
+      await MongoTeam.updateOne(
+        { ownerId: user._id },
+        {
+          $set: { openaiAccount: (user as any).openaiAccount }
+        },
+        { session }
+      );
+
+      // @ts-ignore
+      user.openaiAccount = undefined;
+      await user.save({ session });
+    });
+    count++;
+    console.log(`已更新 ${count} 个用户`);
   }
 
   return { success: true };
--- projects/app/src/service/common/system/volumnMongoWatch.ts
+++ projects/app/src/service/common/system/volumnMongoWatch.ts
@@ -4,6 +4,8 @@ import { createDatasetTrainingMongoWatch } from '@/service/core/dataset/training
 import { MongoSystemConfigs } from '@fastgpt/service/common/system/config/schema';
 import { MongoSystemPlugin } from '@fastgpt/service/core/app/plugin/systemPluginSchema';
 import { debounce } from 'lodash';
+import { MongoAppTemplate } from '@fastgpt/service/core/app/templates/templateSchema';
+import { getAppTemplatesAndLoadThem } from '@fastgpt/templates/register';
 
 export const startMongoWatch = async () => {
   reloadConfigWatch();
@@ -33,6 +35,21 @@ const refetchSystemPlugins = () => {
       setTimeout(() => {
         try {
           getSystemPluginCb(true);
+        } catch (error) {}
+      }, 5000);
+    }, 500)
+  );
+};
+
+const refetchAppTemplates = () => {
+  const changeStream = MongoAppTemplate.watch();
+
+  changeStream.on(
+    'change',
+    debounce(async (change) => {
+      setTimeout(() => {
+        try {
+          getAppTemplatesAndLoadThem(true);
         } catch (error) {}
       }, 5000);
     }, 500)
