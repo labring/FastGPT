bb669ca3ff89ffff7eba83812fa3d347e090511d
Archer <545436317@qq.com>
2025-01-06 12:43:04 +0800

    fix: plugin cost (#3533)

--- packages/service/core/app/plugin/utils.ts
+++ packages/service/core/app/plugin/utils.ts
@@ -1,11 +1,15 @@
 import { ChatNodeUsageType } from '@fastgpt/global/support/wallet/bill/type';
 import { PluginRuntimeType } from '@fastgpt/global/core/plugin/type';
+import { splitCombinePluginId } from './controller';
+import { PluginSourceEnum } from '@fastgpt/global/core/plugin/constants';
 
 /* 
   Plugin points calculation:
-  1. Return 0 if error
-  2. Add configured points if commercial plugin
-  3. Add sum of child nodes points
+  1. 商业版插件：
+    - 有错误：返回 0
+    - 无错误：返回 配置的点数 + 子节点点数
+  2. 其他插件：
+    - 返回 子节点点数
 */
 export const computedPluginUsage = async ({
   plugin,
@@ -16,13 +20,16 @@ export const computedPluginUsage = async ({
   childrenUsage: ChatNodeUsageType[];
   error?: boolean;
 }) => {
-  if (error) {
-    return 0;
+  const { source } = await splitCombinePluginId(plugin.id);
+  const childrenUsages = childrenUsage.reduce((sum, item) => sum + (item.totalPoints || 0), 0);
+
+  if (source !== PluginSourceEnum.personal) {
+    if (error) return 0;
+
+    const pluginCurrentCose = plugin.currentCost ?? 0;
+
+    return plugin.hasTokenFee ? pluginCurrentCose + childrenUsages : pluginCurrentCose;
   }
 
-  const childrenIUsages = childrenUsage.reduce((sum, item) => sum + (item.totalPoints || 0), 0);
-
-  const pluginCurrentCose = plugin.currentCost ?? 0;
-
-  return plugin.hasTokenFee ? pluginCurrentCose + childrenIUsages : pluginCurrentCose;
+  return childrenUsages;
 };
