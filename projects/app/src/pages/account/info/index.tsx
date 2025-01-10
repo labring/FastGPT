9b6517675f86e0118f94d2bc7bcacfa920c67e6c
Finley Ge <m13203533462@163.com>
2025-01-10 10:14:01 +0800

    feat: member update avatar

--- projects/app/src/pages/account/info/index.tsx
+++ projects/app/src/pages/account/info/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import {
   Box,
   Flex,
--- projects/app/src/pages/api/support/user/account/update.ts
+++ projects/app/src/pages/api/support/user/account/update.ts
@@ -1,7 +1,6 @@
 import { MongoUser } from '@fastgpt/service/support/user/schema';
 import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { UserUpdateParams } from '@/types/user';
-import { MongoTeamMember } from '@fastgpt/service/support/user/team/teamMemberSchema';
 
 /* update user info */
 import type { ApiRequestProps, ApiResponseType } from '@fastgpt/service/type/next';
@@ -9,6 +8,7 @@ import { NextAPI } from '@/service/middleware/entry';
 import { mongoSessionRun } from '@fastgpt/service/common/mongo/sessionRun';
 import { getUserDetail } from '@fastgpt/service/support/user/controller';
 import { refreshSourceAvatar } from '@fastgpt/service/common/file/image/controller';
+import { MongoTeamMember } from '@fastgpt/service/support/user/team/teamMemberSchema';
 export type UserAccountUpdateQuery = {};
 export type UserAccountUpdateBody = UserUpdateParams;
 export type UserAccountUpdateResponse = {};
@@ -23,17 +23,28 @@ async function handler(
 
   // 更新对应的记录
   await mongoSessionRun(async (session) => {
-    await MongoUser.updateOne(
-      {
-        _id: user._id
-      },
-      {
-        ...(avatar && { avatar }),
-        ...(timezone && { timezone })
-      }
-    ).session(session);
-
-    await refreshSourceAvatar(avatar, user.avatar, session);
+    if (timezone) {
+      await MongoUser.updateOne(
+        {
+          _id: user._id
+        },
+        {
+          timezone
+        }
+      ).session(session);
+    }
+    // if avatar, update team member avatar
+    if (avatar) {
+      await MongoTeamMember.updateOne(
+        {
+          _id: tmbId
+        },
+        {
+          avatar
+        }
+      ).session(session);
+      await refreshSourceAvatar(avatar, user.team.avatar, session);
+    }
   });
 
   return {};
