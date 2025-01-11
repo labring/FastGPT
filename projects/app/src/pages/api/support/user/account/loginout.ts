21f5a413e011e7de9bdf0fed85fcda279c6a48a0
Archer <stakeswky@gmail.com>
2025-01-11 17:16:17 +0800

    fix: per;fix: invite user un refresh;perf: loginout (#3566)
    * perf: loginout
    
    * fix: invite user un refresh
    
    * fix: per
    
    * fix: dockerfile
    
    * perf: docker file

--- projects/app/src/pages/api/support/user/account/loginout.ts
+++ projects/app/src/pages/api/support/user/account/loginout.ts
@@ -1,15 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { jsonRes } from '@fastgpt/service/common/response';
 import { clearCookie } from '@fastgpt/service/support/permission/controller';
+import { NextAPI } from '@/service/middleware/entry';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  try {
-    clearCookie(res);
-    jsonRes(res);
-  } catch (err) {
-    jsonRes(res, {
-      code: 500,
-      error: err
-    });
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  clearCookie(res);
 }
+
+export default NextAPI(handler);
