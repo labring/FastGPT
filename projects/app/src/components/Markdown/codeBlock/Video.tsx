d1f12b1c7642e95a0e5cbdbb63984e0a0a7611b2
archer <545436317@qq.com>
2025-01-24 19:07:08 +0800

    media cros

--- projects/app/src/components/Markdown/codeBlock/Video.tsx
+++ projects/app/src/components/Markdown/codeBlock/Video.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
 import { useMarkdownWidth } from '../hooks';
 
 const VideoBlock = ({ code: videoUrl }: { code: string }) => {
   const { width, Ref } = useMarkdownWidth();
+
+  useEffect(() => {
+    fetch(videoUrl?.trim(), {
+      mode: 'cors',
+      credentials: 'omit'
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        const video = document.getElementById('player');
+        video?.setAttribute('src', url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [videoUrl]);
+
   return (
     <Box w={width} ref={Ref}>
-      <video src={videoUrl} controls />
+      <video id="player" controls />
     </Box>
   );
 };
