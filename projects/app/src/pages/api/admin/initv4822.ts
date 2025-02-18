c42243e0b7932db8aa5ba2fa6807531c927acff7
FinleyGe <m13203533462@163.com>
2025-02-18 15:12:24 +0800

    chore(init): copy notificationAccount to user.contact

---
+++ projects/app/src/pages/api/admin/initv4822.ts
@@ -0,0 +1,23 @@
+import { NextAPI } from '@/service/middleware/entry';
+import { authCert } from '@fastgpt/service/support/permission/auth/common';
+import { MongoUser } from '@fastgpt/service/support/user/schema';
+import { MongoTeam } from '@fastgpt/service/support/user/team/teamSchema';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+/*
+ * 复制 Team 表中的 notificationAccount 到 User 表的 contact 中
+ */
+async function handler(req: NextApiRequest, _res: NextApiResponse) {
+  await authCert({ req, authRoot: true });
+  const users = await MongoUser.find();
+  const teams = await MongoTeam.find();
+  for (const user of users) {
+    const team = teams.find((team) => String(team.ownerId) === String(user._id));
+    if (team) {
+      user.contact = team.notificationAccount;
+    }
+  }
+  return { success: true };
+}
+
+export default NextAPI(handler);
