5a43fd270b76b959253519d7d7c40a25ea60caaf
I-Info <info@i1nfo.com>
2025-02-21 21:43:08 +0800

    fix: missing column vector

--- projects/app/src/pages/api/admin/inithalfvec.ts
+++ projects/app/src/pages/api/admin/inithalfvec.ts
@@ -5,19 +5,102 @@ import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { PgClient } from '@fastgpt/service/common/vectorStore/pg';
 import { DatasetVectorTableName } from '@fastgpt/service/common/vectorStore/constants';
 
+async function setHalfvec() {
+  const maxIdResult = await PgClient.query(
+    `SELECT MAX(id) as max_id FROM ${DatasetVectorTableName}`
+  );
+  const maxId: number = maxIdResult.rows[0].max_id;
+
+  if (!maxId) {
+    throw new Error('No data found');
+  }
+
+  const batchSize = 32;
+  const numBatches = Math.ceil(maxId / batchSize);
+
+  const tasks: (() => Promise<void>)[] = [];
+  let totalRowsUpdated = 0;
+  let lastLoggedTime = Date.now();
+  let lastLoggedRows = 0;
+
+  const logUpdateSpeed = () => {
+    const currentTime = Date.now();
+    const timeElapsed = (currentTime - lastLoggedTime) / 1000; // seconds
+    const rowsUpdated = totalRowsUpdated - lastLoggedRows;
+    const speed = rowsUpdated / timeElapsed; // rows per second
+    console.log(`Update speed: ${speed.toFixed(2)} rows/s`);
+    lastLoggedTime = currentTime;
+    lastLoggedRows = totalRowsUpdated;
+  };
+
+  for (let i = 0; i < numBatches; i++) {
+    const startId = i * batchSize;
+    const endId = startId + batchSize;
+
+    const asyncUpdate = async () => {
+      const rowsUpdated = await PgClient.query(
+        `
+                  UPDATE ${DatasetVectorTableName}
+                  SET halfvector = vector::halfvec(1536)
+                  WHERE id >= ${startId} AND id < ${endId} AND halfvector IS NULL;
+              `,
+        false
+      );
+      if (rowsUpdated?.rowCount) {
+        totalRowsUpdated += rowsUpdated.rowCount;
+        console.log(`Batch ${i + 1} - rowsUpdated: ${rowsUpdated.rowCount}`);
+      }
+    };
+
+    tasks.push(asyncUpdate);
+  }
+
+  let currentIdx = 0;
+  const executor = async () => {
+    console.log(`Executing tasks from: ${currentIdx}`);
+    let idx: number;
+    while ((idx = currentIdx++) < tasks.length) {
+      while (true) {
+        try {
+          await tasks[idx]();
+          break;
+        } catch (error) {
+          console.error(`Error updating halfvector in task ${idx}: `, error);
+        }
+      }
+    }
+  };
+
+  const maxConcurrency = 32;
+  const promises = [];
+  for (let i = 0; i < maxConcurrency; ++i) {
+    promises.push(executor());
+  }
+
+  const telemetryInterval = setInterval(logUpdateSpeed, 5000);
+
+  try {
+    await Promise.all(promises);
+  } finally {
+    clearInterval(telemetryInterval);
+  }
+
+  console.log('halfvector column updated');
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     await connectToDatabase();
     await authCert({ req, authRoot: true });
 
-    // pg 新建字段：halfvector
+    // pg add column halfvector
     const columnExists = await PgClient.query(`
           SELECT column_name
           FROM information_schema.columns
-          WHERE table_name='${DatasetVectorTableName}' AND column_name='halfvector';
+          WHERE table_name='${DatasetVectorTableName}';
         `);
 
-    if (columnExists.rows.length === 0) {
+    if (columnExists.rows.findIndex((item) => item.column_name === 'halfvector') === -1) {
       await PgClient.query(`
             BEGIN;
               ALTER TABLE ${DatasetVectorTableName} ADD COLUMN halfvector halfvec(1536);
@@ -26,99 +109,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       console.log('halfvector column added');
     }
 
-    const maxIdResult = await PgClient.query(
-      `SELECT MAX(id) as max_id FROM ${DatasetVectorTableName}`
-    );
-    const maxId: number = maxIdResult.rows[0].max_id;
-
-    if (!maxId) {
-      console.warn('No data in the table: empty max_id');
-      jsonRes(res, { code: 500, error: 'No data in the table: empty max_id' });
-      return;
+    if (columnExists.rows.findIndex((item) => item.column_name === 'vector') !== -1) {
+      await setHalfvec();
     }
-
-    const batchSize = 25;
-    const numBatches = Math.ceil(maxId / batchSize);
-
-    const tasks: (() => Promise<void>)[] = [];
-    let totalRowsUpdated = 0;
-    let lastLoggedTime = Date.now();
-    let lastLoggedRows = 0;
-
-    const logUpdateSpeed = () => {
-      const currentTime = Date.now();
-      const timeElapsed = (currentTime - lastLoggedTime) / 1000; // seconds
-      const rowsUpdated = totalRowsUpdated - lastLoggedRows;
-      const speed = rowsUpdated / timeElapsed; // rows per second
-      console.log(`Update speed: ${speed.toFixed(2)} rows/s`);
-      lastLoggedTime = currentTime;
-      lastLoggedRows = totalRowsUpdated;
-    };
-
-    for (let i = 0; i < numBatches; i++) {
-      const startId = i * batchSize;
-      const endId = startId + batchSize;
-
-      const asyncUpdate = async () => {
-        let retryCount = 0;
-        do {
-          try {
-            const rowsUpdated = await PgClient.query(
-              `
-                UPDATE ${DatasetVectorTableName}
-                SET halfvector = vector::halfvec(1536)
-                WHERE id >= ${startId} AND id < ${endId} AND halfvector IS NULL;
-            `,
-              false
-            );
-            if (rowsUpdated?.rowCount) {
-              totalRowsUpdated += rowsUpdated.rowCount;
-              console.log(`Batch ${i + 1} - rowsUpdated: ${rowsUpdated.rowCount}`);
-            }
-            break;
-          } catch (error) {
-            console.error(`Error updating halfvector in batch ${i + 1}:`, error);
-            retryCount++;
-          }
-        } while (retryCount < 3);
-
-        if (retryCount >= 3) {
-          console.error(`Failed to update halfvector in batch ${i + 1} after 3 retries`);
-          Promise.reject(new Error('Failed to update halfvector in batch'));
-        }
-      };
-
-      tasks.push(asyncUpdate);
-    }
-
-    let currentIdx = 0;
-    const executor = async () => {
-      console.log(`Executing tasks from: ${currentIdx}`);
-      let idx: number;
-      while ((idx = currentIdx++) < tasks.length) {
-        try {
-          await tasks[idx]();
-        } catch (error) {
-          console.error(`Error updating halfvector in task ${idx}`, error);
-        }
-      }
-    };
-
-    const maxConcurrency = 32;
-    const promises = [];
-    for (let i = 0; i < maxConcurrency; ++i) {
-      promises.push(executor());
-    }
-
-    const telemetryInterval = setInterval(logUpdateSpeed, 5000);
-
-    try {
-      await Promise.all(promises);
-    } finally {
-      clearInterval(telemetryInterval);
-    }
-
-    console.log('halfvector column updated');
 
     // 设置halfvector字段为非空
     await PgClient.query(
@@ -131,14 +124,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     );
     console.log('halfvector column set not null');
 
-    // 创建索引以提升查询性能
-    await PgClient.query(`
-      CREATE INDEX CONCURRENTLY IF NOT EXISTS halfvector_index ON ${DatasetVectorTableName} USING hnsw (halfvector halfvec_ip_ops) WITH (m = 32, ef_construction = 128);
-    `);
-    console.log('halfvector index created');
-
     // 后台释放空间，避免使用 VACUUM FULL 导致锁表。
-    await PgClient.query(`VACUUM ${DatasetVectorTableName};`);
+    PgClient.query(`VACUUM ${DatasetVectorTableName};`);
 
     jsonRes(res, {
       message: 'success'
