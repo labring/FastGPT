8cee8fade7b02bfd4ee44f3096949cdf78503cbc
gggaaallleee <91131304+gggaaallleee@users.noreply.github.com>
2025-03-27 17:37:23 +0800

    update python box (#4251)
    * update python box
    
    * Adjust the height of the NodeCode border.
    
    * update python sandbox and add test systemcall bash

--- packages/global/core/workflow/template/system/sandbox/constants.ts
+++ packages/global/core/workflow/template/system/sandbox/constants.ts
@@ -6,7 +6,7 @@ export const JS_TEMPLATE = `function main({data1, data2}){
     }
 }`;
 
-export const PY_TEMPLATE = `def main(data1, data2) -> dict:
+export const PY_TEMPLATE = `def main(data1, data2):
     return {
         "result": data1,
         "data2": data2
