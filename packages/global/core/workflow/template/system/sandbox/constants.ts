cbd85db5ba5a160d25a365267228c312422d7f6c
archer <545436317@qq.com>
2025-03-28 13:39:58 +0800

    fix: js template

--- packages/global/core/workflow/template/system/sandbox/constants.ts
+++ packages/global/core/workflow/template/system/sandbox/constants.ts
@@ -1,9 +1,9 @@
 export const JS_TEMPLATE = `function main({data1, data2}){
     
-return {
-    result: data1,
-    data2
-}
+  return {
+      result: data1,
+      data2
+  }
 }`;
 
 export const PY_TEMPLATE = `def main(data1, data2):
