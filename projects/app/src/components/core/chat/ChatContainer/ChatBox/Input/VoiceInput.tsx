284cdae7dc2058d09c7e2e6bdf3d3d844417bf45
dreamer6680 <1468683855@qq.com>
2025-04-01 18:07:42 +0800

    Update VoiceInput.tsx

--- projects/app/src/components/core/chat/ChatContainer/ChatBox/Input/VoiceInput.tsx
+++ projects/app/src/components/core/chat/ChatContainer/ChatBox/Input/VoiceInput.tsx
@@ -12,28 +12,16 @@ import { useTranslation } from 'next-i18next';
 import MyTooltip from '@fastgpt/web/components/common/MyTooltip';
 import MyIcon from '@fastgpt/web/components/common/Icon';
 import { useSystem } from '@fastgpt/web/hooks/useSystem';
-import { useFileUpload } from '../hooks/useFileUpload';
-import { ChatBoxInputFormType } from '../type';
-import { useToast } from '@fastgpt/web/hooks/useToast';
+import { useContextSelector } from 'use-context-selector';
+import { ChatBoxContext } from '../Provider';
 
 export interface VoiceInputComponentRef {
   onSpeak: () => void;
 }
 
-interface VoiceInputProps {
+type VoiceInputProps = {
   onSendMessage: (params: { text: string; files?: any[]; autoTTSResponse?: boolean }) => void;
   resetInputVal: (val: { text: string }) => void;
-  whisperConfig?: {
-    open?: boolean;
-    autoSend?: boolean;
-  };
-  autoTTSResponse?: boolean;
-  isChatting: boolean;
-  appId: string;
-  chatId: string;
-  fileSelectConfig: any;
-  fileCtrl: any;
-  outLinkAuthData: any;
   onStateChange?: (state: {
     isSpeaking: boolean;
     needSpeak: boolean;
@@ -41,9 +29,9 @@ interface VoiceInputProps {
     onWhisperRecord: () => void;
     prepareSpeak: () => void;
   }) => void;
-}
+};
 
-interface TouchListenComponentProps {
+type TouchListenComponentProps = {
   isSpeaking: boolean;
   needSpeak: boolean;
   isTransCription: boolean;
@@ -51,148 +39,142 @@ interface TouchListenComponentProps {
   stopSpeak: (param: boolean) => void;
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
   changeWaveColor: (isPrimary: boolean) => void;
-}
+};
 
-const TouchListenComponent = React.memo(
-  ({
-    isSpeaking,
-    isTransCription,
-    onWhisperRecord,
-    stopSpeak,
-    canvasRef,
-    needSpeak,
-    changeWaveColor
-  }: TouchListenComponentProps) => {
-    const { t } = useTranslation();
-    const startTimeRef = useRef(0);
-    const elapsedTimeRef = useRef(0);
-    const startYRef = useRef(0);
-    const isCancle = useRef(false);
-    const isPressing = useRef(false);
-    const moveRef = useRef(false);
+const TouchListenComponent = ({
+  isSpeaking,
+  isTransCription,
+  onWhisperRecord,
+  stopSpeak,
+  canvasRef,
+  needSpeak,
+  changeWaveColor
+}: TouchListenComponentProps) => {
+  const { t } = useTranslation();
+  const startTimeRef = useRef(0);
+  const elapsedTimeRef = useRef(0);
+  const startYRef = useRef(0);
+  const isCancle = useRef(false);
+  const isPressing = useRef(false);
+  const moveRef = useRef(false);
 
-    const handleTouchStart = useCallback(
-      (e: React.TouchEvent<HTMLDivElement>) => {
-        if (isPressing.current) return;
-        startTimeRef.current = Date.now();
-        const touch = e.touches[0] as Touch;
-        startYRef.current = touch.pageY;
+  const handleTouchStart = useCallback(
+    (e: React.TouchEvent<HTMLDivElement>) => {
+      if (isPressing.current) return;
+      startTimeRef.current = Date.now();
+      const touch = e.touches[0] as Touch;
+      startYRef.current = touch.pageY;
+      isCancle.current = false;
+      isPressing.current = true;
+      onWhisperRecord();
+    },
+    [onWhisperRecord]
+  );
+
+  const handleTouchMove = useCallback(
+    (e: React.TouchEvent<HTMLDivElement>) => {
+      const touch = e.touches[0] as Touch;
+      const currentY = touch.pageY;
+      const deltaY = startYRef.current - currentY;
+
+      if (deltaY > 90 && !isCancle.current) {
+        isCancle.current = true;
+        changeWaveColor(false);
+      } else if (deltaY <= 90 && isCancle) {
         isCancle.current = false;
-        isPressing.current = true;
-        onWhisperRecord();
-      },
-      [onWhisperRecord]
-    );
-
-    const handleTouchMove = useCallback(
-      (e: React.TouchEvent<HTMLDivElement>) => {
-        const touch = e.touches[0] as Touch;
-        const currentY = touch.pageY;
-        const deltaY = startYRef.current - currentY;
-
-        if (deltaY > 90 && !isCancle.current) {
-          isCancle.current = true;
-          changeWaveColor(false);
-        } else if (deltaY <= 90 && isCancle) {
-          isCancle.current = false;
-          changeWaveColor(true);
-        }
-      },
-      [startYRef, changeWaveColor]
-    );
-
-    const handleTouchEnd = useCallback(
-      (e: React.TouchEvent<HTMLDivElement>) => {
-        if (!isPressing.current) return;
-        const endTime = Date.now();
-        const timeDifference = endTime - startTimeRef.current;
-        elapsedTimeRef.current = timeDifference;
-        startTimeRef.current = endTime;
-        isPressing.current = false;
-        moveRef.current = false;
         changeWaveColor(true);
-        if (isCancle.current) {
+      }
+    },
+    [startYRef, changeWaveColor]
+  );
+
+  const handleTouchEnd = useCallback(
+    (e: React.TouchEvent<HTMLDivElement>) => {
+      if (!isPressing.current) return;
+      const endTime = Date.now();
+      const timeDifference = endTime - startTimeRef.current;
+      elapsedTimeRef.current = timeDifference;
+      startTimeRef.current = endTime;
+      isPressing.current = false;
+      moveRef.current = false;
+      changeWaveColor(true);
+      if (isCancle.current) {
+        stopSpeak(true);
+      } else {
+        if (timeDifference < 200) {
           stopSpeak(true);
         } else {
-          if (timeDifference < 200) {
-            stopSpeak(true);
-          } else {
-            stopSpeak(false);
-          }
+          stopSpeak(false);
         }
-      },
-      [isCancle, stopSpeak, changeWaveColor]
-    );
+      }
+    },
+    [isCancle, stopSpeak, changeWaveColor]
+  );
 
-    return (
-      <Box
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
-        onTouchStart={handleTouchStart}
-        w="100%"
-        h="100%"
-        position="relative"
+  return (
+    <Box
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      onTouchStart={handleTouchStart}
+      w="100%"
+      h="100%"
+      position="relative"
+      justifyContent="center"
+      alignItems="center"
+      userSelect="none"
+    >
+      <Flex
         justifyContent="center"
         alignItems="center"
-        userSelect="none"
+        height="100%"
+        backgroundColor={moveRef.current ? 'red' : 'white'}
+        position="absolute"
+        left={0}
+        right={0}
+        top={0}
+        bottom={0}
       >
+        <Box position="absolute" margin={0} zIndex={5}>
+          {t('chat:pressToSpeak')}
+        </Box>
+        <Box
+          as="canvas"
+          ref={canvasRef}
+          h="100%"
+          w="100%"
+          zIndex={5}
+          bg={'white'}
+          visibility={isPressing.current ? 'visible' : 'hidden'}
+        />
+      </Flex>
+      {isPressing.current && (
         <Flex
           justifyContent="center"
           alignItems="center"
           height="100%"
-          backgroundColor={moveRef.current ? 'red' : 'white'}
           position="absolute"
           left={0}
           right={0}
-          top={0}
+          top={-50}
           bottom={0}
+          background="linear-gradient(to bottom, white 0%, rgba(255, 255, 255, 0.5) 100%)"
+          zIndex={4}
         >
-          <Text position="absolute" margin={0} zIndex={5}>
-            {t('common:core.chat.pressToSpeak')}
-          </Text>
           <Box
-            as="canvas"
-            ref={canvasRef}
-            h="100%"
-            w="100%"
+            fontSize="sm"
+            fontWeight="medium"
+            color="gray.700"
+            position="relative"
             zIndex={5}
-            bg={'white'}
-            visibility={isPressing.current ? 'visible' : 'hidden'}
-          />
-        </Flex>
-        {isPressing.current && (
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            height="100%"
-            visibility={needSpeak && !isTransCription ? 'visible' : 'hidden'}
-            position="absolute"
-            left={0}
-            right={0}
-            top={-50}
-            bottom={0}
-            _after={{
-              content: '""',
-              position: 'absolute',
-              inset: 0,
-              bgGradient: 'linear(to-b, white 0%, rgba(255, 255, 255, 0.93) 100%)',
-              pointerEvents: 'none',
-              zIndex: 4
-            }}
+            bg="transparent"
           >
-            <Text fontSize="sm" fontWeight="medium" color="gray.700" position="relative">
-              {isCancle.current
-                ? t('common:core.chat.releaseToCancel')
-                : t('common:core.chat.releaseToSend')}
-            </Text>
-          </Flex>
-        )}
-      </Box>
-    );
-  }
-);
-
-TouchListenComponent.displayName = 'TouchListenComponent';
+            {isCancle.current ? t('chat:releaseToCancel') : t('chat:releaseToSend')}
+          </Box>
+        </Flex>
+      )}
+    </Box>
+  );
+};
 
 // PC voice input
 const PCVoiceInput = React.memo(
@@ -234,7 +216,6 @@ const PCVoiceInput = React.memo(
             bottom={0}
             left={0}
             right={0}
-            gap={2}
             bg="white"
             p={2}
             borderRadius="md"
@@ -242,9 +223,9 @@ const PCVoiceInput = React.memo(
             justifyContent="space-between"
             zIndex={5}
           >
-            <Text fontSize="sm" fontWeight="medium" color="gray.600" ml={1}>
+            <Box fontSize="sm" fontWeight="medium" color="gray.600" ml={1}>
               {t('common:core.chat.Speaking')}
-            </Text>
+            </Box>
             <Flex alignItems="center" gap={2}>
               <canvas
                 ref={canvasRef}
@@ -254,7 +235,7 @@ const PCVoiceInput = React.memo(
                   background: 'white'
                 }}
               />
-              <Box color={'#5A646E'} whiteSpace={'nowrap'}>
+              <Box color={'#5A646E'} whiteSpace={'nowrap'} mr={2}>
                 {speakingTimeString}
               </Box>
               <MyTooltip label={t('common:core.chat.Cancel Speak')}>
@@ -262,7 +243,6 @@ const PCVoiceInput = React.memo(
                   alignItems={'center'}
                   justifyContent={'center'}
                   flexShrink={0}
-                  mr={2}
                   h={['26px', '32px']}
                   w={['26px', '32px']}
                   borderRadius={'md'}
@@ -279,16 +259,15 @@ const PCVoiceInput = React.memo(
               </MyTooltip>
               <MyTooltip label={t('common:core.chat.Finish Speak')}>
                 <Flex
+                  mr={2}
                   alignItems={'center'}
                   justifyContent={'center'}
                   flexShrink={0}
-                  mr={0}
                   h={['26px', '32px']}
                   w={['26px', '32px']}
-                  borderRadius={'full'}
+                  borderRadius={'md'}
                   cursor={'pointer'}
-                  bg={isSpeaking ? 'primary.50' : 'gray.50'}
-                  _hover={{ bg: isSpeaking ? 'primary.100' : 'gray.100' }}
+                  _hover={{ bg: '#F5F5F8' }}
                   onClick={onWhisperRecord}
                 >
                   <MyIcon
@@ -366,7 +345,7 @@ const MobileVoiceInput = React.memo(
           zIndex={5}
         >
           {!isSpeaking && (
-            <MyTooltip label={t('common:core.chat.Back to Text')}>
+            <MyTooltip label={t('chat:Back to Text')}>
               <Flex
                 alignItems={'center'}
                 justifyContent={'center'}
@@ -393,31 +372,16 @@ const MobileVoiceInput = React.memo(
 );
 
 const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, ref) => {
-  const {
-    onSendMessage,
-    resetInputVal,
-    whisperConfig,
-    autoTTSResponse,
-    isChatting,
-    appId,
-    chatId,
-    fileSelectConfig,
-    fileCtrl,
-    outLinkAuthData,
-    onStateChange
-  } = props;
+  const { onSendMessage, resetInputVal, onStateChange } = props;
+  const outLinkAuthData = useContextSelector(ChatBoxContext, (v) => v.outLinkAuthData);
+  const appId = useContextSelector(ChatBoxContext, (v) => v.appId);
+  const chatId = useContextSelector(ChatBoxContext, (v) => v.chatId);
+  const isChatting = useContextSelector(ChatBoxContext, (v) => v.isChatting);
+  const whisperConfig = useContextSelector(ChatBoxContext, (v) => v.whisperConfig);
+  const autoTTSResponse = useContextSelector(ChatBoxContext, (v) => v.autoTTSResponse);
   const { t } = useTranslation();
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const { isPc } = useSystem();
-  const { toast } = useToast();
-
-  const { fileList, replaceFiles } = useFileUpload({
-    fileSelectConfig,
-    fileCtrl,
-    outLinkAuthData,
-    appId,
-    chatId
-  });
   const {
     isSpeaking,
     isTransCription,
@@ -428,7 +392,7 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
     renderAudioGraphMobile,
     stream,
     changeWaveColor
-  } = useSpeech({ appId, isPc, ...outLinkAuthData });
+  } = useSpeech({ appId, ...outLinkAuthData });
 
   useImperativeHandle(ref, () => ({
     onSpeak: isPc ? onWhisperRecord : prepareSpeak
@@ -440,7 +404,7 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
     return {
       needSpeak
     };
-  }, [toast, t]);
+  }, []);
 
   const finishSpeak = useCallback(() => {
     setNeedspeak(false);
@@ -456,10 +420,8 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
       if (whisperConfig?.autoSend) {
         onSendMessage({
           text,
-          files: fileList,
           autoTTSResponse
         });
-        replaceFiles([]);
       } else {
         resetInputVal({ text });
       }
@@ -472,10 +434,8 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
     startSpeak(finishWhisperTranscription);
   }, [
     autoTTSResponse,
-    fileList,
     isSpeaking,
     onSendMessage,
-    replaceFiles,
     resetInputVal,
     startSpeak,
     stopSpeak,
@@ -527,16 +487,6 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
     };
   }, [stream, canvasRef, renderAudioGraphPc, renderAudioGraphMobile, isPc]);
 
-  useEffect(() => {
-    onStateChange?.({
-      isSpeaking,
-      needSpeak,
-      isTransCription,
-      onWhisperRecord,
-      prepareSpeak
-    });
-  }, [isSpeaking, needSpeak, isTransCription, onStateChange]);
-
   const renderVoiceInput = () => {
     if (!whisperConfig?.open) return null;
     if (!(isSpeaking || needSpeak)) return null;
@@ -553,7 +503,7 @@ const VoiceInput = forwardRef<VoiceInputComponentRef, VoiceInputProps>((props, r
         zIndex={4}
         display="flex"
       >
-        {isTransCription && (
+        {isTransCription && !isChatting && (
           <Flex
             position={'absolute'}
             top={0}
