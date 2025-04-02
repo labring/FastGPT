27332743c717d9d91b4ca09f897e2574c7367144
Archer <545436317@qq.com>
2025-04-02 10:53:15 +0800

    Training status (#4424)
    * dataset data training state (#4311)
    
    * dataset data training state
    
    * fix
    
    * fix ts
    
    * fix
    
    * fix api format
    
    * fix
    
    * fix
    
    * perf: count training
    
    * format
    
    * fix: dataset training state (#4417)
    
    * fix
    
    * add test
    
    * fix
    
    * fix
    
    * fix test
    
    * fix test
    
    * perf: training count
    
    * count
    
    * loading status
    
    ---------
    
    Co-authored-by: heheer <heheer@sealos.io>

---
+++ test/cases/api/core/dataset/training/getTrainingDataDetail.test.ts
@@ -0,0 +1,59 @@
+import handler, {
+  type getTrainingDataDetailBody,
+  type getTrainingDataDetailResponse
+} from '@/pages/api/core/dataset/training/getTrainingDataDetail';
+import {
+  DatasetCollectionTypeEnum,
+  TrainingModeEnum
+} from '@fastgpt/global/core/dataset/constants';
+import { MongoDatasetCollection } from '@fastgpt/service/core/dataset/collection/schema';
+import { MongoDataset } from '@fastgpt/service/core/dataset/schema';
+import { MongoDatasetTraining } from '@fastgpt/service/core/dataset/training/schema';
+import { getRootUser } from '@test/datas/users';
+import { Call } from '@test/utils/request';
+import { describe, expect, it } from 'vitest';
+
+describe('get training data detail test', () => {
+  it('should return training data detail', async () => {
+    const root = await getRootUser();
+    const dataset = await MongoDataset.create({
+      name: 'test',
+      teamId: root.teamId,
+      tmbId: root.tmbId
+    });
+    const collection = await MongoDatasetCollection.create({
+      name: 'test',
+      type: DatasetCollectionTypeEnum.file,
+      teamId: root.teamId,
+      tmbId: root.tmbId,
+      datasetId: dataset._id
+    });
+    const trainingData = await MongoDatasetTraining.create({
+      teamId: root.teamId,
+      tmbId: root.tmbId,
+      datasetId: dataset._id,
+      collectionId: collection._id,
+      model: 'test',
+      mode: TrainingModeEnum.chunk,
+      q: 'test',
+      a: 'test'
+    });
+
+    const res = await Call<getTrainingDataDetailBody, {}, getTrainingDataDetailResponse>(handler, {
+      auth: root,
+      body: {
+        datasetId: dataset._id,
+        collectionId: collection._id,
+        dataId: trainingData._id
+      }
+    });
+
+    expect(res.code).toBe(200);
+    expect(res.data).toBeDefined();
+    expect(res.data?._id).toStrictEqual(trainingData._id);
+    expect(res.data?.datasetId).toStrictEqual(dataset._id);
+    expect(res.data?.mode).toBe(TrainingModeEnum.chunk);
+    expect(res.data?.q).toBe('test');
+    expect(res.data?.a).toBe('test');
+  });
+});
