2246e640b8c238448c586aaad43ed62722f3e14a
Archer <545436317@qq.com>
2025-04-02 22:05:42 +0800

    Redis cache (#4436)
    * perf: add Redis cache for vector counting (#4432)
    
    * feat: cache
    
    * perf: get cache key
    
    ---------
    
    Co-authored-by: a.e. <49438478+I-Info@users.noreply.github.com>

--- packages/service/common/redis/index.ts
+++ packages/service/common/redis/index.ts
@@ -1,8 +1,9 @@
+import { addLog } from '../system/log';
 import Redis from 'ioredis';
 
 const REDIS_URL = process.env.REDIS_URL ?? 'redis://localhost:6379';
 
-export function newQueueRedisConnection() {
+export const newQueueRedisConnection = () => {
   const redis = new Redis(REDIS_URL);
   redis.on('connect', () => {
     console.log('Redis connected');
@@ -11,9 +12,9 @@ export function newQueueRedisConnection() {
     console.error('Redis connection error', error);
   });
   return redis;
-}
+};
 
-export function newWorkerRedisConnection() {
+export const newWorkerRedisConnection = () => {
   const redis = new Redis(REDIS_URL, {
     maxRetriesPerRequest: null
   });
@@ -24,4 +25,19 @@ export function newWorkerRedisConnection() {
     console.error('Redis connection error', error);
   });
   return redis;
-}
+};
+
+export const getGlobalRedisCacheConnection = () => {
+  if (global.redisCache) return global.redisCache;
+
+  global.redisCache = new Redis(REDIS_URL, { keyPrefix: 'fastgpt:cache:' });
+
+  global.redisCache.on('connect', () => {
+    addLog.info('Redis connected');
+  });
+  global.redisCache.on('error', (error) => {
+    addLog.error('Redis connection error', error);
+  });
+
+  return global.redisCache;
+};
--- packages/service/common/vectorStore/controller.ts
+++ packages/service/common/vectorStore/controller.ts
@@ -2,10 +2,11 @@
 import { PgVectorCtrl } from './pg/class';
 import { ObVectorCtrl } from './oceanbase/class';
 import { getVectorsByText } from '../../core/ai/embedding';
-import { InsertVectorProps } from './controller.d';
+import { DelDatasetVectorCtrlProps, InsertVectorProps } from './controller.d';
 import { EmbeddingModelItemType } from '@fastgpt/global/core/ai/model.d';
 import { MILVUS_ADDRESS, PG_ADDRESS, OCEANBASE_ADDRESS } from './constants';
 import { MilvusCtrl } from './milvus/class';
+import { setRedisCache, getRedisCache, delRedisCache, CacheKeyEnum } from '../redis/cache';
 
 const getVectorObj = () => {
   if (PG_ADDRESS) return new PgVectorCtrl();
@@ -14,14 +15,29 @@ const getVectorObj = () => {
 
   return new PgVectorCtrl();
 };
+const getChcheKey = (teamId: string) => `${CacheKeyEnum.team_vector_count}:${teamId}`;
 
 const Vector = getVectorObj();
 
 export const initVectorStore = Vector.init;
-export const deleteDatasetDataVector = Vector.delete;
 export const recallFromVectorStore = Vector.embRecall;
 export const getVectorDataByTime = Vector.getVectorDataByTime;
-export const getVectorCountByTeamId = Vector.getVectorCountByTeamId;
+
+export const getVectorCountByTeamId = async (teamId: string) => {
+  const key = getChcheKey(teamId);
+
+  const countStr = await getRedisCache(key);
+  if (countStr) {
+    return Number(countStr);
+  }
+
+  const count = await Vector.getVectorCountByTeamId(teamId);
+
+  await setRedisCache(key, count, 30 * 60);
+
+  return count;
+};
+
 export const getVectorCountByDatasetId = Vector.getVectorCountByDatasetId;
 export const getVectorCountByCollectionId = Vector.getVectorCountByCollectionId;
 
@@ -43,8 +59,16 @@ export const insertDatasetDataVector = async ({
     vector: vectors[0]
   });
 
+  delRedisCache(getChcheKey(props.teamId));
+
   return {
     tokens,
     insertId
   };
+};
+
+export const deleteDatasetDataVector = async (props: DelDatasetVectorCtrlProps) => {
+  const result = await Vector.delete(props);
+  delRedisCache(getChcheKey(props.teamId));
+  return result;
 };
