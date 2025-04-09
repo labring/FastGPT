583d0ba41a681151df8a4dbace4c09617a3812e9
sd0ric4 <1286518974@qq.com>
2025-04-09 17:01:44 +0800

    feat: Remove the node property from ChatDispatchProps to simplify type definitions

--- packages/global/core/workflow/runtime/type.d.ts
+++ packages/global/core/workflow/runtime/type.d.ts
@@ -31,7 +31,6 @@ export type ExternalProviderType = {
 
 /* workflow props */
 export type ChatDispatchProps = {
-  node?: StoreNodeItemType;
   parentContext?: InteractiveContext;
   res?: NextApiResponse;
   requestOrigin?: string;
