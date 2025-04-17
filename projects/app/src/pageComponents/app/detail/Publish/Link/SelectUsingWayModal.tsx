ab059d105c762d75018d26f17a350888c2867b2d
dreamer6680 <1468683855@qq.com>
2025-04-17 18:56:52 +0800

    Fixed: Updated the iframe allow property in the SelectUsingWayModal component to add support for microphones. (#4583)

--- projects/app/src/pageComponents/app/detail/Publish/Link/SelectUsingWayModal.tsx
+++ projects/app/src/pageComponents/app/detail/Publish/Link/SelectUsingWayModal.tsx
@@ -90,7 +90,7 @@ const SelectUsingWayModal = ({ share, onClose }: { share: OutLinkSchema; onClose
   src="${linkUrl}"
   style="width: 100%; height: 100%;"
   frameborder="0" 
-  allow="*"
+  allow="microphone *; *"
 />`
     },
     [UsingWayEnum.script]: {
