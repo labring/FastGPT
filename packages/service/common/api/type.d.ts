84701af3093c53cfef6916f54c8cf4795d24f6da
Archer <stakeswky@gmail.com>
2025-04-18 00:34:15 +0800

    update package version (#4586)

--- packages/service/common/api/type.d.ts
+++ packages/service/common/api/type.d.ts
@@ -15,8 +15,8 @@ declare global {
   var textCensorHandler: (params: { text: string }) => Promise<{ code: number; message?: string }>;
   var deepRagHandler: (data: DeepRagSearchProps) => Promise<SearchDatasetDataResponse>;
   var authOpenApiHandler: (data: AuthOpenApiLimitProps) => Promise<any>;
-  var createUsageHandler: (data: CreateUsageProps) => Promise<void>;
-  var concatUsageHandler: (data: ConcatUsageProps) => Promise<void>;
+  var createUsageHandler: (data: CreateUsageProps) => any;
+  var concatUsageHandler: (data: ConcatUsageProps) => any;
 
   // API dataset
   var getProApiDatasetFileList: (data: GetProApiDatasetFileListParams) => Promise<APIFileItem[]>;
