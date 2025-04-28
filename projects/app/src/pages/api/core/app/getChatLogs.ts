b2923d048a73f94d37d6067b11d851ffb1419334
archer <545436317@qq.com>
2025-04-28 18:16:38 +0800

    remove log

--- projects/app/src/pages/api/core/app/getChatLogs.ts
+++ projects/app/src/pages/api/core/app/getChatLogs.ts
@@ -132,7 +132,7 @@ async function handler(
     ),
     MongoChat.countDocuments(where, { ...readFromSecondary })
   ]);
-  console.log(list);
+
   const listWithSourceMember = await addSourceMember({
     list: list
   });
