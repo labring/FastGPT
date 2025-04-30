1ed178c5f041eadf35f61e7fcfdf5db011a0c62b
FinleyGe <m13203533462@163.com>
2025-04-30 10:08:27 +0800

    refactor: templates

--- projects/tools/README.md
+++ projects/tools/README.md
@@ -1,15 +1,42 @@
 # fastgpt-tools
 
-To install dependencies:
+FastGPT 系统工具服务
 
-```bash
-bun install
-```
+## 架构说明
 
-To run:
+- `dev` 调试工具时可以使用的 Web 工具
+- `runtime` 工具的运行时
+- `scripts` 脚本
+- `test` 测试配置
+- `tools` 系统工具目录
 
-```bash
-bun run index.ts
-```
+系统工具可以分为两种：
+1. 系统工具
+2. 工具集，工具集内包括功能相似的多个工具
 
-This project was created using `bun init` in bun v1.2.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+## 部署
+
+### 编译
+1. `bun i`
+2. `bun run build`
+
+### 通过 docker 部署
+
+<!-- TODO -->
+
+## 贡献指南
+
+### 贡献社区插件
+
+1. 安装依赖: `bun i`
+2. 创建新的工具/工具集 `bun run new tool <name>` 或 `bun run new folder <name>`
+3. `cd tools/<name>`
+4. 修改配置文件 `config.ts`
+5. 在 `src` 目录下实现工具逻辑
+6. 编写测试样例并通过测试
+7. 提交代码并发起 PR 到 labring/FastGPT
+
+### 私有化插件
+
+按照上述步骤开发完毕后，执行 `bun run build` 将构建执行工具构建。
+请参考**部署**中的步骤进行部署。
--- projects/tools/scripts/new.ts
+++ projects/tools/scripts/new.ts
@@ -5,66 +5,72 @@ const program = new Command();
 
 program
   .name('new')
-  .description('Create a new tool')
-  .argument('<name>', 'Tool name')
-  .action(async (name) => {
-    console.log('Creating tool', name);
-    // name validation:
-    // 1. less than 20 characters
-    // 2. kebab-case
-
-    if (name.length > 20) {
-      console.error('Tool name must be less than 20 characters');
-      process.exit(1);
-    }
-
-    if (!/^[a-z0-9-]+$/.test(name)) {
-      console.error('Tool name must be kebab-case');
-      process.exit(1);
-    }
-    // copy template to tools/<name>
-
-    const templateDir = path.join('scripts', 'template');
-    const toolDir = path.join('tools', name);
-
-    if (!fs.existsSync(toolDir)) {
-      fs.mkdirSync(toolDir, { recursive: true });
-    }
-
-    const copyTemplate = (src: string, dest: string) => {
-      const stat = fs.statSync(src);
-      if (stat.isDirectory()) {
-        fs.mkdirSync(dest, { recursive: true });
-        fs.readdirSync(src).forEach((file) => {
-          copyTemplate(path.join(src, file), path.join(dest, file));
-        });
-      } else {
-        fs.copyFileSync(src, dest);
-      }
-    };
-
-    copyTemplate(templateDir, toolDir);
-
-    // update package.json
-    const packageJsonPath = path.join(toolDir, 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
-    packageJson.name = `fastgpt-tools-${name}`;
-    packageJson.module = 'index.ts';
-    packageJson.type = 'module';
-    packageJson.devDependencies = {
-      '@types/bun': 'latest'
-    };
-    packageJson.peerDependencies = {
-      typescript: '^5.0.0'
-    };
-    packageJson.dependencies = {};
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-    // output success message
-    console.log('Tool created successfully! 🎉');
-    console.log('Next steps:');
-    console.log('- cd tools/' + name);
-    console.log('- bun i');
-  });
+  .description('Create a new tool or folder')
+  .option('-f --folder', 'Create a folder')
+  .argument('<name>', 'name');
 
 program.parse();
+
+const isFolder = program.opts().folder as boolean;
+const name = program.args[0];
+
+// name validation:
+// 1. less than 20 characters
+// 2. kebab-case
+if (name.length > 20) {
+  console.error('Tool name must be less than 20 characters');
+  process.exit(1);
+}
+
+if (!/^[a-z0-9-]+$/.test(name)) {
+  console.error('Tool name must be kebab-case');
+  process.exit(1);
+}
+
+console.log('Creating tool', name);
+// copy template to tools/<name>
+
+const templateDir = isFolder
+  ? path.join('scripts', 'template', 'folder')
+  : path.join('scripts', 'template', 'tool');
+
+const toolDir = path.join('tools', name);
+
+if (!fs.existsSync(toolDir)) {
+  fs.mkdirSync(toolDir, { recursive: true });
+}
+
+const copyTemplate = (src: string, dest: string) => {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory()) {
+    fs.mkdirSync(dest, { recursive: true });
+    fs.readdirSync(src).forEach((file) => {
+      copyTemplate(path.join(src, file), path.join(dest, file));
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+};
+
+copyTemplate(templateDir, toolDir);
+
+// update package.json
+const packageJsonPath = path.join(toolDir, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+packageJson.name = `fastgpt-tools-${name}`;
+packageJson.module = 'index.ts';
+packageJson.type = 'module';
+packageJson.devDependencies = {
+  '@types/bun': 'latest'
+};
+packageJson.peerDependencies = {
+  typescript: '^5.0.0'
+};
+packageJson.dependencies = {};
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+// output success message
+console.log('Tool created successfully! 🎉');
+console.log('Next steps:');
+console.log('- cd tools/' + name);
+console.log('- bun i');
