3e20f8312f7da8bd0a15f3ead1ef37a99c098346
FinleyGe <m13203533462@163.com>
2025-04-30 15:03:21 +0800

    refactor: migrate delay

---
+++ projects/tools/tools/delay/config.ts
@@ -0,0 +1,25 @@
+import { defineTool, FlowNodeInputTypeEnum, WorkflowIOValueTypeEnum } from '@/type';
+
+export default defineTool({
+  version: '4.9.7',
+  type: 'tools',
+  name: {
+    'zh-CN': '流程等待',
+    en: 'Delay'
+  },
+  description: {
+    'zh-CN': '让工作流等待指定时间后运行"',
+    en: 'Delay the workflow after a specified time'
+  },
+  icon: 'core/workflow/template/sleep',
+  inputs: [
+    {
+      key: 'ms',
+      label: '延迟时长',
+      renderTypeList: [FlowNodeInputTypeEnum.input, FlowNodeInputTypeEnum.reference],
+      valueType: WorkflowIOValueTypeEnum.number,
+      toolDescription: '要暂停的时间，单位毫秒'
+    }
+  ],
+  outputs: []
+});
