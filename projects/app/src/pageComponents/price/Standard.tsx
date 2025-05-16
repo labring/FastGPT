714d222ad8877d9566868bc40e6217aa06b57900
archer <545436317@qq.com>
2025-05-16 10:17:22 +0800

    perf: i18n

--- projects/app/src/pageComponents/price/Standard.tsx
+++ projects/app/src/pageComponents/price/Standard.tsx
@@ -160,7 +160,7 @@ const Standard = ({
                 <Box fontSize={['32px', '42px']} fontWeight={'bold'} color={'myGray.900'}>
                   ￥{item.price}
                 </Box>
-                <Box color={'myGray.500'} h={'40px'} fontSize={'xs'}>
+                <Box color={'myGray.500'} minH={'40px'} fontSize={'xs'}>
                   {t(item.desc as any, { title: feConfigs?.systemTitle })}
                 </Box>
 
@@ -183,17 +183,6 @@ const Standard = ({
                       </Button>
                     );
                   }
-                  // feature:
-                  // if (
-                  //   item.level === myStandardPlan?.nextSubLevel &&
-                  //   selectSubMode === myStandardPlan?.nextMode
-                  // ) {
-                  //   return (
-                  //     <Button mt={4} mb={6} w={'100%'} variant={'whiteBase'} isDisabled>
-                  //       {t('common:support.wallet.subscription.Next plan')}
-                  //     </Button>
-                  //   );
-                  // }
                   if (isCurrentPlan) {
                     return (
                       <Button
@@ -312,7 +301,7 @@ const RowTabs = ({
           px={'12px'}
           py={'7px'}
           userSelect={'none'}
-          w={['150px', '170px']}
+          w={['150px', '190px']}
           {...(value === item.value
             ? {
                 color: 'white',
--- projects/app/src/web/common/api/request.ts
+++ projects/app/src/web/common/api/request.ts
@@ -109,6 +109,12 @@ function checkRes(data: ResponseDataType) {
  */
 function responseError(err: any) {
   console.log('error->', '请求错误', err);
+  const isOutlinkPage = {
+    '/chat/share': true,
+    '/chat/team': true,
+    '/login': true
+  }[window.location.pathname];
+
   const data = err?.response?.data || err;
 
   if (!err) {
@@ -123,7 +129,7 @@ function responseError(err: any) {
 
   // 有报错响应
   if (data?.code in TOKEN_ERROR_CODE) {
-    if (!['/chat/share', '/chat/team', '/login'].includes(window.location.pathname)) {
+    if (!isOutlinkPage) {
       clearToken();
       window.location.replace(
         getWebReqUrl(`/login?lastRoute=${encodeURIComponent(location.pathname + location.search)}`)
@@ -133,13 +139,17 @@ function responseError(err: any) {
     return Promise.reject({ message: i18nT('common:unauth_token') });
   }
   if (
-    data?.statusText === TeamErrEnum.aiPointsNotEnough ||
-    data?.statusText === TeamErrEnum.datasetSizeNotEnough ||
-    data?.statusText === TeamErrEnum.datasetAmountNotEnough ||
-    data?.statusText === TeamErrEnum.appAmountNotEnough ||
-    data?.statusText === TeamErrEnum.pluginAmountNotEnough ||
-    data?.statusText === TeamErrEnum.websiteSyncNotEnough ||
-    data?.statusText === TeamErrEnum.reRankNotEnough
+    data?.statusText &&
+    [
+      TeamErrEnum.aiPointsNotEnough,
+      TeamErrEnum.datasetSizeNotEnough,
+      TeamErrEnum.datasetAmountNotEnough,
+      TeamErrEnum.appAmountNotEnough,
+      TeamErrEnum.pluginAmountNotEnough,
+      TeamErrEnum.websiteSyncNotEnough,
+      TeamErrEnum.reRankNotEnough
+    ].includes(data?.statusText) &&
+    !isOutlinkPage
   ) {
     useSystemStore.getState().setNotSufficientModalType(data.statusText);
     return Promise.reject(data);
