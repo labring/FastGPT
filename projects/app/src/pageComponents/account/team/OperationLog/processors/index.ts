55100552722ad045fe9b7fbdabee2122af1a8c3a
archer <545436317@qq.com>
2025-06-03 22:03:12 +0800

    perf: log

--- projects/app/src/pageComponents/account/team/OperationLog/processors/index.ts
+++ projects/app/src/pageComponents/account/team/OperationLog/processors/index.ts
@@ -1,30 +1,11 @@
-import { OperationLogEventEnum } from '@fastgpt/global/support/operationLog/constants';
-import { defaultMetadataProcessor } from './commonProcessor';
+import type { OperationLogEventEnum } from '@fastgpt/global/support/operationLog/constants';
 import { createTeamProcessors } from './teamProcessors';
 import { createAppProcessors } from './appProcessors';
 import { createDatasetProcessors } from './datasetProcessors';
 
 export type MetadataProcessor = (metadata: any, t: any) => any;
-
-export const createMetadataProcessorMap = (): Record<OperationLogEventEnum, MetadataProcessor> => {
-  const specialProcessors: Partial<Record<OperationLogEventEnum, MetadataProcessor>> = {
-    ...createTeamProcessors(),
-    ...createAppProcessors(),
-    ...createDatasetProcessors()
-  };
-
-  const processorMap = {} as Record<OperationLogEventEnum, MetadataProcessor>;
-
-  Object.values(OperationLogEventEnum).forEach((event) => {
-    processorMap[event] =
-      specialProcessors[event] ||
-      ((metadata: any, t: any) => defaultMetadataProcessor(metadata, t));
-  });
-
-  return processorMap;
+export const specialProcessors: Partial<Record<OperationLogEventEnum, MetadataProcessor>> = {
+  ...createTeamProcessors,
+  ...createAppProcessors,
+  ...createDatasetProcessors
 };
-
-export * from './commonProcessor';
-export * from './teamProcessors';
-export * from './appProcessors';
-export * from './datasetProcessors';
