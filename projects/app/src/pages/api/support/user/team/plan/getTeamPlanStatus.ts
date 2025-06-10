662cc6ce01798a10003f0b132b51f76d8c85d3fa
archer <545436317@qq.com>
2025-06-10 18:12:40 +0800

    feat: more sub plan info

--- projects/app/src/pages/api/support/user/team/plan/getTeamPlanStatus.ts
+++ projects/app/src/pages/api/support/user/team/plan/getTeamPlanStatus.ts
@@ -2,17 +2,53 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { authCert } from '@fastgpt/service/support/permission/auth/common';
 import { getTeamPlanStatus } from '@fastgpt/service/support/wallet/sub/utils';
 import { NextAPI } from '@/service/middleware/entry';
+import type { ClientTeamPlanStatusType } from '@fastgpt/global/support/wallet/sub/type';
+import { MongoApp } from '@fastgpt/service/core/app/schema';
+import { AppTypeEnum } from '@fastgpt/global/core/app/constants';
+import { MongoDataset } from '@fastgpt/service/core/dataset/schema';
+import { DatasetTypeEnum } from '@fastgpt/global/core/dataset/constants';
+import { getVectorCountByTeamId } from '@fastgpt/service/common/vectorDB/controller';
+import { MongoTeamMember } from '@fastgpt/service/support/user/team/teamMemberSchema';
+import { TeamMemberStatusEnum } from '@fastgpt/global/support/user/team/constant';
 
-async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+): Promise<ClientTeamPlanStatusType | undefined> {
   try {
     const { teamId } = await authCert({
       req,
       authToken: true
     });
 
-    return getTeamPlanStatus({
-      teamId
-    });
+    const [planStatus, usedMember, usedAppAmount, usedDatasetSize, usedDatasetIndexSize] =
+      await Promise.all([
+        getTeamPlanStatus({
+          teamId
+        }),
+        MongoTeamMember.countDocuments({
+          teamId,
+          status: { $ne: TeamMemberStatusEnum.leave }
+        }),
+        MongoApp.countDocuments({
+          teamId,
+          type: {
+            $in: [AppTypeEnum.simple, AppTypeEnum.workflow, AppTypeEnum.plugin, AppTypeEnum.tool]
+          }
+        }),
+        MongoDataset.countDocuments({
+          type: { $ne: DatasetTypeEnum.folder }
+        }),
+        getVectorCountByTeamId(teamId)
+      ]);
+
+    return {
+      ...planStatus,
+      usedMember,
+      usedAppAmount,
+      usedDatasetSize,
+      usedDatasetIndexSize
+    };
   } catch (error) {}
 }
 
--- projects/app/src/web/support/user/team/api.ts
+++ projects/app/src/web/support/user/team/api.ts
@@ -15,7 +15,10 @@ import type {
   TeamMemberItemType,
   TeamMemberSchema
 } from '@fastgpt/global/support/user/team/type.d';
-import type { FeTeamPlanStatusType, TeamSubSchema } from '@fastgpt/global/support/wallet/sub/type';
+import type {
+  ClientTeamPlanStatusType,
+  TeamSubSchema
+} from '@fastgpt/global/support/wallet/sub/type';
 import type { TeamInvoiceHeaderType } from '@fastgpt/global/support/user/team/type';
 import type { PaginationProps, PaginationResponse } from '@fastgpt/web/common/fetch/type';
 import type {
@@ -100,7 +103,7 @@ export const checkTeamDatasetSizeLimit = (size: number) =>
 
 /* plans */
 export const getTeamPlanStatus = () =>
-  GET<FeTeamPlanStatusType>(`/support/user/team/plan/getTeamPlanStatus`, { maxQuantity: 1 });
+  GET<ClientTeamPlanStatusType>(`/support/user/team/plan/getTeamPlanStatus`, { maxQuantity: 1 });
 export const getTeamPlans = () =>
   GET<TeamSubSchema[]>(`/proApi/support/user/team/plan/getTeamPlans`);
 
