642cd80ba7f41dd4586ae190ce625d572edd8d4e
archer <545436317@qq.com>
2025-06-18 10:39:51 +0800

    fix: ts

--- test/mocks/index.ts
+++ test/mocks/index.ts
@@ -1,8 +1,10 @@
 import { vi } from 'vitest';
 import './request';
 
-vi.mock(import('@fastgpt/service/support/audit/util'), () => {
+vi.mock(import('@fastgpt/service/support/audit/util'), async (importOriginal) => {
+  const actual = await importOriginal();
   return {
+    ...actual,
     addAuditLog: vi.fn()
   };
 });
