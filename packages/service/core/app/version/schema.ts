25c00cdef603d409c579ce368acad67be45f099a
Theresa <63280168+sd0ric4@users.noreply.github.com>
2025-06-26 12:59:34 +0800

    Mcp db fix (#5096)
    * fix: correct input schema property name in getTools method
    
    * fix: refactor schema definitions for AppSchema and AppVersionSchema

--- packages/service/core/app/version/schema.ts
+++ packages/service/core/app/version/schema.ts
@@ -6,35 +6,40 @@ import { TeamMemberCollectionName } from '@fastgpt/global/support/user/team/cons
 
 export const AppVersionCollectionName = 'app_versions';
 
-const AppVersionSchema = new Schema({
-  tmbId: {
-    type: String,
-    ref: TeamMemberCollectionName,
-    required: true
+const AppVersionSchema = new Schema(
+  {
+    tmbId: {
+      type: String,
+      ref: TeamMemberCollectionName,
+      required: true
+    },
+    appId: {
+      type: Schema.Types.ObjectId,
+      ref: AppVersionCollectionName,
+      required: true
+    },
+    time: {
+      type: Date,
+      default: () => new Date()
+    },
+    nodes: {
+      type: Array,
+      default: []
+    },
+    edges: {
+      type: Array,
+      default: []
+    },
+    chatConfig: {
+      type: chatConfigType
+    },
+    isPublish: Boolean,
+    versionName: String
   },
-  appId: {
-    type: Schema.Types.ObjectId,
-    ref: AppVersionCollectionName,
-    required: true
-  },
-  time: {
-    type: Date,
-    default: () => new Date()
-  },
-  nodes: {
-    type: Array,
-    default: []
-  },
-  edges: {
-    type: Array,
-    default: []
-  },
-  chatConfig: {
-    type: chatConfigType
-  },
-  isPublish: Boolean,
-  versionName: String
-});
+  {
+    minimize: false
+  }
+);
 
 try {
   AppVersionSchema.index({ appId: 1, time: -1 });
