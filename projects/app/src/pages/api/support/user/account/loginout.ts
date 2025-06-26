1cc86f9eb7d39c0afe6612d079750c0b7d1435c8
dreamer6680 <1468683855@qq.com>
2025-06-26 13:00:31 +0800

    fix loginout don`t delete session (#5088)
    Co-authored-by: dreamer6680 <146868355@qq.com>

--- projects/app/src/pages/api/support/user/account/loginout.ts
+++ projects/app/src/pages/api/support/user/account/loginout.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { clearCookie } from '@fastgpt/service/support/permission/controller';
 import { NextAPI } from '@/service/middleware/entry';
+import { authCert } from '@fastgpt/service/support/permission/auth/common';
+import { delUserAllSession } from '@fastgpt/service/support/user/session';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const { userId } = await authCert({ req, authToken: true });
+  await delUserAllSession(userId);
   clearCookie(res);
 }
 
