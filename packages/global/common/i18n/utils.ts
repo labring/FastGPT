6d9cdf8c2b65a6da9988e8a4b9916ca60eb43538
archer <545436317@qq.com>
2025-06-27 11:52:23 +0800

    fix: secret

--- packages/global/common/i18n/utils.ts
+++ packages/global/common/i18n/utils.ts
@@ -1,6 +1,6 @@
 import type { I18nStringType, localeType } from './type';
 
 export const parseI18nString = (str: I18nStringType | string = '', lang: localeType = 'en') => {
-  if (typeof str === 'string') return str;
+  if (!str || typeof str === 'string') return str;
   return str[lang] ?? str['en'];
 };
