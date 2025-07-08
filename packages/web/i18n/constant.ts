a954513af4092dd9cbbcee57b2c9b744152cab98
heheer <heheer@sealos.io>
2025-07-08 20:48:17 +0800

    fix form render (#5177)

---
+++ packages/web/i18n/constant.ts
@@ -0,0 +1,23 @@
+export const I18N_NAMESPACES = [
+  'common',
+  'dataset',
+  'app',
+  'file',
+  'publish',
+  'workflow',
+  'user',
+  'chat',
+  'login',
+  'account_info',
+  'account_usage',
+  'account_bill',
+  'account_apikey',
+  'account_setting',
+  'account_inform',
+  'account_promotion',
+  'account_thirdParty',
+  'account',
+  'account_team',
+  'account_model',
+  'dashboard_mcp'
+];
