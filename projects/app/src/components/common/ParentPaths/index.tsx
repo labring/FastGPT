9acdc8ecafaaa332711023bfcb2405a86b51f75e
heheer <heheer@sealos.io>
2025-07-08 18:17:21 +0800

    optimize ParentPaths component

--- projects/app/src/components/common/ParentPaths/index.tsx
+++ projects/app/src/components/common/ParentPaths/index.tsx
@@ -2,6 +2,8 @@ import { Box, Flex } from '@chakra-ui/react';
 import { type ParentTreePathItemType } from '@fastgpt/global/common/parentFolder/type';
 import React, { useMemo } from 'react';
 import { useTranslation } from 'next-i18next';
+import MyMenu from '@fastgpt/web/components/common/MyMenu';
+import MyTooltip from '@fastgpt/web/components/common/MyTooltip';
 
 const ParentPaths = (props: {
   paths?: ParentTreePathItemType[];
@@ -23,43 +25,104 @@ const ParentPaths = (props: {
     [rootName, paths]
   );
 
+  const displayPaths = useMemo(() => {
+    if (concatPaths.length <= 3) {
+      return concatPaths;
+    } else {
+      return [
+        concatPaths[0],
+        null,
+        concatPaths[concatPaths.length - 2],
+        concatPaths[concatPaths.length - 1]
+      ];
+    }
+  }, [concatPaths]);
+
+  const renderPathItem = (item: (typeof concatPaths)[0] | null, index: number) => {
+    if (item === null) {
+      const middlePaths = concatPaths.slice(1, -2);
+
+      return (
+        <Flex alignItems={'center'}>
+          <MyMenu
+            Button={
+              <Box fontSize={['sm', fontSize || 'sm']} px={1} color={'myGray.600'} cursor="pointer">
+                ...
+              </Box>
+            }
+            trigger={'hover'}
+            size={'xs'}
+            menuList={[
+              {
+                children: middlePaths.map((pathItem) => ({
+                  label: pathItem.parentName,
+                  icon: 'file/fill/folder',
+                  onClick: () => onClick(pathItem.parentId)
+                }))
+              }
+            ]}
+          />
+          <Box mx={1} color={'myGray.500'}>
+            /
+          </Box>
+        </Flex>
+      );
+    }
+
+    const isLast = index === displayPaths.length - 1;
+    const displayName =
+      !isLast && item.parentName.length > 10
+        ? `${item.parentName.slice(0, 10)}...`
+        : item.parentName;
+
+    const pathBox = (
+      <Box
+        fontSize={['sm', fontSize || 'sm']}
+        py={0.5}
+        px={1.5}
+        borderRadius={'md'}
+        {...(isLast
+          ? {
+              cursor: 'default',
+              color: 'myGray.700',
+              fontWeight: 'bold'
+            }
+          : {
+              cursor: 'pointer',
+              color: 'myGray.600',
+              _hover: {
+                bg: 'myGray.100'
+              },
+              onClick: () => {
+                onClick(item.parentId);
+              }
+            })}
+      >
+        {displayName}
+      </Box>
+    );
+
+    return (
+      <Flex key={item.parentId || index} alignItems={'center'}>
+        {!isLast && item.parentName.length > 10 ? (
+          <MyTooltip label={item.parentName}>{pathBox}</MyTooltip>
+        ) : (
+          pathBox
+        )}
+        {!isLast && (
+          <Box mx={1} color={'myGray.500'}>
+            /
+          </Box>
+        )}
+      </Flex>
+    );
+  };
+
   return paths.length === 0 && !!FirstPathDom ? (
     <>{FirstPathDom}</>
   ) : (
     <Flex flex={1} ml={-2}>
-      {concatPaths.map((item, i) => (
-        <Flex key={item.parentId || i} alignItems={'center'}>
-          <Box
-            fontSize={['sm', fontSize || 'sm']}
-            py={0.5}
-            px={1.5}
-            borderRadius={'md'}
-            {...(i === concatPaths.length - 1
-              ? {
-                  cursor: 'default',
-                  color: 'myGray.700',
-                  fontWeight: 'bold'
-                }
-              : {
-                  cursor: 'pointer',
-                  color: 'myGray.600',
-                  _hover: {
-                    bg: 'myGray.100'
-                  },
-                  onClick: () => {
-                    onClick(item.parentId);
-                  }
-                })}
-          >
-            {item.parentName}
-          </Box>
-          {i !== concatPaths.length - 1 && (
-            <Box mx={1} color={'myGray.500'}>
-              /
-            </Box>
-          )}
-        </Flex>
-      ))}
+      {displayPaths.map((item, index) => renderPathItem(item, index))}
     </Flex>
   );
 };
