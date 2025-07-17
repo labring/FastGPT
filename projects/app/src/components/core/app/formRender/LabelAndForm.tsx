cd486bf406eca09713be9393bc321023359aa382
archer <545436317@qq.com>
2025-07-18 00:21:03 +0800

    fix: variablesinit

--- projects/app/src/components/core/app/formRender/LabelAndForm.tsx
+++ projects/app/src/components/core/app/formRender/LabelAndForm.tsx
@@ -2,7 +2,7 @@ import type { BoxProps } from '@chakra-ui/react';
 import { Box, Flex } from '@chakra-ui/react';
 import FormLabel from '@fastgpt/web/components/common/MyBox/FormLabel';
 import QuestionTip from '@fastgpt/web/components/common/MyTooltip/QuestionTip';
-import React, { useMemo } from 'react';
+import React from 'react';
 import type { UseFormReturn } from 'react-hook-form';
 import { Controller } from 'react-hook-form';
 import InputRender from '.';
@@ -46,14 +46,13 @@ const LabelAndFormRender = ({
   const { control } = variablesForm;
 
   return (
-    <Box _notLast={{ mb: 4 }} px={1}>
+    <Box _notLast={{ mb: 4 }}>
       <Flex alignItems={'center'} mb={1}>
         <FormLabel required={required}>{label}</FormLabel>
         {placeholder && <QuestionTip ml={1} label={placeholder} />}
       </Flex>
 
       <Controller
-        key={formKey}
         control={control}
         name={formKey}
         rules={{
