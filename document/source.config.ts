e15e6cd75995a4687d096108f1e04a102137384c
Archer <c121914yjl@gmail.com>
2025-07-24 13:14:06 +0800

    doc (#102)
    * doc (#101)
    
    * do
    
    * add redirect
    
    * doc
    
    * fix: action
    
    * action

--- document/source.config.ts
+++ document/source.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, defineDocs, frontmatterSchema, metaSchema } from 'fumadocs-mdx/config';
+import { z } from 'zod';
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
   docs: {
-    schema: frontmatterSchema
+    schema: frontmatterSchema.extend({
+      title: z.string().optional().default('Untitled')
+    })
   },
   meta: {
     schema: metaSchema
