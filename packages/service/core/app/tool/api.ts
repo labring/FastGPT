90825809ad4557f03da1365ee1bedc5b05c0e603
archer <545436317@qq.com>
2025-07-31 14:42:34 +0800

    perf: model config move to plugin

--- packages/service/core/app/tool/api.ts
+++ packages/service/core/app/tool/api.ts
@@ -1,16 +1,9 @@
-import createClient, { RunToolWithStream } from '@fastgpt-sdk/plugin';
+import { RunToolWithStream } from '@fastgpt-sdk/plugin';
 import { PluginSourceEnum } from '@fastgpt/global/core/app/plugin/constants';
-
-const BASE_URL = process.env.PLUGIN_BASE_URL || '';
-const TOKEN = process.env.PLUGIN_TOKEN || '';
-
-const client = createClient({
-  baseUrl: BASE_URL,
-  token: TOKEN
-});
+import { pluginClient, BASE_URL, TOKEN } from '../../../thirdProvider/fastgptPlugin';
 
 export async function APIGetSystemToolList() {
-  const res = await client.tool.list();
+  const res = await pluginClient.tool.list();
 
   if (res.status === 200) {
     return res.body.map((item) => {
