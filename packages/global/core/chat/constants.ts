3c2999af6c15665aa62dee16d78a3aedea4d49be
heheer <zhiyu44@qq.com>
2025-08-04 18:09:13 +0800

    add logs chart (#5352)
    * charts
    
    * chart data
    
    * log chart
    
    * delete
    
    * rename api
    
    * fix
    
    * move api
    
    * fix
    
    * fix
    
    * pro config
    
    * fix

--- packages/global/core/chat/constants.ts
+++ packages/global/core/chat/constants.ts
@@ -44,34 +44,44 @@ export enum ChatSourceEnum {
 
 export const ChatSourceMap = {
   [ChatSourceEnum.test]: {
-    name: i18nT('common:core.chat.logs.test')
+    name: i18nT('common:core.chat.logs.test'),
+    color: '#5E8FFF'
   },
   [ChatSourceEnum.online]: {
-    name: i18nT('common:core.chat.logs.online')
+    name: i18nT('common:core.chat.logs.online'),
+    color: '#47B2FF'
   },
   [ChatSourceEnum.share]: {
-    name: i18nT('common:core.chat.logs.share')
+    name: i18nT('common:core.chat.logs.share'),
+    color: '#9E8DFB'
   },
   [ChatSourceEnum.api]: {
-    name: i18nT('common:core.chat.logs.api')
+    name: i18nT('common:core.chat.logs.api'),
+    color: '#D389F6'
   },
   [ChatSourceEnum.cronJob]: {
-    name: i18nT('chat:source_cronJob')
+    name: i18nT('chat:source_cronJob'),
+    color: '#FF81AE'
   },
   [ChatSourceEnum.team]: {
-    name: i18nT('common:core.chat.logs.team')
+    name: i18nT('common:core.chat.logs.team'),
+    color: '#42CFC6'
   },
   [ChatSourceEnum.feishu]: {
-    name: i18nT('common:core.chat.logs.feishu')
+    name: i18nT('common:core.chat.logs.feishu'),
+    color: '#39CC83'
   },
   [ChatSourceEnum.official_account]: {
-    name: i18nT('common:core.chat.logs.official_account')
+    name: i18nT('common:core.chat.logs.official_account'),
+    color: '#FDB022'
   },
   [ChatSourceEnum.wecom]: {
-    name: i18nT('common:core.chat.logs.wecom')
+    name: i18nT('common:core.chat.logs.wecom'),
+    color: '#FD853A'
   },
   [ChatSourceEnum.mcp]: {
-    name: i18nT('common:core.chat.logs.mcp')
+    name: i18nT('common:core.chat.logs.mcp'),
+    color: '#F97066'
   }
 };
 
