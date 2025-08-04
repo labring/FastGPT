6a0b0b1991f2547a04996f3eeeacaa14eef4ef46
Archer <545436317@qq.com>
2025-08-04 22:07:52 +0800

    update doc search engine (#5386)
    * update doc search engine
    
    * custom tokenizer
    
    * tokenizer

--- document/components/CustomSearchDialog.tsx
+++ document/components/CustomSearchDialog.tsx
@@ -1,6 +1,5 @@
 'use client';
 // components/CustomSearchDialog.tsx
-import { liteClient } from 'algoliasearch/lite';
 import { useDocsSearch } from 'fumadocs-core/search/client';
 import {
   SearchDialog,
@@ -15,21 +14,11 @@ import {
 } from 'fumadocs-ui/components/dialog/search';
 import { useI18n } from 'fumadocs-ui/contexts/i18n';
 
-if (!process.env.NEXT_PUBLIC_SEARCH_APPID || !process.env.NEXT_PUBLIC_SEARCH_APPKEY) {
-  throw new Error('NEXT_PUBLIC_SEARCH_APPID and NEXT_PUBLIC_SEARCH_APPKEY are not set');
-}
-
-const client = liteClient(
-  process.env.NEXT_PUBLIC_SEARCH_APPID,
-  process.env.NEXT_PUBLIC_SEARCH_APPKEY
-);
-
 export default function CustomSearchDialog(props: SharedProps) {
   const { locale } = useI18n();
   const { search, setSearch, query } = useDocsSearch({
-    type: 'algolia',
-    client,
-    indexName: 'document',
+    type: 'fetch',
+    api: '/api/search',
     locale
   });
 
