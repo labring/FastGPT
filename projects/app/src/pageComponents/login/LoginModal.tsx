dc6e86a6586c21a9ba0b6c820d9304f60e66bb6e
archer <545436317@qq.com>
2025-08-05 16:09:13 +0800

    perf: login ui

--- projects/app/src/pageComponents/login/LoginModal.tsx
+++ projects/app/src/pageComponents/login/LoginModal.tsx
@@ -3,7 +3,7 @@ import { Box, Flex } from '@chakra-ui/react';
 import { LoginContainer } from '@/pageComponents/login';
 import I18nLngSelector from '@/components/Select/I18nLngSelector';
 import { useSystem } from '@fastgpt/web/hooks/useSystem';
-import MyModal from '@fastgpt/web/components/common/MyModal';
+import { getWebReqUrl } from '@fastgpt/web/common/system/utils';
 
 type LoginModalProps = {
   onSuccess?: () => void;
@@ -13,35 +13,39 @@ const LoginModal = ({ onSuccess }: LoginModalProps) => {
   const { isPc } = useSystem();
 
   return (
-    <MyModal
-      isOpen
-      closeOnOverlayClick={false}
-      isCentered
-      overflow="auto"
-      minH={['100vh', '690px']}
-      maxW={['100%', '560px']}
-      w={'100%'}
+    <Flex
+      alignItems={'center'}
+      justifyContent={'center'}
+      bg={['white', `url(${getWebReqUrl('/icon/login-bg.svg')}) no-repeat`]}
+      backgroundSize={['cover', 'cover']}
+      userSelect={'none'}
+      h={'100%'}
     >
+      {/* Language selector - login page */}
+      {isPc && (
+        <Box position="absolute" top="24px" right="24px" zIndex={10}>
+          <I18nLngSelector />
+        </Box>
+      )}
+
       <Flex
+        flexDirection={'column'}
         w={['100%', '560px']}
-        px={['5vw', '90px']}
-        py={['5vh', '90px']}
-        h="690px"
-        flexGrow={1}
-        maxW="560px"
-        flexDirection="column"
+        h={['100%', '690px']}
+        bg={[`url(${getWebReqUrl('/icon/login-bg-phone.svg')}) no-repeat`, 'white']}
+        backgroundSize={'cover'}
+        px={['8', '90px']}
+        py={['38px', '90px']}
+        borderRadius={[0, '16px']}
+        boxShadow={[
+          '',
+          '0px 32px 64px -12px rgba(19, 51, 107, 0.20), 0px 0px 1px 0px rgba(19, 51, 107, 0.20)'
+        ]}
         position="relative"
       >
-        {/* language selector - modal */}
-        {isPc && (
-          <Box position="absolute" top="24px" right="24px" zIndex={10}>
-            <I18nLngSelector />
-          </Box>
-        )}
-
         <LoginContainer onSuccess={onSuccess} />
       </Flex>
-    </MyModal>
+    </Flex>
   );
 };
 
