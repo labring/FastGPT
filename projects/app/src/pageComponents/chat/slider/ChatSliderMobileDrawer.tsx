ff9c6c9b81cac4bf0985cbe60b70bb7e0abd36a9
伍闲犬 <whoeverimf5@gmail.com>
2025-08-20 20:22:21 +0800

    chore: extract chat history and drawer; fix model selector (#5492)
    * chore: extract chat history and drawer; fix model selector
    
    * refactor: chat slider components structure

---
+++ projects/app/src/pageComponents/chat/slider/ChatSliderMobileDrawer.tsx
@@ -0,0 +1,64 @@
+import { Drawer, DrawerOverlay, DrawerContent, useTheme } from '@chakra-ui/react';
+import React from 'react';
+import MyBox from '@fastgpt/web/components/common/MyBox';
+import ChatSliderHeader from '@/pageComponents/chat/slider/ChatSliderHeader';
+import ChatSliderMenu from '@/pageComponents/chat/slider/ChatSliderMenu';
+import ChatSliderList from '@/pageComponents/chat/slider/ChatSliderList';
+import { useContextSelector } from 'use-context-selector';
+import { ChatContext } from '@/web/core/chat/context/chatContext';
+import ChatSliderFooter from '@/pageComponents/chat/slider/ChatSliderFooter';
+
+type Props = {
+  title?: string;
+  banner?: string;
+  menuConfirmButtonText?: string;
+  showHeader?: boolean;
+  showFooter?: boolean;
+};
+
+const ChatSliderMobileDrawer = ({
+  title,
+  banner,
+  menuConfirmButtonText,
+  showHeader = false,
+  showFooter = false
+}: Props) => {
+  const theme = useTheme();
+
+  const isOpenSlider = useContextSelector(ChatContext, (v) => v.isOpenSlider);
+  const onCloseSlider = useContextSelector(ChatContext, (v) => v.onCloseSlider);
+
+  return (
+    <Drawer
+      size="xs"
+      placement="left"
+      autoFocus={false}
+      isOpen={isOpenSlider}
+      onClose={onCloseSlider}
+    >
+      <DrawerOverlay backgroundColor="rgba(255,255,255,0.5)" />
+
+      <DrawerContent maxWidth="75vw">
+        <MyBox
+          display={'flex'}
+          flexDirection={'column'}
+          w={'100%'}
+          h={'100%'}
+          bg={'white'}
+          borderRight={['', theme.borders.base]}
+          whiteSpace={'nowrap'}
+        >
+          {showHeader && <ChatSliderHeader title={title} banner={banner} />}
+
+          <ChatSliderMenu menuConfirmButtonText={menuConfirmButtonText} />
+
+          <ChatSliderList />
+
+          {showFooter && <ChatSliderFooter />}
+        </MyBox>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default ChatSliderMobileDrawer;
