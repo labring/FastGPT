e54cf6af964d097115bc277f6da4ff11f53f6c00
熊涛10037 <10037@sangfor.com>
2025-08-26 14:58:58 +0800

    [add]添加打包配置

--- packages/global/common/system/constants.ts
+++ packages/global/common/system/constants.ts
@@ -1,5 +1,6 @@
 export const HUMAN_ICON = `/icon/human.svg`;
 export const LOGO_ICON = `/icon/logo.svg`;
+export const SANGFOR_LOGO_ICON = `/icon/sangfor-logo.svg`;
 export const HUGGING_FACE_ICON = `/imgs/model/huggingface.svg`;
 
 export const DEFAULT_TEAM_AVATAR = `/imgs/avatar/defaultTeamAvatar.svg`;
