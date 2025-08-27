7434adf6033c228b50287dcfba22c8e28b21280c
FinleyGe <m13203533462@163.com>
2025-08-27 19:54:20 +0800

    feat: auto accept invitation when login

--- projects/app/src/pages/login/index.tsx
+++ projects/app/src/pages/login/index.tsx
@@ -4,14 +4,20 @@ import { serviceSideProps } from '@/web/common/i18n/utils';
 import { clearToken } from '@/web/support/user/auth';
 import { useMount } from 'ahooks';
 import LoginModal from '@/pageComponents/login/LoginModal';
+import { postAcceptInvitationLink } from '@/web/support/user/team/api';
 
 const Login = () => {
   const router = useRouter();
   const { lastRoute = '' } = router.query as { lastRoute: string };
 
-  const loginSuccess = useCallback(() => {
+  const loginSuccess = useCallback(async () => {
     const decodeLastRoute = decodeURIComponent(lastRoute);
-
+    if (decodeLastRoute.includes('/account/team?invitelinkid=')) {
+      const id = decodeLastRoute.split('invitelinkid=')[1];
+      await postAcceptInvitationLink(id);
+      router.push('/account/team');
+      return;
+    }
     const navigateTo =
       decodeLastRoute && !decodeLastRoute.includes('/login') && decodeLastRoute.startsWith('/')
         ? lastRoute
