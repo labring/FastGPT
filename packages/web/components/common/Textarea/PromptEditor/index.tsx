891ba24af0b001dbbc8254b5f0470312c8edbee8
heheer <zhiyu44@qq.com>
2025-08-29 10:19:36 +0800

    fix build

--- packages/web/components/common/Textarea/PromptEditor/index.tsx
+++ packages/web/components/common/Textarea/PromptEditor/index.tsx
@@ -1,6 +1,4 @@
-// Entry point for PromptEditor components
 export { default as PlainPromptEditor } from './editors/Plain';
 export { default as RichPromptEditor } from './editors/Rich';
 
-// Default export maintains backward compatibility
 export { default } from './editors/Plain';
