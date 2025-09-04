6601951e458172b85cdcfe04f40ea12110f47a65
archer <545436317@qq.com>
2025-09-04 13:48:03 +0800

    fix: fileselector default

--- projects/app/src/pageComponents/app/detail/SimpleApp/index.tsx
+++ projects/app/src/pageComponents/app/detail/SimpleApp/index.tsx
@@ -11,6 +11,7 @@ import { type SimpleAppSnapshotType, useSimpleAppSnapshots } from './useSnapshot
 import { useDebounceEffect, useMount } from 'ahooks';
 import { v1Workflow2V2 } from '@/web/core/workflow/adapt';
 import { getAppConfigByDiff } from '@/web/core/app/diff';
+import { defaultAppSelectFileConfig } from '@fastgpt/global/core/app/constants';
 
 const Edit = dynamic(() => import('./Edit'));
 const Logs = dynamic(() => import('../Logs/index'));
@@ -79,7 +80,13 @@ const SimpleEdit = () => {
     if (past.length === 0) {
       const appForm = appWorkflow2Form({
         nodes: appDetail.modules,
-        chatConfig: appDetail.chatConfig
+        chatConfig: {
+          ...appDetail.chatConfig,
+          fileSelectConfig: appDetail.chatConfig.fileSelectConfig || {
+            ...defaultAppSelectFileConfig,
+            canSelectFile: true
+          }
+        }
       });
       saveSnapshot({
         appForm,
