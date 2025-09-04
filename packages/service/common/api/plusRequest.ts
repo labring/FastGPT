204084c9f9bf10c7b9ea7656fed08762d648fb84
xqvvu <whoeverimf5@gmail.com>
2025-09-04 13:15:26 +0800

    fix: delete "Content-Length" while redirect request to pro api

--- packages/service/common/api/plusRequest.ts
+++ packages/service/common/api/plusRequest.ts
@@ -22,6 +22,9 @@ interface ResponseDataType {
  * 请求开始
  */
 function requestStart(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
+  if (config.headers) {
+    delete config.headers['content-length'];
+  }
   return config;
 }
 
