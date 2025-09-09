c1db83bf427746a5cdafab26fcf7ebd58c2e6e30
archer <545436317@qq.com>
2025-09-09 17:26:07 +0800

    perf: plan agent name

--- packages/service/core/workflow/dispatch/ai/agent/sub/constants.ts
+++ packages/service/core/workflow/dispatch/ai/agent/sub/constants.ts
@@ -1,4 +1,5 @@
 import type { ChatCompletionTool } from '@fastgpt/global/core/ai/type';
+import { i18nT } from '../../../../../../../web/i18n/utils';
 
 export enum SubAppIds {
   plan = 'plan_agent',
@@ -7,3 +8,26 @@ export enum SubAppIds {
   model = 'model_agent',
   fileRead = 'file_read'
 }
+
+export const systemSubInfo: Record<string, { name: string; avatar: string }> = {
+  [SubAppIds.plan]: {
+    name: i18nT('chat:plan_agent'),
+    avatar: 'common/detail'
+  },
+  [SubAppIds.fileRead]: {
+    name: i18nT('chat:file_parse'),
+    avatar: 'core/workflow/template/readFiles'
+  },
+  [SubAppIds.ask]: {
+    name: 'Ask Agent',
+    avatar: 'core/workflow/template/agent'
+  },
+  [SubAppIds.stop]: {
+    name: 'Stop Agent',
+    avatar: 'core/workflow/template/agent'
+  },
+  [SubAppIds.model]: {
+    name: 'Model Agent',
+    avatar: 'core/workflow/template/agent'
+  }
+};
