69a08bdc46b120446063b3c33d6b778940fe8a10
heheer <zhiyu44@qq.com>
2025-09-09 16:02:17 +0800

    temp: list value

--- packages/web/components/common/Textarea/PromptEditor/type.d.ts
+++ packages/web/components/common/Textarea/PromptEditor/type.d.ts
@@ -23,3 +23,93 @@ export type EditorVariableLabelPickerType = {
 };
 
 export type FormPropsType = Omit<BoxProps, 'onChange' | 'onBlur'>;
+
+// Lexical editor node types
+export type BaseEditorNode = {
+  type: string;
+  version: number;
+};
+
+export type TextEditorNode = BaseEditorNode & {
+  type: 'text';
+  text: string;
+  detail: number;
+  format: number;
+  mode: string;
+  style: string;
+};
+
+export type LineBreakEditorNode = BaseEditorNode & {
+  type: 'linebreak';
+};
+
+export type VariableLabelEditorNode = BaseEditorNode & {
+  type: 'variableLabel';
+  variableKey: string;
+};
+
+export type VariableEditorNode = BaseEditorNode & {
+  type: 'Variable';
+  variableKey: string;
+};
+
+export type ChildEditorNode =
+  | TextEditorNode
+  | LineBreakEditorNode
+  | VariableLabelEditorNode
+  | VariableEditorNode;
+
+export type ParagraphEditorNode = BaseEditorNode & {
+  type: 'paragraph';
+  children: ChildEditorNode[];
+  direction: string;
+  format: string;
+  indent: number;
+};
+
+export type ListItemEditorNode = BaseEditorNode & {
+  type: 'listitem';
+  children: Array<ChildEditorNode | ListEditorNode>;
+  direction: string | null;
+  format: string;
+  indent: number;
+  value: number;
+};
+
+export type ListEditorNode = BaseEditorNode & {
+  type: 'list';
+  children: ListItemEditorNode[];
+  direction: string | null;
+  format: string;
+  indent: number;
+  listType: 'bullet' | 'number';
+  start: number;
+  tag: 'ul' | 'ol';
+};
+
+export type RootEditorNode = BaseEditorNode & {
+  type: 'root';
+  children: Array<ParagraphEditorNode | ListEditorNode>;
+  direction: string;
+  format: string;
+  indent: number;
+};
+
+export type EditorState = {
+  root: RootEditorNode;
+};
+
+// For textToEditorState parsing
+export type ParsedTextLine = {
+  type: 'paragraph' | 'bullet' | 'number';
+  text: string;
+  indent: number;
+  value?: number;
+};
+
+export type ListItemInfo = {
+  type: 'bullet' | 'number';
+  text: string;
+  indent: number;
+  value?: number;
+};
