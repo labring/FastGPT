08a0ee1f2390badc15c1da7b366657d5ff04d3e7
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2025-09-11 10:38:02 +0800

    refactor: inherit permission (#5529)
    * refactor: permission update conflict check function
    
    * refactor(permission): app collaborator update api
    
    * refactor(permission): support app update collaborator
    
    * feat: support fe permission conflict check
    
    * refactor(permission): app permission
    
    * refactor(permission): dataset permission
    
    * refactor(permission): team permission
    
    * chore: fe adjust
    
    * fix: type error
    
    * fix: audit pagiation
    
    * fix: tc
    
    * chore: initv4130
    
    * fix: app/dataset auth logic
    
    * chore: move code
    
    * refactor(permission): remove selfPermission
    
    * fix: mock
    
    * fix: test
    
    * fix: app & dataset auth
    
    * fix: inherit
    
    * test(inheritPermission): test syncChildrenPermission

--- projects/app/src/pageComponents/app/detail/InfoModal.tsx
+++ projects/app/src/pageComponents/app/detail/InfoModal.tsx
@@ -31,6 +31,7 @@ import { useTranslation } from 'next-i18next';
 import React, { useCallback } from 'react';
 import { useForm } from 'react-hook-form';
 import { useContextSelector } from 'use-context-selector';
+import { ReadRoleVal } from '@fastgpt/global/support/permission/constant';
 
 const InfoModal = ({ onClose }: { onClose: () => void }) => {
   const { t } = useTranslation();
@@ -100,25 +101,6 @@ const InfoModal = ({ onClose }: { onClose: () => void }) => {
     [handleSubmit, onClose, saveSubmitError, saveSubmitSuccess]
   );
 
-  const onUpdateCollaborators = ({
-    members,
-    groups,
-    orgs,
-    permission
-  }: {
-    members?: string[];
-    groups?: string[];
-    orgs?: string[];
-    permission: PermissionValueType;
-  }) =>
-    postUpdateAppCollaborators({
-      members,
-      groups,
-      permission,
-      orgs,
-      appId: appDetail._id
-    });
-
   const onDelCollaborator = async (
     props: RequireOnlyOne<{ tmbId: string; groupId: string; orgId: string }>
   ) =>
@@ -185,15 +167,14 @@ const InfoModal = ({ onClose }: { onClose: () => void }) => {
             )}
             <Box mt={6}>
               <CollaboratorContextProvider
+                defaultRole={ReadRoleVal}
                 permission={appDetail.permission}
                 onGetCollaboratorList={() => getCollaboratorList(appDetail._id)}
                 roleList={AppRoleList}
-                onUpdateCollaborators={async (props) =>
-                  onUpdateCollaborators({
-                    permission: props.permission,
-                    members: props.members,
-                    groups: props.groups,
-                    orgs: props.orgs
+                onUpdateCollaborators={async ({ collaborators }) =>
+                  postUpdateAppCollaborators({
+                    collaborators,
+                    appId: appDetail._id
                   })
                 }
                 onDelOneCollaborator={onDelCollaborator}
@@ -201,7 +182,7 @@ const InfoModal = ({ onClose }: { onClose: () => void }) => {
                 isInheritPermission={appDetail.inheritPermission}
                 hasParent={!!appDetail.parentId}
               >
-                {({ MemberListCard, onOpenManageModal, onOpenAddMember }) => {
+                {({ MemberListCard, onOpenManageModal }) => {
                   return (
                     <>
                       <Flex
@@ -211,24 +192,14 @@ const InfoModal = ({ onClose }: { onClose: () => void }) => {
                         w="full"
                       >
                         <Box fontSize={'sm'}>{t('common:permission.Collaborator')}</Box>
-                        <Flex flexDirection="row" gap="2">
-                          <Button
-                            size="sm"
-                            variant="whitePrimary"
-                            leftIcon={<MyIcon w="4" name="common/settingLight" />}
-                            onClick={onOpenManageModal}
-                          >
-                            {t('common:permission.Manage')}
-                          </Button>
-                          <Button
-                            size="sm"
-                            variant="whitePrimary"
-                            leftIcon={<MyIcon w="4" name="support/permission/collaborator" />}
-                            onClick={onOpenAddMember}
-                          >
-                            {t('common:Add')}
-                          </Button>
-                        </Flex>
+                        <Button
+                          size="sm"
+                          variant="whitePrimary"
+                          leftIcon={<MyIcon w="4" name="common/settingLight" />}
+                          onClick={onOpenManageModal}
+                        >
+                          {t('common:permission.Manage')}
+                        </Button>
                       </Flex>
                       <MemberListCard mt={2} p={1.5} bg="myGray.100" borderRadius="md" />
                     </>
