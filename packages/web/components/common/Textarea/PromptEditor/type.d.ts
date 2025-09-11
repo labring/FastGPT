a0e9ef006c71b1b03e407a4f630ea4c3cade125e
heheer <heheer@sealos.io>
2025-09-11 15:33:18 +0800

    prompt editor add list plugin (#5620)
    * perf: search result (#5608)
    
    * fix: table size (#5598)
    
    * temp: list value
    
    * backspace
    
    * optimize code
    
    ---------
    
    Co-authored-by: Archer <545436317@qq.com>
    Co-authored-by: 伍闲犬 <whoeverimf5@gmail.com>

--- packages/web/components/common/Textarea/PromptEditor/type.d.ts
+++ packages/web/components/common/Textarea/PromptEditor/type.d.ts
@@ -23,3 +23,88 @@ export type EditorVariableLabelPickerType = {
 };
 
 export type FormPropsType = Omit<BoxProps, 'onChange' | 'onBlur'>;
+
+// Lexical editor node types
+export type BaseEditorNode = {
+  type: string;
+  version: number;
+};
+
+export type TextEditorNode = BaseEditorNode & {
+  type: 'text';
+  text: string;
+  detail: number;
+  format: number;
+  mode: string;
+  style: string;
+};
+
+export type LineBreakEditorNode = BaseEditorNode & {
+  type: 'linebreak';
+};
+
+export type VariableLabelEditorNode = BaseEditorNode & {
+  type: 'variableLabel';
+  variableKey: string;
+};
+
+export type VariableEditorNode = BaseEditorNode & {
+  type: 'Variable';
+  variableKey: string;
+};
+
+export type TabEditorNode = BaseEditorNode & {
+  type: 'tab';
+};
+
+export type ChildEditorNode =
+  | TextEditorNode
+  | LineBreakEditorNode
+  | VariableLabelEditorNode
+  | VariableEditorNode
+  | TabEditorNode;
+
+export type ParagraphEditorNode = BaseEditorNode & {
+  type: 'paragraph';
+  children: ChildEditorNode[];
+  direction: string;
+  format: string;
+  indent: number;
+};
+
+export type ListItemEditorNode = BaseEditorNode & {
+  type: 'listitem';
+  children: Array<ChildEditorNode | ListEditorNode>;
+  direction: string | null;
+  format: string;
+  indent: number;
+  value: number;
+};
+
+export type ListEditorNode = BaseEditorNode & {
+  type: 'list';
+  children: ListItemEditorNode[];
+  direction: string | null;
+  format: string;
+  indent: number;
+  listType: 'bullet' | 'number';
+  start: number;
+  tag: 'ul' | 'ol';
+};
+
+export type EditorState = {
+  root: {
+    type: 'root';
+    children: Array<ParagraphEditorNode | ListEditorNode>;
+    direction: string;
+    format: string;
+    indent: number;
+  } & BaseEditorNode;
+};
+
+export type ListItemInfo = {
+  type: 'bullet' | 'number';
+  text: string;
+  indent: number;
+  numberValue?: number;
+};
