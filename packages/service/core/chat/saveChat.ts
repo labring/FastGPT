5ca6f8e607959ef757a7de990ada65e4e2449292
francis <1485059943@qq.com>
2025-09-16 19:04:04 +0800

    perf: plan agent detail fixing (#5658)
    * chore: clean up
    
    * fix: tool response
    
    * fix: context memory
    
    * feat: distinguish by workflow deepth
    
    * perf: master prompt

--- packages/service/core/chat/saveChat.ts
+++ packages/service/core/chat/saveChat.ts
@@ -422,6 +422,13 @@ export const updateInteractiveChat = async (props: Props) => {
         : aiResponse.citeCollectionIds;
     }
 
+    if (aiContent.memories) {
+      chatItem.memories = {
+        ...chatItem.memories,
+        ...aiContent.memories
+      };
+    }
+
     chatItem.durationSeconds = chatItem.durationSeconds
       ? +(chatItem.durationSeconds + durationSeconds).toFixed(2)
       : durationSeconds;
