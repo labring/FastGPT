e623fc5b7f28ac8bcc06753a04eca24e1c7a130c
heheer <zhiyu44@qq.com>
2025-09-24 15:19:28 +0800

    fix prompt editor

--- packages/web/components/common/Textarea/PromptEditor/plugins/VariablePlugin/index.tsx
+++ packages/web/components/common/Textarea/PromptEditor/plugins/VariablePlugin/index.tsx
@@ -21,32 +21,26 @@ export default function VariablePlugin({ variables }: { variables: EditorVariabl
     return variables.map((item) => item.key);
   }, [variables]);
 
-  const createVariableNode = useCallback(
-    (textNode: TextNode): VariableNode => {
-      const currentVariable = variables.find(
-        (item) => item.key === textNode.getTextContent().replace(/[{}]/g, '')
-      );
-      const variableLabel = currentVariable?.label;
-      return $createVariableNode(textNode.getTextContent(), variableLabel || '');
-    },
-    [variables]
-  );
+  const createVariableNode = useCallback((textNode: TextNode): VariableNode => {
+    const currentVariable = variables.find(
+      (item) => item.key === textNode.getTextContent().replace(/[{}]/g, '')
+    );
+    const variableLabel = currentVariable?.label;
+    return $createVariableNode(textNode.getTextContent(), variableLabel || '');
+  }, []);
 
-  const getVariableMatch = useCallback(
-    (text: string) => {
-      const matches = REGEX.exec(text);
-      if (!matches) return null;
-      if (variableKeys.indexOf(matches[3]) === -1) return null;
-      const hashtagLength = matches[3].length + 4;
-      const startOffset = matches.index;
-      const endOffset = startOffset + hashtagLength;
-      return {
-        end: endOffset,
-        start: startOffset
-      };
-    },
-    [variableKeys]
-  );
+  const getVariableMatch = useCallback((text: string) => {
+    const matches = REGEX.exec(text);
+    if (!matches) return null;
+    if (variableKeys.indexOf(matches[3]) === -1) return null;
+    const hashtagLength = matches[3].length + 4;
+    const startOffset = matches.index;
+    const endOffset = startOffset + hashtagLength;
+    return {
+      end: endOffset,
+      start: startOffset
+    };
+  }, []);
 
   useEffect(() => {
     mergeRegister(
