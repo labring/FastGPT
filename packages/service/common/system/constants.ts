59b445fbbec6dae6b495bdfddb7729872c435f46
archer <545436317@qq.com>
2025-09-30 11:21:41 +0800

    http response limit

--- packages/service/common/system/constants.ts
+++ packages/service/common/system/constants.ts
@@ -8,4 +8,4 @@ export const isProVersion = () => {
 };
 
 export const serviceRequestMaxContentLength =
-  Number(process.env.SERVICE_REQUEST_MAX_CONTENT_LENGTH || 5) * 1024 * 1024; // 5MB
+  Number(process.env.SERVICE_REQUEST_MAX_CONTENT_LENGTH || 10) * 1024 * 1024; // 10MB
