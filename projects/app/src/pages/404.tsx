6611599301c1a2f259289a058f1318be2b113de8
archer <545436317@qq.com>
2025-10-20 10:25:39 +0800

    doc

--- projects/app/src/pages/404.tsx
+++ projects/app/src/pages/404.tsx
@@ -1,5 +1,3 @@
-'use client';
-import { serviceSideProps } from '@/web/common/i18n/utils';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
@@ -11,13 +9,5 @@ const NonePage = () => {
 
   return <div></div>;
 };
-
-export async function getStaticProps(content: any) {
-  return {
-    props: {
-      ...(await serviceSideProps(content))
-    }
-  };
-}
 
 export default NonePage;
