2ae20eda07fd53f9aa6e88f35155161d3a605c7b
archer <545436317@qq.com>
2025-10-24 15:39:30 +0800

    fix: toolcall check

--- packages/service/core/ai/llm/request.ts
+++ packages/service/core/ai/llm/request.ts
@@ -243,7 +243,7 @@ export const createStreamResponse = async ({
                   type: 'function',
                   function: callingTool!
                 };
-                toolCalls.push(call);
+                toolCalls[index] = call;
                 onToolCall?.({ call });
                 callingTool = null;
               }
@@ -268,7 +268,7 @@ export const createStreamResponse = async ({
         reasoningText: reasoningContent,
         finish_reason,
         usage,
-        toolCalls
+        toolCalls: toolCalls.filter((call) => !!call)
       };
     } else {
       let startResponseWrite = false;
