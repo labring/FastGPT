32f49ae7cc9c91875ae9225374324882f959e43b
FinleyGe <m13203533462@163.com>
2025-10-28 15:14:56 +0800

    chore: cache data expire 10 mins

--- projects/marketplace/src/service/tool/data.ts
+++ projects/marketplace/src/service/tool/data.ts
@@ -6,18 +6,20 @@ import { join } from 'node:path';
 declare global {
   // eslint-disable-next-line no-var
   var toolListData: Array<ToolDetailType>;
+  var expire: Date;
 }
 
 const dataFileURL = process.env.S3_PREFIX + '/data.json';
 const localDataFilePath = join(tmpdir(), 'data.json');
 
 export const getToolList = async () => {
-  if (!global.toolListData) {
+  if (!global.toolListData || global.expire < new Date()) {
     // download the file to local
     const res = await fetch(dataFileURL);
     await writeFile(localDataFilePath, Buffer.from(await res.arrayBuffer()));
     const data = await readFile(localDataFilePath, 'utf-8');
     global.toolListData = JSON.parse(data);
+    global.expire = new Date(Date.now() + 1000 * 10 * 60); // 10 minutes
   }
   return global.toolListData;
 };
