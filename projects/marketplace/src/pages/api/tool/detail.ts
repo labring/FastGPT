9361f139a5bce3f52e01c184f8d8df51c67a7765
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2025-10-28 11:05:03 +0800

    feat: marketplace apis & type definitions (#5817)
    * chore: marketplace init
    
    * chore: marketplace list api type
    
    * chore: detail api

---
+++ projects/marketplace/src/pages/api/tool/detail.ts
@@ -0,0 +1,39 @@
+import { getToolList } from '@/service/tool/data';
+import { ToolDetailSchema, type ToolDetailType } from '@fastgpt/global/sdk/fastgpt-plugin';
+import { getPkgdownloadURL } from '@/service/s3';
+import type { ApiRequestProps, ApiResponseType } from '@fastgpt/service/type/next';
+import { NextAPI } from '@/service/middleware/entry';
+
+export type ToolDetailQuery = {
+  toolId: string;
+};
+export type ToolDetailBody = {};
+export type ToolDetailResponse = {
+  tool: ToolDetailType;
+  downloadUrl: string;
+};
+
+async function handler(
+  req: ApiRequestProps<ToolDetailBody, ToolDetailQuery>,
+  res: ApiResponseType<any>
+): Promise<ToolDetailResponse> {
+  const { toolId } = req.query;
+
+  if (!toolId) {
+    throw new Error('toolId is required');
+  }
+
+  const toolList = await getToolList();
+  const tool = toolList.find((item) => item.toolId === toolId);
+
+  if (!tool) {
+    throw new Error('tool not found');
+  }
+
+  return {
+    tool: ToolDetailSchema.parse(tool),
+    downloadUrl: getPkgdownloadURL(toolId)
+  };
+}
+
+export default NextAPI(handler);
---
+++ projects/marketplace/src/service/middleware/entry.ts
@@ -0,0 +1,3 @@
+import { NextEntry } from '@fastgpt/service/common/middle/entry';
+
+export const NextAPI = NextEntry({ beforeCallback: [] });
