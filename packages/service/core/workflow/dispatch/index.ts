52d059cd039186ac35c7d1115c41a0c02e7a68a2
archer <545436317@qq.com>
2025-10-30 18:50:24 +0800

    fix: interactive loop

--- packages/service/core/workflow/dispatch/index.ts
+++ packages/service/core/workflow/dispatch/index.ts
@@ -467,6 +467,9 @@ export const runWorkflow = async (data: RunWorkflowProps): Promise<DispatchFlowR
 
       const dispatchData: ModuleDispatchProps<Record<string, any>> = {
         ...data,
+        lastInteractive: data.lastInteractive?.entryNodeIds.includes(node.nodeId)
+          ? data.lastInteractive
+          : undefined,
         variables,
         histories,
         retainDatasetCite,
