5be0a22cc36641ddb3592990a0f1389e80d43e24
archer <545436317@qq.com>
2025-10-30 23:01:20 +0800

    title

--- projects/marketplace/src/pages/_document.tsx
+++ projects/marketplace/src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Title should be defined at page level, not in _document */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
