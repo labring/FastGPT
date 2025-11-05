adf9981845e4997e2915821479cb3b149ed1d5ad
Archer <545436317@qq.com>
2025-11-05 13:49:13 +0800

    fix: http inputs (#5864)
    * fix: timeselector ui error
    
    * var update node
    
    * fix: http

--- packages/global/core/workflow/template/system/http468.ts
+++ packages/global/core/workflow/template/system/http468.ts
@@ -36,8 +36,8 @@ export const HttpNode468: FlowNodeTemplateType = {
         selectValueTypeList: Object.values(WorkflowIOValueTypeEnum),
         showDescription: false,
         showDefaultValue: true
-      },
-      deprecated: true
+      }
+      // deprecated: true
     },
     {
       key: NodeInputKeyEnum.httpMethod,
