c14d20321ec44ef34f279dba8526227df60a919a
heheer <zhiyu44@qq.com>
2025-11-19 17:49:42 +0800

    fix file var

--- packages/service/core/workflow/dispatch/init/workflowStart.tsx
+++ packages/service/core/workflow/dispatch/init/workflowStart.tsx
@@ -1,10 +1,12 @@
 import { chatValue2RuntimePrompt } from '@fastgpt/global/core/chat/adapt';
+import { ChatFileTypeEnum } from '@fastgpt/global/core/chat/constants';
 import { NodeInputKeyEnum, NodeOutputKeyEnum } from '@fastgpt/global/core/workflow/constants';
 import { DispatchNodeResponseKeyEnum } from '@fastgpt/global/core/workflow/runtime/constants';
 import type {
   DispatchNodeResultType,
   ModuleDispatchProps
 } from '@fastgpt/global/core/workflow/runtime/type';
+import { getS3ChatSource } from '../../../../common/s3/sources/chat';
 
 export type UserChatInputProps = ModuleDispatchProps<{
   [NodeInputKeyEnum.userChatInput]: string;
@@ -14,7 +16,7 @@ type Response = DispatchNodeResultType<{
   [NodeOutputKeyEnum.userFiles]: string[];
 }>;
 
-export const dispatchWorkflowStart = (props: Record<string, any>): Response => {
+export const dispatchWorkflowStart = async (props: Record<string, any>): Promise<Response> => {
   const {
     query,
     variables,
@@ -32,8 +34,35 @@ export const dispatchWorkflowStart = (props: Record<string, any>): Response => {
     ? variables.fileUrlList
     : [];
 
+  /*
+   * 参考 pluginInput 的实现逻辑
+   */
+  const processedVariables = { ...variables };
+  for (const key in processedVariables) {
+    const val = processedVariables[key];
+    if (
+      Array.isArray(val) &&
+      val.every(
+        (item) => item.type === ChatFileTypeEnum.file || item.type === ChatFileTypeEnum.image
+      )
+    ) {
+      for (let i = 0; i < val.length; i++) {
+        const fileItem = val[i];
+        if (fileItem.key && !fileItem.url) {
+          val[i].url = await getS3ChatSource().createGetChatFileURL({
+            key: fileItem.key,
+            external: true,
+            expiredHours: 1
+          });
+        }
+      }
+      processedVariables[key] = val.map((item) => item.url);
+    }
+  }
+
   return {
     [DispatchNodeResponseKeyEnum.nodeResponse]: {},
+    [DispatchNodeResponseKeyEnum.newVariables]: processedVariables,
     data: {
       [NodeInputKeyEnum.userChatInput]: text || userChatInput,
       [NodeOutputKeyEnum.userFiles]: [...queryFiles, ...variablesFiles]
