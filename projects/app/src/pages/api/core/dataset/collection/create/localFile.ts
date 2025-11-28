a80b3ebf9f3b9faa52b8086a4cb39983cc133e0f
Roy <stakeswky@gmail.com>
2025-11-28 15:02:59 +0800

    fix: get file buffer and upload file to S3 (#6004)

--- projects/app/src/pages/api/core/dataset/collection/create/localFile.ts
+++ projects/app/src/pages/api/core/dataset/collection/create/localFile.ts
@@ -1,36 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { uploadFile } from '@fastgpt/service/common/file/gridfs/controller';
 import { getUploadModel } from '@fastgpt/service/common/file/multer';
 import { authDataset } from '@fastgpt/service/support/permission/dataset/auth';
 import { type FileCreateDatasetCollectionParams } from '@fastgpt/global/core/dataset/api';
-import { removeFilesByPaths } from '@fastgpt/service/common/file/utils';
 import { createCollectionAndInsertData } from '@fastgpt/service/core/dataset/collection/controller';
 import { DatasetCollectionTypeEnum } from '@fastgpt/global/core/dataset/constants';
-import { getNanoid } from '@fastgpt/global/common/string/tools';
-import { BucketNameEnum } from '@fastgpt/global/common/file/constants';
 import { NextAPI } from '@/service/middleware/entry';
 import { WritePermissionVal } from '@fastgpt/global/support/permission/constant';
 import { type CreateCollectionResponse } from '@/global/core/dataset/api';
+import { getS3DatasetSource } from '@fastgpt/service/common/s3/sources/dataset';
+import { removeS3TTL } from '@fastgpt/service/common/s3/utils';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>): CreateCollectionResponse {
-  let filePaths: string[] = [];
-
   try {
-    // Create multer uploader
-    const upload = getUploadModel({
-      maxSize: global.feConfigs?.uploadFileMaxSize
-    });
-    const { file, data, bucketName } =
-      await upload.getUploadFile<FileCreateDatasetCollectionParams>(
-        req,
-        res,
-        BucketNameEnum.dataset
-      );
-    filePaths = [file.path];
-
-    if (!file || !bucketName) {
-      throw new Error('file is empty');
-    }
+    const upload = getUploadModel({ maxSize: global.feConfigs?.uploadFileMaxSize });
+    const { buffer, originalname } = await upload.getFileBuffer(req, res);
+    const data = (() => {
+      try {
+        return JSON.parse(req.body?.data || '{}');
+      } catch (error) {
+        return {};
+      }
+    })() as FileCreateDatasetCollectionParams;
 
     const { teamId, tmbId, dataset } = await authDataset({
       req,
@@ -40,29 +30,24 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>): CreateCo
       datasetId: data.datasetId
     });
 
-    const { fileMetadata, collectionMetadata, ...collectionData } = data;
-    const collectionName = file.originalname;
+    const s3DatasetSource = getS3DatasetSource();
 
-    // 1. upload file
-    const fileId = await uploadFile({
-      teamId,
-      uid: tmbId,
-      bucketName,
-      path: file.path,
-      filename: file.originalname,
-      contentType: file.mimetype,
-      metadata: fileMetadata
+    const fileId = await s3DatasetSource.uploadDatasetFileByBuffer({
+      datasetId: String(dataset._id),
+      buffer,
+      filename: originalname
     });
 
-    // 2. delete tmp file
-    removeFilesByPaths(filePaths);
+    await removeS3TTL({ key: fileId, bucketName: 'private' });
+
+    const { fileMetadata, collectionMetadata, ...collectionData } = data;
 
     // 3. Create collection
     const { collectionId, insertResults } = await createCollectionAndInsertData({
       dataset,
       createCollectionParams: {
         ...collectionData,
-        name: collectionName,
+        name: originalname,
         teamId,
         tmbId,
         type: DatasetCollectionTypeEnum.file,
@@ -76,8 +61,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>): CreateCo
 
     return { collectionId, results: insertResults };
   } catch (error) {
-    removeFilesByPaths(filePaths);
-
     return Promise.reject(error);
   }
 }
