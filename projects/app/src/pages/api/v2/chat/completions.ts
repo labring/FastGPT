a47927c3848ba1b9cfc7560b14b4e44a3963cdaf
Roy <whoeverimf5@gmail.com>
2025-12-04 15:37:05 +0800

    feat: add ip region for chat logs (#6010)
    * feat: add ip region for chat logs
    
    * refactor: use Geolite2.mmdb
    
    * fix: export chat logs
    
    * fix: return location directly
    
    * test: add unit test

--- projects/app/src/pages/api/v2/chat/completions.ts
+++ projects/app/src/pages/api/v2/chat/completions.ts
@@ -22,7 +22,6 @@ import { saveChat, updateInteractiveChat } from '@fastgpt/service/core/chat/save
 import { responseWrite } from '@fastgpt/service/common/response';
 import { authOutLinkChatStart } from '@/service/support/permission/auth/outLink';
 import { pushResult2Remote, addOutLinkUsage } from '@fastgpt/service/support/outLink/tools';
-import requestIp from 'request-ip';
 import { getUsageSourceByAuthType } from '@fastgpt/global/support/wallet/usage/tools';
 import { authTeamSpaceToken } from '@/service/support/permission/auth/team';
 import {
@@ -62,6 +61,7 @@ import { UserError } from '@fastgpt/global/common/error/utils';
 import { getLocale } from '@fastgpt/service/common/middle/i18n';
 import { formatTime2YMDHM } from '@fastgpt/global/common/string/time';
 import { LimitTypeEnum, teamFrequencyLimit } from '@fastgpt/service/common/api/frequencyLimit';
+import { getIpFromRequest } from '@fastgpt/service/common/geo';
 
 type FastGptWebChatProps = {
   chatId?: string; // undefined: get histories from messages, '': new chat, 'xxxxx': get histories from db
@@ -115,7 +115,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     metadata
   } = req.body as Props;
 
-  const originIp = requestIp.getClientIp(req);
+  const originIp = getIpFromRequest(req);
 
   const startTime = Date.now();
 
