1b4f427e285c1153c024549c0decd6ccae57ccf2
xqvvu <whoeverimf5@gmail.com>
2025-12-10 17:42:21 +0800

    CHORE

--- projects/app/src/pages/api/proApi/[...path].ts
+++ projects/app/src/pages/api/proApi/[...path].ts
@@ -44,7 +44,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.end();
     });
   } catch (error) {
-    console.dir(error, { depth: null });
     jsonRes(res, {
       code: 500,
       error
