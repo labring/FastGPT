3799f815d89ed7319189a1480861bd5034fa11e3
xqvvu <whoeverimf5@gmail.com>
2025-12-25 18:10:23 +0800

    refactor: use fetch instead of http.request

--- packages/service/common/proxy/index.ts
+++ packages/service/common/proxy/index.ts
@@ -1,14 +1,7 @@
-import http from 'http';
-import https from 'https';
-import { ProxyAgent } from 'proxy-agent';
 import { EnvHttpProxyAgent, setGlobalDispatcher } from 'undici';
 
 const fetchProxyAgent = new EnvHttpProxyAgent();
 setGlobalDispatcher(fetchProxyAgent);
-
-const httpProxyAgent = new ProxyAgent();
-http.globalAgent = httpProxyAgent;
-https.globalAgent = httpProxyAgent;
 
 console.info('HTTP_PROXY: %s', process.env.HTTP_PROXY);
 console.info('HTTPS_PROXY: %s', process.env.HTTPS_PROXY);
