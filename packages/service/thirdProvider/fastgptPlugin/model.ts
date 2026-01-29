8c645945a30bad86ecd3cfa639311320342bb5b8
xqvvu <whoeverimf5@gmail.com>
2026-01-29 10:16:51 +0800

    chore: adapt new plugin sdk

--- packages/service/thirdProvider/fastgptPlugin/model.ts
+++ packages/service/thirdProvider/fastgptPlugin/model.ts
@@ -1,11 +1,5 @@
 import { pluginClient } from '.';
 
 export const loadModelProviders = async () => {
-  const res = await pluginClient.model.getProviders();
-
-  if (res.status === 200) {
-    return res.body;
-  }
-
-  return Promise.reject(res.body);
+  return await pluginClient.getModelProviders();
 };
