358109f556175feb88b7d87272d79094122727d8
roy <whoeverimf5@gmail.com>
2026-02-02 11:37:19 +0800

    fix: nullish tag schema (#6365)

--- packages/global/openapi/core/plugin/team/api.ts
+++ packages/global/openapi/core/plugin/team/api.ts
@@ -11,7 +11,10 @@ export const TeamPluginListItemSchema = z.object({
   name: z.string(),
   intro: z.string().optional(),
   author: z.string().optional(),
-  tags: z.array(z.string()).nullish(),
+  tags: z
+    .array(z.string().nullish())
+    .nullish()
+    .transform((val) => val?.filter((v) => v !== null && v !== undefined)),
   status: PluginStatusSchema.optional().default(PluginStatusEnum.Normal),
   installed: z.boolean(),
   associatedPluginId: z.string().optional()
