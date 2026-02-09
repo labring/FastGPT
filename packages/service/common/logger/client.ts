863707a5d2e581d76a54ff2de1c596a7eaf858b2
xqvvu <whoeverimf5@gmail.com>
2026-02-09 17:55:24 +0800

    wip(log): add request id and inject it into context

--- packages/service/common/logger/client.ts
+++ packages/service/common/logger/client.ts
@@ -2,6 +2,7 @@ import { configure, dispose } from '@logtape/logtape';
 import { env } from '../../env';
 import { createSinks } from './sinks';
 import { createLoggers } from './loggers';
+import { getLogger as getLogtapeLogger } from '@logtape/logtape';
 
 let configured = false;
 export async function configureLogger() {
@@ -39,4 +40,40 @@ export async function desposeLogger() {
 
   await dispose();
   configured = false;
+}
+
+export function getLogger(category: readonly string[] = ['app']) {
+  const logger = getLogtapeLogger(category);
+
+  return new Proxy(logger, {
+    get(target, prop, receiver) {
+      const fn = Reflect.get(target, prop, receiver);
+      if (typeof fn !== 'function') return fn;
+      return (...args: unknown[]) => {
+        if (args.length === 0) return fn.call(target);
+        const [f, s] = args;
+        if (args.length === 1) {
+          return fn.call(target, f);
+        }
+        if (typeof f === 'string') {
+          if (
+            typeof s === 'object' &&
+            s &&
+            'verbose' in s &&
+            typeof s.verbose === 'boolean' &&
+            !s.verbose
+          ) {
+            delete s.verbose;
+            return fn.call(target, f, s);
+          }
+
+          return fn.call(target, `${f}: {*}`, s);
+        }
+        if (typeof f === 'object') {
+          return fn.call(target, f);
+        }
+        return fn.apply(target, args);
+      };
+    }
+  });
 }
