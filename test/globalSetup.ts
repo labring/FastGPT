d220414477f92bfc8485ab9f8939abba27842410
s0wa48 <msmaciejsowinski@gmail.com>
2026-02-20 13:47:03 +0100

    refactor(deps): upgrade mongodb-memory-server from v10.1.4 to v11.0.1

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -2,7 +2,7 @@ import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import type { TestProject } from 'vitest/node';
 
 export default async function setup(project: TestProject) {
-  const replset = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
+  const replset = await MongoMemoryReplSet.create({ replSet: { count: 1, storageEngine: 'wiredTiger' } });
   const uri = replset.getUri();
   project.provide('MONGODB_URI', uri);
 
