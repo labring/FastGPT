f4606433ee6e2bce8d1732e046549d7c7757d433
xxyyh <2289112474@qq.com>
2026-03-06 13:15:46 +0800

    agent pause

--- packages/global/core/chat/adapt.ts
+++ packages/global/core/chat/adapt.ts
@@ -138,7 +138,7 @@ export const chats2GPTMessages = ({
         const hasTools = Array.isArray(value.tools) && value.tools.length > 0;
 
         if (reserveTool && (hasTools || value.tool)) {
-          const tools = hasTools ? value.tools : [value.tool!];
+          const tools = hasTools ? value.tools! : [value.tool!];
           const tool_calls: ChatCompletionMessageToolCall[] = [];
           const toolResponse: ChatCompletionToolMessageParam[] = [];
           tools.forEach((tool) => {
