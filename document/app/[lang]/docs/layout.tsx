8fe81834e0b46eae2434d37df9f75253cd143b54
archer <545436317@qq.com>
2026-03-10 10:09:07 +0800

    fix: ts

--- document/app/[lang]/docs/layout.tsx
+++ document/app/[lang]/docs/layout.tsx
@@ -65,7 +65,6 @@ export default async function Layout({
         collapsible: false,
         components: CustomSidebarComponents
       }}
-      tabMode="sidebar"
       links={[
         {
           type: 'icon',
