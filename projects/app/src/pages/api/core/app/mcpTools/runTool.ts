01a8095328866d2152e9a3e8cc34346dbd24fba2
archer <545436317@qq.com>
2026-03-25 17:10:17 +0800

    mcp ssrf check

--- projects/app/src/pages/api/core/app/mcpTools/runTool.ts
+++ projects/app/src/pages/api/core/app/mcpTools/runTool.ts
@@ -7,12 +7,17 @@ import {
   type RunMcpToolBodyType,
   type RunMcpToolResponseType
 } from '@fastgpt/global/openapi/core/app/mcpTools/api';
+import { isInternalAddress, PRIVATE_URL_TEXT } from '@fastgpt/service/common/system/utils';
 
 async function handler(
   req: ApiRequestProps<RunMcpToolBodyType>,
   _res: ApiResponseType<any>
 ): Promise<RunMcpToolResponseType> {
   const { url, toolName, headerSecret, params } = RunMcpToolBodySchema.parse(req.body);
+
+  if (await isInternalAddress(url)) {
+    return Promise.reject(PRIVATE_URL_TEXT);
+  }
 
   const mcpClient = new MCPClient({
     url,
