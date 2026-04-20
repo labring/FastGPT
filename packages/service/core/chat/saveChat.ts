c1a08778e1a7d637f968c46e31a1570c7fa2c8ce
Ryo <whoeverimf5@gmail.com>
2026-04-20 16:51:22 +0800

    fix(chat): keep stream resume out of v1 completions (#6774)
    * fix(chat): avoid duplicate v1 completion history items
    
    * fix(chat): restore v1 completion persistence flow
    
    * fix(chat): keep stream resume out of v1 completions
    
    * fix(chat): revert pushChatRecords append flow

--- packages/service/core/chat/saveChat.ts
+++ packages/service/core/chat/saveChat.ts
@@ -771,96 +771,30 @@ export const pushChatRecords = async (props: Props) => {
       errorMsg
     });
     const processedContent = [userContent, aiResponse];
-    const humanRoundDataId = (processedContent[0] as { dataId?: string }).dataId as string;
-    const aiRoundDataId = (processedContent[1] as { dataId?: string }).dataId as string;
 
     await mongoSessionRun(async (session) => {
-      const humanExisting = await MongoChatItem.findOne({
-        appId,
-        chatId,
-        dataId: humanRoundDataId,
-        obj: ChatRoleEnum.Human
-      }).session(session);
-      const aiExisting = await MongoChatItem.findOne({
-        appId,
-        chatId,
-        dataId: aiRoundDataId,
-        obj: ChatRoleEnum.AI
-      }).session(session);
+      const [{ _id: chatItemIdHuman }, { _id: chatItemIdAi, dataId }] = await MongoChatItem.create(
+        processedContent.map((item) => ({
+          chatId,
+          teamId,
+          tmbId,
+          appId,
+          ...item
+        })),
+        { session, ordered: true, ...writePrimary }
+      );
 
-      let chatItemIdHuman: unknown;
-      let chatItemIdAi: unknown;
-      let responseDataId = aiRoundDataId;
-
-      if (humanExisting && aiExisting) {
-        await MongoChatItem.updateOne(
-          { _id: humanExisting._id },
-          {
-            $set: {
-              ...(processedContent[0] as Record<string, unknown>),
-              obj: ChatRoleEnum.Human
-            }
-          },
-          { session }
-        );
-        await MongoChatItem.updateOne(
-          { _id: aiExisting._id },
-          {
-            $set: {
-              ...(processedContent[1] as Record<string, unknown>),
-              obj: ChatRoleEnum.AI
-            }
-          },
-          { session }
-        );
-
-        await MongoChatItemResponse.deleteMany(
-          { appId, chatId, chatItemDataId: aiRoundDataId },
-          { session }
-        );
-
-        if (nodeResponses?.length) {
-          await MongoChatItemResponse.create(
-            nodeResponses.map((item) => ({
-              teamId,
-              appId,
-              chatId,
-              chatItemDataId: aiRoundDataId,
-              data: item
-            })),
-            { session, ordered: true }
-          );
-        }
-
-        chatItemIdHuman = humanExisting._id;
-        chatItemIdAi = aiExisting._id;
-      } else {
-        const [humanCreated, aiCreated] = await MongoChatItem.create(
-          processedContent.map((item) => ({
-            chatId,
+      if (nodeResponses) {
+        await MongoChatItemResponse.create(
+          nodeResponses.map((item) => ({
             teamId,
-            tmbId,
             appId,
-            ...item
+            chatId,
+            chatItemDataId: dataId,
+            data: item
           })),
-          { session, ordered: true }
+          { session, ordered: true, ...writePrimary }
         );
-        chatItemIdHuman = humanCreated._id;
-        chatItemIdAi = aiCreated._id;
-        responseDataId = aiCreated.dataId;
-
-        if (nodeResponses) {
-          await MongoChatItemResponse.create(
-            nodeResponses.map((item) => ({
-              teamId,
-              appId,
-              chatId,
-              chatItemDataId: responseDataId,
-              data: item
-            })),
-            { session, ordered: true }
-          );
-        }
       }
 
       await MongoChat.updateOne(
@@ -894,7 +828,8 @@ export const pushChatRecords = async (props: Props) => {
         },
         {
           session,
-          upsert: true
+          upsert: true,
+          ...writePrimary
         }
       );
 
