c7c8f7eea33b837652caba2313f616c7100a9750
黄越w56800 <56800@sangfor.com>
2026-04-24 17:53:10 +0800

    fix: 修复训练模型弹窗翻译未注册，修改合入引发问题

--- projects/app/src/pages/account/model/index.tsx
+++ projects/app/src/pages/account/model/index.tsx
@@ -60,7 +60,7 @@ const ModelProvider = () => {
 export async function getServerSideProps(content: any) {
   return {
     props: {
-      ...(await serviceSideProps(content, ['account', 'account_model', 'user', 'app']))
+      ...(await serviceSideProps(content, ['account', 'account_model', 'user', 'app', 'train']))
     }
   };
 }
