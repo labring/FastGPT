f9cc7eca305f379a32476c02c5ed086c8e3e98f6
archer <545436317@qq.com>
2026-05-25 19:51:25 +0800

    chore: remove unused app version refs index

--- packages/service/core/app/version/schema.ts
+++ packages/service/core/app/version/schema.ts
@@ -49,7 +49,6 @@ const AppVersionSchema = new Schema(
 );
 
 AppVersionSchema.index({ appId: 1, time: -1 });
-AppVersionSchema.index({ isPublish: 1, appId: 1, time: -1, _id: -1 });
 
 export const MongoAppVersion = getMongoModel<AppVersionSchemaType>(
   AppVersionCollectionName,
