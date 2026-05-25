a1ea7c40cdde04a80a0061d908afb3af34fd38e1
Archer <545436317@qq.com>
2026-05-25 18:16:39 +0800

    doc (#6981)
    * doc
    
    * docs: translate cloud FAQ

--- document/mdx-components.tsx
+++ document/mdx-components.tsx
@@ -4,12 +4,24 @@ import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import { TypeTable } from 'fumadocs-ui/components/type-table';
 import { LocalizedLink } from '@/components/docs/LocalizedLink';
+import type { ComponentProps } from 'react';
+
+function MdxImage(props: ComponentProps<'img'>) {
+  const hasWidth = props.width !== undefined && props.width !== null && props.width !== '';
+  const hasHeight = props.height !== undefined && props.height !== null && props.height !== '';
+
+  if (hasWidth && hasHeight) {
+    return <ImageZoom {...(props as any)} />;
+  }
+
+  return <img {...props} alt={props.alt ?? ''} loading={props.loading ?? 'lazy'} />;
+}
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as any)} />,
+    img: (props) => <MdxImage {...props} />,
     a: (props) => <LocalizedLink {...(props as any)} />,
     ...TabsComponents,
     ...components,
