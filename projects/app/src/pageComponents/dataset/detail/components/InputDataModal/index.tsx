8076b070cb5c3f23d2fa598f0c065b7408eeb4fe
xxyyh <2289112474@qq.com>
2026-06-01 10:48:49 +0800

    fix: align dataset index modal layout

--- projects/app/src/pageComponents/dataset/detail/components/InputDataModal/index.tsx
+++ projects/app/src/pageComponents/dataset/detail/components/InputDataModal/index.tsx
@@ -66,9 +66,15 @@ const InputDataModal = ({
       borderRadius={'10px'}
       bg={'white'}
       boxShadow={'3.5'}
-      px={[5, '32px']}
-      py={[5, '32px']}
-      headerStyles={{ px: 0 }}
+      headerStyles={{
+        px: [5, '32px']
+      }}
+      bodyStyles={{
+        h: 'auto',
+        flex: '1 1 0',
+        minH: 0,
+        px: [5, '32px']
+      }}
       title={
         <Box
           className={'textEllipsis'}
