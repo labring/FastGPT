8ea362ab3c28188a27c1190eebf95b4a7035ea99
熊涛10037 <10037@sangfor.com>
2026-06-26 14:26:14 +0800

    Revert "fix: 2026061700085支持wmf格式图片转换为png"
    This reverts commit b819aac97cc63fcac1b311dce960d3848e1671c5.

--- packages/service/worker/readFile/extension/docx.ts
+++ packages/service/worker/readFile/extension/docx.ts
@@ -2,7 +2,6 @@ import mammoth, { images } from 'mammoth';
 import { type ReadRawTextByBuffer, type ReadFileResponse, type ImageType } from '../type';
 import { html2md } from '../../htmlStr2Md/utils';
 import { getLogger, LogCategories } from '../../../common/logger';
-import { isUnsupportedImageMime, convertUnsupportedImageToPng } from '../convertImage';
 
 /**
  * read docx to markdown
@@ -20,21 +19,10 @@ export const readDocsFile = async ({ buffer }: ReadRawTextByBuffer): Promise<Rea
         convertImage: images.imgElement(async (image) => {
           const imageBase64 = await image.readAsBase64String();
           const uuid = crypto.randomUUID();
-          let mime = image.contentType;
-          let base64 = imageBase64;
-
-          // Convert EMF/WMF to PNG for browser compatibility
-          if (isUnsupportedImageMime(mime)) {
-            const converted = await convertUnsupportedImageToPng(imageBase64, mime);
-            if (converted) {
-              base64 = converted.base64;
-              mime = converted.mime;
-            }
-          }
-
+          const mime = image.contentType;
           imageList.push({
             uuid,
-            base64,
+            base64: imageBase64,
             mime
           });
           return {
