8fee8a0418110b06b75a8a04263ca06926caa6d7
shortlight5980 <lijunwei5980@163.com>
2026-07-29 11:51:31 +0800

    feat: add account verification service

---
+++ packages/service/support/user/account/verification/password/type.ts
@@ -0,0 +1,30 @@
+import type { UserModelSchema } from '@fastgpt/global/support/user/type';
+
+export type IssuePreLoginCodeParams = {
+  username: string;
+};
+
+export type IssuePreLoginCodeResult = {
+  code: string;
+};
+
+export type VerifyPasswordCredentialsParams = {
+  username: string;
+  password: string;
+  code: string;
+};
+
+export type PasswordVerificationDependencies = {
+  generateCode: (length: number) => string;
+  now: () => Date;
+  savePreLoginCode: (params: {
+    username: string;
+    code: string;
+    expiredTime: Date;
+  }) => Promise<unknown>;
+  verifyPreLoginCode: (params: { username: string; code: string }) => Promise<unknown>;
+  findUserByCredentials: (params: {
+    username: string;
+    password: string;
+  }) => Promise<UserModelSchema | null>;
+};
