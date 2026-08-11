26e4521e0186ac82838e3fc2160541c4425e13d1
Archer <545436317@qq.com>
2026-08-11 11:54:01 +0800

    Fix dev API route paths

--- packages/global/openapi/support/user/team/index.ts
+++ packages/global/openapi/support/user/team/index.ts
@@ -5,7 +5,7 @@ import { EnterpriseAuthPath } from './enterpriseAuth';
 
 export const TeamPath: OpenAPIPath = {
   ...EnterpriseAuthPath,
-  '/api/support/user/team/update': {
+  '/support/user/team/update': {
     post: {
       summary: '更新团队信息',
       description: '更新团队名称、头像、域名、第三方账号（OpenAI）及外部工作流变量',
