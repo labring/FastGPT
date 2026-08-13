b6beffd60103326048039d2688cf889039d7cdf3
Archer <545436317@qq.com>
2026-08-13 11:14:12 +0800

    fix(docs): handle missing static assets correctly

--- document/middleware.ts
+++ document/middleware.ts
@@ -182,6 +182,6 @@ export default function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|robots\\.txt|sitemap.*\\.xml|.*\\.svg|.*\\.png|deploy/.*).*)'
+    '/((?!api|_next/static|_next/image|favicon(?:\\.ico|/)|robots\\.txt|sitemap.*\\.xml|.*\\.svg|.*\\.png|deploy/.*).*)'
   ]
 };
