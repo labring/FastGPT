56be0985da596977cff3249d7f3e6a604a52c527
Archer <545436317@qq.com>
2026-09-05 23:09:12 +0800

    fix(ui): increase model provider filter height

--- projects/app/src/pageComponents/account/model/ModelConfigTable.tsx
+++ projects/app/src/pageComponents/account/model/ModelConfigTable.tsx
@@ -419,6 +419,7 @@ const ModelTable = ({ Tab }: { Tab: React.ReactNode }) => {
               onChange={setProvider}
               showSearch
               maxW={'240px'}
+              listMaxH={'240px'}
             />
             <SingleSelectFilter
               title={t('common:model.model_type')}
