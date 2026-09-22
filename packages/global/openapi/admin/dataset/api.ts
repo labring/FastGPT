2f59eef4abcc08f2d71edfde33ab13528ebb04bc
Archer <545436317@qq.com>
2026-09-22 18:11:21 +0800

    refactor(openapi): use ObjectIdSchema for dataset item ids

--- packages/global/openapi/admin/dataset/api.ts
+++ packages/global/openapi/admin/dataset/api.ts
@@ -1,9 +1,10 @@
 import z from 'zod';
+import { ObjectIdSchema } from '../../../common/type/mongo';
 import { PaginationResponseSchema } from '../../api';
 
 export const DatasetItemSchema = z.object({
-  id: z.string().meta({ description: '知识库ID' }),
-  teamId: z.string().meta({ description: '所属团队ID' }),
+  id: ObjectIdSchema.meta({ description: '知识库ID' }),
+  teamId: ObjectIdSchema.meta({ description: '所属团队ID' }),
   name: z.string().meta({ description: '知识库名称' }),
   intro: z.string().meta({ description: '知识库简介' }),
   username: z.string().meta({ description: '创建者用户名' }),
